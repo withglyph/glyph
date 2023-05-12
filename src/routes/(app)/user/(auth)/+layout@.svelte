ff26184224f7ecaa16044b8c768eb82afd918b23
Bae Junehyeon <devunt@gmail.com>
2023-05-12 19:45:30 +0900

    💄 (layout): auth 관련 페이지에 랜덤 배경 이미지 추가

--- src/routes/(app)/user/(auth)/+layout@.svelte
+++ src/routes/(app)/user/(auth)/+layout@.svelte
@@ -2,12 +2,17 @@
   import Wordmark from '$assets/branding/wordmark.svg?component';
 </script>
 
-<main class="flex grow center bg-brand-500">
-  <a class="absolute left-4 top-4" href="/">
+<main class="flex grow center">
+  <img
+    class="pointer-events-none absolute inset-0 h-full w-full"
+    src="https://picsum.photos/1920/1080"
+  />
+
+  <a class="absolute left-4 top-4 z-1" href="/">
     <Wordmark class="h-6 text-white" />
   </a>
 
-  <div class="rounded-lg bg-gray-50 p-12 shadow-2xl">
+  <div class="z-1 rounded-xl bg-white p-12 shadow-2xl">
     <slot />
   </div>
 </main>
