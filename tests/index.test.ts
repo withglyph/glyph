40b0d4ed53b5dbe635d9e83b08f48c2f651bf0d9
Bae Junehyeon <devunt@gmail.com>
2023-06-04 14:29:00 +0900

    ✅ (test): e2e 테스트 업데이트

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,13 +4,13 @@ test('메인 페이지에 GraphQL 응답이 포함되어 있는지 확인 (SSR)'
   page,
 }) => {
   await page.goto('/');
-  await expect(page.getByText('world')).toBeVisible();
+  await expect(page.getByText('Hello world!')).toBeVisible();
 });
 
 test('메인 페이지에 GraphQL 응답이 포함되어 있는지 확인 (CSR)', async ({
   page,
 }) => {
-  await page.goto('/db');
-  await page.getByText('Home').click();
-  await expect(page.getByText('world')).toBeVisible();
+  await page.goto('/posts');
+  await page.getByText('전체').click();
+  await expect(page.getByText('Hello world!')).toBeVisible();
 });
