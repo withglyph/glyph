3ea98628ad2c5f5266ee3c650331f9c78674583a
Bae Junehyeon <devunt@gmail.com>
2023-06-05 00:36:50 +0900

    🐛 (database): varbinary도 텍스트 인코딩 함

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,3 +1,4 @@
+import { cast } from '@planetscale/database';
 import { CamelCasePlugin, Kysely, sql } from 'kysely';
 import { PlanetScaleDialect } from 'kysely-planetscale';
 import { DATABASE_URL } from '$env/static/private';
@@ -7,6 +8,19 @@ export const db = new Kysely<DB>({
   dialect: new PlanetScaleDialect({
     url: DATABASE_URL,
     useSharedConnection: true,
+    cast: (field, value) => {
+      if (value === '' || value === null) {
+        return value;
+      }
+
+      if (field.type === 'VARBINARY') {
+        const data = Uint8Array.from(value, (x) => x.codePointAt(0)!);
+        return new TextDecoder().decode(data);
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return cast(field, value);
+    },
   }),
   plugins: [new CamelCasePlugin()],
 });
@@ -18,7 +32,7 @@ export const injectLoader = <T extends keyof DB>(tableName: T) => {
         .selectFrom<T>(tableName)
         .selectAll()
         .where(sql`id in (${ids})`)
-        .orderBy('id', 'asc')
+        .orderBy(sql`id`, 'asc')
         .execute();
     },
     sort: (entity: { id: string }) => entity.id,
