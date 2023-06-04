0deb6c31f8cf4ef311505995568dc9fb2d75c2ff
Bae Junehyeon <devunt@gmail.com>
2023-06-05 01:25:20 +0900

    ⚗️ (wasm): wasm 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import houdini from 'houdini/vite';
 import unocss from 'unocss/vite';
 import { defineConfig } from 'vite';
+import tla from 'vite-plugin-top-level-await';
+import wasm from 'vite-plugin-wasm';
 import { svg } from './src/vite';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   server: { host: '127.0.0.1', port: 4000, strictPort: true },
-  plugins: [unocss({ mode: 'global' }), houdini(), svg(), sveltekit()],
+  plugins: [
+    houdini(),
+    sveltekit(),
+    svg(),
+    tla(),
+    unocss({ mode: 'global' }),
+    wasm(),
+  ],
   preview: { host: '127.0.0.1', port: 5000, strictPort: true },
 });
