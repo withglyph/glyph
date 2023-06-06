4f5f9b92059c73f5ef934aabd8c6f9a76479081b
Bae Junehyeon <devunt@gmail.com>
2023-06-06 09:46:17 +0900

    ✨ (component): tiptap 추가

---
+++ src/lib/components/forms/Tiptap.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Editor } from '@tiptap/core';
+  import { StarterKit } from '@tiptap/starter-kit';
+  import { onDestroy, onMount } from 'svelte';
+
+  let element: HTMLDivElement;
+  let editor: Editor | undefined;
+
+  onMount(() => {
+    editor = new Editor({
+      element,
+      extensions: [StarterKit],
+      content: '<p>Hello World! 🌍️ </p>',
+    });
+  });
+
+  onDestroy(() => {
+    editor?.destroy();
+  });
+</script>
+
+<div bind:this={element} />
