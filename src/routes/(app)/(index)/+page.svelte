69aa83f9e2063b02195e813d3d0d75d7370df7de
Bae Junehyeon <devunt@gmail.com>
2023-06-09 17:11:15 +0900

    🚚 (chore): og image 경로 수정

--- src/routes/(app)/(index)/+page.svelte
+++ src/routes/(app)/(index)/+page.svelte
@@ -14,7 +14,7 @@
 
 <Helmet
   description="펜슬은 개인 창작자들을 위한 자유롭고 즐거운 창작 공간이에요."
-  image={{ src: absolutePath('/og/index.png'), size: 'large' }}
+  image={{ src: absolutePath('/images/og.png'), size: 'large' }}
   title="펜슬 - 창작을 즐겁게"
 />
 
