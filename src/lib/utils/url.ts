bc80f28f2e7bc99b6487af08d27783277cbd7c8b
Bae Junehyeon <devunt@gmail.com>
2023-06-10 22:06:14 +0900

    🐛 (util): absolutePath 버그 수정

--- src/lib/utils/url.ts
+++ src/lib/utils/url.ts
@@ -1,5 +1,8 @@
 import { get } from 'svelte/store';
 import { page } from '$app/stores';
 
-export const absolutePath = (path: string) =>
-  new URL(path, get(page).url).toString();
+export const absolutePath = (path: string) => {
+  const { url } = get(page);
+  url.pathname = '';
+  return new URL(path, url).toString();
+};
