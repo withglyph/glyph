2153e22e89d0617b5e4741aeeef6bb260296c0d4
Bae Junehyeon <devunt@gmail.com>
2023-06-12 15:40:02 +0900

    ✅ (test): 변경된 레이아웃에 맞춰 e2e 테스트 수정

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -10,7 +10,7 @@ test('메인 페이지에 GraphQL 응답이 포함되어 있는지 확인 (SSR)'
 test('메인 페이지에 GraphQL 응답이 포함되어 있는지 확인 (CSR)', async ({
   page,
 }) => {
-  await page.goto('/posts');
-  await page.getByText('전체').click();
+  await page.goto('/search?q=1');
+  await page.locator('header > nav > section > a').click();
   await expect(page.getByText('Hello world!')).toBeVisible();
 });
