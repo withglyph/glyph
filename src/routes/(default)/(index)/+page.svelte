d7d99899c2ab9317e37adc69810e5b93acb43dd4
Bae Junehyeon <devunt@gmail.com>
2023-06-19 11:27:15 +0900

    🔥 (asset): 폰트/이미지 cdn으로 옮김

--- src/routes/(default)/(index)/+page.svelte
+++ src/routes/(default)/(index)/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { Helmet } from '$lib/components';
   import { graphql, useQuery } from '$lib/houdini';
-  import { absolutePath } from '$lib/utils';
 
   $: query = useQuery(
     graphql(`
@@ -14,7 +13,7 @@
 
 <Helmet
   description="펜슬은 개인 창작자들을 위한 자유롭고 즐거운 창작 공간이에요."
-  image={{ src: absolutePath('/images/og.png'), size: 'large' }}
+  image={{ src: 'https://pnxl.net/s/og.png', size: 'large' }}
   title="펜슬 - 창작을 즐겁게"
 />
 
