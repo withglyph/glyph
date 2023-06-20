84ed64c95995504cc5a2aaa56bf7748a24f780e7
Bae Junehyeon <finn@penxle.io>
2023-06-20 16:36:37 +0900

    🎨 (lib): web-vitals 코드 스타일 개선

--- src/lib/analytics/web-vitals.ts
+++ src/lib/analytics/web-vitals.ts
@@ -1,6 +1,6 @@
 import { get } from 'svelte/store';
 import { onCLS, onFCP, onFID, onINP, onLCP, onTTFB } from 'web-vitals';
-import { page as $page } from '$app/stores';
+import { page } from '$app/stores';
 import { PUBLIC_VERCEL_ANALYTICS_ID } from '$env/static/public';
 import type { Metric } from 'web-vitals';
 
@@ -12,15 +12,15 @@ const onEvent = (metric: Metric) => {
     return;
   }
 
-  const page = get($page);
+  const $page = get(page);
 
   const parameters = new URLSearchParams({
     dsn: PUBLIC_VERCEL_ANALYTICS_ID,
     id: metric.id,
     event_name: metric.name,
     value: metric.value.toString(),
-    href: page.url.href,
-    page: page.route.id ?? '(unknown)',
+    href: $page.url.href,
+    page: $page.route.id ?? '(unknown)',
     // @ts-expect-error: navigator connection is not yet in the types
     speed: navigator.connection?.effectiveType ?? '',
   });
