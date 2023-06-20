a76689d71636516f45a16ab5ed2a87de8fc724ea
Bae Junehyeon <finn@penxle.io>
2023-06-20 14:23:11 +0900

    🔧 (chore): houdini grpahql document 경로 수정

--- graphql.config.cjs
+++ graphql.config.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   schema: ['schema.graphql', '$houdini/graphql/schema.graphql'],
-  documents: ['**/*.{gql,svelte}', '$houdini/graphql/documents.graphql'],
+  documents: ['**/*.{gql,svelte}', '$houdini/graphql/documents.gql'],
 };
