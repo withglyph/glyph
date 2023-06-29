9ddd5cbe6f6a5997e972f267692589ee8cf24ef8
Bae Junehyeon <finn@penxle.io>
2023-06-29 19:52:18 +0900

    💄 (layout): 푸터에 현재 커밋 및 SNS 링크 추가

--- src/routes/(default)/Footer.svelte
+++ src/routes/(default)/Footer.svelte
@@ -4,11 +4,27 @@
   import { Link } from '$lib/components';
 </script>
 
-<footer class="border-t px-4 py-8 text-xs tracking-wider">
+<footer class="border-t px-4 py-8 text-xs tracking-wider text-gray-400">
   <nav class="mx-auto max-w-screen-lg space-y-4">
-    <Wordmark class="h-4 text-gray-400" />
+    <section class="flex items-center justify-between">
+      <Wordmark class="h-4" />
+      <div class="flex items-center gap-4">
+        <Link
+          class="i-lg-twitter square-5 hover:text-[#1DA1F2]"
+          href="https://twitter.com/penxle"
+        />
+        <Link
+          class="i-lg-instagram square-5 hover:text-[#E4405F]"
+          href="https://www.instagram.com/penxle.team/"
+        />
+        <Link
+          class="i-lg-github square-5 hover:text-[#181717]"
+          href="https://github.com/penxle"
+        />
+      </div>
+    </section>
 
-    <section class="text-gray-400">
+    <section>
       <div class="flex items-center gap-2">
         <div>(주)펜슬컴퍼니</div>
         <div class="h-2 border-l border-gray-300" />
@@ -34,16 +50,18 @@
       </div>
     </section>
 
-    <section class="flex items-center gap-2 text-gray-400">
-      <Link href="/">이용약관</Link>
+    <section class="flex items-center gap-2">
+      <Link class="hover:text-gray-500" href="/">이용약관</Link>
       <div class="h-2 border-l border-gray-300" />
-      <Link class="font-medium" href="/">개인정보처리방침</Link>
+      <Link class="font-medium hover:text-gray-500" href="/">
+        개인정보처리방침
+      </Link>
       <div class="h-2 border-l border-gray-300" />
       <Link
-        class="flex items-center gap-1"
+        class="flex items-center gap-1 hover:text-gray-500"
         href={`https://github.com/penxle/penxle/tree/${PUBLIC_VERCEL_GIT_COMMIT_SHA}`}
       >
-        <span class="i-lg-github" />
+        <span class="i-lc-git-branch" />
         {PUBLIC_VERCEL_GIT_COMMIT_SHA.slice(0, 7)}
       </Link>
     </section>
