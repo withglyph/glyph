3dd880ee5458b0a4352922e893afd777bc7bd12a
Bae Junehyeon <finn@penxle.io>
2023-06-30 14:50:49 +0900

    🚧 (hooks): 점검 체크 임시 비활성화

--- src/hooks/server.ts
+++ src/hooks/server.ts
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/sveltekit';
 import { sequence } from '@sveltejs/kit/hooks';
-import { isUnderMaintenance } from '$lib/server/external/vercel';
+// import { isUnderMaintenance } from '$lib/server/external/vercel';
 import { setupGlobals } from './common';
 import type { Handle } from '@sveltejs/kit';
 
@@ -13,9 +13,9 @@ const _handle = (async ({ event, resolve }) => {
     return new Response(null, { status: 405 });
   }
 
-  if (await isUnderMaintenance(event)) {
-    return await event.fetch('/_/internal/under-maintenance', event.request);
-  }
+  // if (await isUnderMaintenance(event)) {
+  //   return await event.fetch('/_/internal/under-maintenance', event.request);
+  // }
 
   return await resolve(event);
 }) satisfies Handle;
