1aa73dc68e7ce207f31c63a893e8b83ddabc1551
Bae Junehyeon <finn@penxle.io>
2023-07-16 12:50:22 +0900

    🚚 (chore): cdn static path 업데이트

--- src/routes/(default)/(index)/+page.svelte
+++ src/routes/(default)/(index)/+page.svelte
@@ -4,7 +4,7 @@
 
 <Helmet
   description="펜슬은 개인 창작자들을 위한 자유롭고 즐거운 창작 공간이에요."
-  image={{ src: 'https://pnxl.net/s/og.png', size: 'large' }}
+  image={{ src: 'https://pnxl.net/static/og.png', size: 'large' }}
   title="펜슬 - 창작을 즐겁게"
 />
 
