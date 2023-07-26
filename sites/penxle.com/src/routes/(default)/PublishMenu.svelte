891f2bb0bec905b0dee93def74291385bcde0b78
finn <finn@penxle.io>
2023-07-26 15:21:15 +0900

    💄 (layout): 올리기 버튼 컬러 변경해봄

--- sites/penxle.com/src/routes/(default)/PublishMenu.svelte
+++ sites/penxle.com/src/routes/(default)/PublishMenu.svelte
@@ -48,7 +48,7 @@
 
 <button
   bind:this={targetEl}
-  class="relative flex items-center gap-2 rounded-full from-cyan-500 via-brand-500 to-violet-500 bg-gradient-to-rb px-4 py-2 font-medium text-white transition before:(absolute inset-0 rounded-full bg-brand-500 opacity-0 transition content-empty hover:opacity-100)"
+  class="relative flex items-center gap-2 rounded-full from-[#FFBB33] to-[#D85454] bg-gradient-to-r px-4 py-2 font-medium text-white transition before:(absolute inset-0 rounded-full bg-black opacity-0 transition content-empty hover:opacity-10)"
   tabindex="-1"
   type="button"
   on:click={() => (open = true)}
