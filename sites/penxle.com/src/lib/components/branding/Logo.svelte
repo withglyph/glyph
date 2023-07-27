e60f47bc001583e9f8b6fb475e6bb58cbfbce748
finn <finn@penxle.io>
2023-07-27 16:22:06 +0900

    🍱 (asset): 로고 업데이트

--- sites/penxle.com/src/lib/components/branding/Logo.svelte
+++ sites/penxle.com/src/lib/components/branding/Logo.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import { clsx } from 'clsx';
+  import Logo from '$assets/branding/logo.svg?component';
   import { Link } from '..';
 
   let _class: string | undefined = undefined;
   export { _class as class };
 </script>
 
-<Link class={clsx('i-px-logo text-gray-900', _class)} href="/" />
+<Link href="/">
+  <Logo class={clsx('flex-none', _class)} />
+</Link>
