d745350e8dcf131e0242a93d280f88f174528edb
finn <finn@penxle.io>
2023-07-29 04:43:31 +0900

    🚚 (chore): 기본 og:image 경로 수정

--- sites/penxle.com/src/routes/(default)/(index)/+page.svelte
+++ sites/penxle.com/src/routes/(default)/(index)/+page.svelte
@@ -4,7 +4,10 @@
 
 <Helmet
   description="펜슬은 개인 창작자들을 위한 자유롭고 즐거운 창작 공간이에요."
-  image={{ src: 'https://pnxl.net/assets/og.png', size: 'large' }}
+  image={{
+    src: 'https://pnxl.net/assets/opengraph/default.png',
+    size: 'large',
+  }}
   title="펜슬 - 함께 그리는 반짝임"
 />
 
