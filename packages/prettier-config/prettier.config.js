7ddc2d2adf9abb4df38a805dc9c861ed73151a21
finn <finn@penxle.io>
2023-08-01 12:49:52 +0900

    🍱 (sites/*, prettier-config): 로고 및 워드마크 업데이트

--- packages/prettier-config/prettier.config.js
+++ packages/prettier-config/prettier.config.js
@@ -2,9 +2,14 @@ export default {
   htmlWhitespaceSensitivity: 'ignore',
   singleQuote: true,
   quoteProps: 'consistent',
+
+  xmlWhitespaceSensitivity: 'ignore',
+  xmlQuoteAttributes: 'double',
+
   plugins: [
     'prettier-plugin-prisma',
     'prettier-plugin-sh',
     'prettier-plugin-svelte',
+    '@prettier/plugin-xml',
   ],
 };
