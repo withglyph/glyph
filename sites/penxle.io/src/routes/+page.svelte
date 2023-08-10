640a97375d310c00e8becdcefb0ff6647eac35dc
finn <finn@penxle.io>
2023-08-10 14:23:37 +0900

    ⚗️ (company): 폰트 테스트

--- sites/penxle.io/src/routes/+page.svelte
+++ sites/penxle.io/src/routes/+page.svelte
@@ -5,9 +5,18 @@
 
 <Helmet title="펜슬컴퍼니" />
 
-<div class="pointer-events-none flex grow select-none center">
+<div class="pointer-events-none flex grow flex-col select-none center gap-4">
   <div class="flex items-center gap-4">
     <Logo class="square-24" />
-    <div class="text-20 font-black">펜슬컴퍼니</div>
+    <div class="font-sans text-20 font-black">펜슬컴퍼니</div>
   </div>
+
+  <div class="flex items-center gap-4">
+    <Logo class="square-24" />
+    <div class="font-content-sans text-20 font-black">펜슬컴퍼니</div>
+  </div>
+
+  <div class="font-sans mt-16 text-20 font-bold">일이삼사 1234 !@#$</div>
+
+  <div class="font-content-sans text-20 font-bold">일이삼사 1234 !@#$</div>
 </div>
