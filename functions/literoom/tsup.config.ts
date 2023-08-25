08b13c66908db0db41457075391e1ff378005007
finn <finn@penxle.io>
2023-08-25 19:02:17 +0900

    ♻️ (functions/literoom): media 람다 핸들러 재작성

--- functions/literoom/tsup.config.ts
+++ functions/literoom/tsup.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   clean: true,
-
-  entry: ['src/handlers/*.ts'],
+  entry: ['src/handlers/finalize.ts', 'src/handlers/transform.ts'],
   format: 'esm',
-
-  splitting: false,
-  external: ['sharp'],
 });
