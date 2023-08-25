433f23c036ee6ac120a0732ffb68b272b14c84d4
finn <finn@penxle.io>
2023-08-25 19:31:25 +0900

    🎨 (sites/penxle.com): 이미지 처리 관련 자잘한 코드 정리

--- sites/penxle.com/src/lib/server/utils/image.ts
+++ sites/penxle.com/src/lib/server/utils/image.ts
@@ -1,44 +1,50 @@
-import { createId } from '$lib/utils';
-import { createS3ObjectKey, s3PutObject } from '../external/aws';
-import { finalizeMedia } from '../external/media';
+import { aws } from '$lib/server/external-api';
+import { createS3ObjectKey } from './id';
 import type { InteractiveTransactionClient } from '../database';
 
-export const persistImage = async (
+export const uploadImage = async (
   db: InteractiveTransactionClient,
   name: string,
   buffer: Buffer,
 ) => {
-  const key = createS3ObjectKey();
-  await s3PutObject(key, name, buffer);
-
-  const {
-    format,
-    fileSize,
-    blobSize,
-    width,
-    height,
-    path,
-    color,
-    placeholder,
-    hash,
-  } = await finalizeMedia(key);
-
-  const { id } = await db.image.create({
-    select: { id: true },
-    data: {
-      id: createId(),
-      name,
-      format,
-      fileSize,
-      blobSize,
-      width,
-      height,
-      path,
-      color,
-      placeholder,
-      hash,
-    },
+  const key = createS3ObjectKey('images');
+  await aws.s3PutObject({
+    bucket: 'penxle-uploads',
+    key,
+    buffer,
+    meta: { name },
   });
 
-  return id;
+  return '';
+  // return await finalizeImage(db, key);
 };
+
+// type FinalizeResult =
+//   | {
+//       name: string;
+//       format: string;
+//       fileSize: number;
+//       blobSize: number;
+//       width: number;
+//       height: number;
+//       path: string;
+//       color: string;
+//       placeholder: string;
+//       hash: string;
+//     }
+//   | { error: string };
+
+// export const finalizeImage = async (
+//   db: InteractiveTransactionClient,
+//   key: string,
+// ) => {
+//   const { id } = await db.image.create({
+//     select: { id: true },
+//     data: {
+//       id: createId(),
+//       ...item,
+//     },
+//   });
+
+//   return id;
+// };
