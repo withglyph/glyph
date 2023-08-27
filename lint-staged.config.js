f1f903f378e636b5f43df6c47d54c2e638395437
finn <finn@penxle.io>
2023-08-27 15:19:09 +0900

    🐛 (github/workflows): eslint 메모리 옵션 빌트인으로 넣음

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   '*': [
-    'eslint --fix',
+    'cross-env NODE_OPTIONS=--max-old-space-size=10240 eslint --fix',
     'prettier --write --ignore-unknown',
     'cspell --no-progress --relative --no-must-find-files',
   ],
