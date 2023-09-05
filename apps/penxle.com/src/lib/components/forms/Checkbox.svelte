ec9e3d3587a53293fa69c6e5e644f2b5e238c0d9
kylie <kylie@penxle.io>
2023-09-05 14:12:35 +0900

    💄 (apps/penxle.com): Checkbox 스타일 수정

--- apps/penxle.com/src/lib/components/forms/Checkbox.svelte
+++ apps/penxle.com/src/lib/components/forms/Checkbox.svelte
@@ -17,16 +17,16 @@
   }
 </script>
 
-<label class="flex select-none items-center gap-2">
+<label class="flex select-none items-center gap-1.5">
   <input
     id={name}
     {name}
-    class="relative checked:after:(square-4 i-lc-check text-white content-empty) square-5 flex appearance-none center border rounded transition checked:(border-none bg-brand-50) enabled:(aria-[invalid]:border-red-50 hover:border-gray-30)"
+    class="relative cursor-pointer bg-gray-30 checked:after:(square-4 i-lc-check text-white content-empty) square-4.5 flex appearance-none center border rounded transition checked:(border-none bg-brand-50) enabled:(aria-[invalid]:border-red-50 hover:border-gray-30)"
     type="checkbox"
     bind:checked
     {...$$restProps}
   />
-  <span class={clsx('text-sm text-gray-50', _class)}>
+  <span class={clsx('text-gray-90 cursor-pointer', _class)}>
     <slot />
   </span>
 </label>
