5aac0aed40b72dec7806e5feaff3eb0c215613c5
kylie <kylie@penxle.io>
2023-09-14 12:44:37 +0900

    💄 (apps/penxle.com): 탭 컴포넌트 추가

---
+++ apps/penxle.com/src/lib/components/Tag.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import clsx from 'clsx';
+
+  export let size: 'sm' | 'lg' = 'lg';
+  export let state: 'default' | 'select' = 'default';
+</script>
+
+<button
+  class={clsx(
+    'px-4 rounded-lg bg-gray-5 transition border border-gray-5 hover:(border-gray-30)',
+    size === 'sm' && 'h-6.5 text-3.25',
+    size === 'lg' && 'py-1 h-8 text-3.75',
+    state === 'select' && 'border-gray-90',
+  )}
+  type="button"
+  on:click
+>
+  <slot />
+</button>
