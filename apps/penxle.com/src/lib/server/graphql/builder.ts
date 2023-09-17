8b3bca6bf3f1c477b8fd25a84327b1c56b3732fd
finn <finn@penxle.io>
2023-09-17 23:29:15 +0900

    🎨 (apps/penxle.com): dev 대신 import.meta.hot 체크

--- apps/penxle.com/src/lib/server/graphql/builder.ts
+++ apps/penxle.com/src/lib/server/graphql/builder.ts
@@ -3,16 +3,14 @@ import PrismaPlugin from '@pothos/plugin-prisma';
 import ScopeAuthPlugin from '@pothos/plugin-scope-auth';
 import SimpleObjectsPlugin from '@pothos/plugin-simple-objects';
 import ValidationPlugin from '@pothos/plugin-validation';
-import { Prisma } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 import { GraphQLDateTime, GraphQLJSON } from 'graphql-scalars';
-import { dev } from '$app/environment';
 import { PermissionDeniedError } from '$lib/errors';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
-import type { PrismaClient } from '@prisma/client';
 import type { AuthContext, Context } from '../context';
 
-if (dev) {
-  // 개발환경에서만 강제로 순환 참조 걸어서 schema/**/* 리로드할 때 HMR이 builder까지 리로드하도록 함
+// 개발환경에서만 강제로 순환 참조 걸어서 ./schemas/**/* 리로드할 때 HMR이 builder까지 리로드하도록 함
+if (import.meta.hot) {
   import('./schemas');
 }
 
