a2df7aca70a0a7342b8001e081aa13fac0e26314
kylie <kylie@penxle.io>
2023-09-19 14:36:52 +0900

    💄 (apps/penxle.com): 모달 모바일에서는 바텀팝업으로 변경

--- apps/penxle.com/src/lib/components/Modal.svelte
+++ apps/penxle.com/src/lib/components/Modal.svelte
@@ -37,87 +37,90 @@
 
     <div
       class={clsx(
-        'pointer-events-none absolute inset-0 flex center',
-        size === 'sm' && 'p-5',
-        size === 'md' && 'p-6',
-        size === 'lg' && 'p-9',
+        'pointer-events-none absolute inset-0 flex',
+        size === 'sm' && 'p-5 center',
+        size === 'md' && 'w-full justify-center items-end sm:(p-6 center)',
+        size === 'lg' && 'w-full justify-center items-end sm:(p-9 center)',
       )}
     >
       <div
         class={clsx(
-          'pointer-events-auto relative max-h-full w-full flex flex-col rounded-2xl bg-white shadow-xl',
-          size === 'sm' && 'max-w-92 p-4 pt-8',
-          size === 'md' && 'max-w-85.5 sm:max-w-107.5 p-6 pb-5.5',
-          size === 'lg' && 'max-w-187n p-7 pt-5',
+          'pointer-events-auto relative max-h-full w-full flex flex-col center rounded-2xl bg-white shadow-xl',
+          size === 'sm' && 'p-4 pt-8 max-w-92',
+          size === 'md' &&
+            'p-6 pb-5.5 rounded-b-none sm:(max-w-107.5 rounded-2xl)',
+          size === 'lg' && 'p-7 pt-5 rounded-b-none sm:(max-w-187 rounded-2xl)',
         )}
         in:fly={{ y: '10%', duration: 150 }}
         out:fade={{ duration: 150 }}
       >
-        {#if $$slots.title}
-          <div
-            class={clsx(
-              'flex justify-between mb-6',
-              size === 'sm' && 'justify-center!',
-            )}
-          >
+        <div class={clsx('flex flex-col w-full', size === 'sm' && 'max-w-92')}>
+          {#if $$slots.title}
             <div
               class={clsx(
-                'flex flex-col break-all',
-                size !== 'sm' && 'pr-9 mt-4',
+                'flex justify-between mb-6',
+                size === 'sm' && 'justify-center!',
               )}
             >
-              <h3
+              <div
                 class={clsx(
-                  'text-lg font-bold break-keep',
-                  size === 'md' && 'text-xl',
+                  'flex flex-col break-all',
+                  size !== 'sm' && 'pr-9 mt-4',
                 )}
               >
-                <slot name="title" />
-              </h3>
-              {#if $$slots.subtitle}
-                <div
+                <h3
                   class={clsx(
-                    'flex justify-between mt-2 text-gray-50',
-                    size === 'sm' && 'justify-center!',
+                    'text-lg font-bold break-keep',
+                    size === 'md' && 'text-xl',
                   )}
                 >
-                  <h3 class="text-3.75 font-bold">
-                    <slot name="subtitle" />
-                  </h3>
-                </div>
+                  <slot name="title" />
+                </h3>
+                {#if $$slots.subtitle}
+                  <div
+                    class={clsx(
+                      'flex justify-between mt-2 text-gray-50',
+                      size === 'sm' && 'justify-center!',
+                    )}
+                  >
+                    <h3 class="text-3.75 font-bold">
+                      <slot name="subtitle" />
+                    </h3>
+                  </div>
+                {/if}
+              </div>
+              {#if size !== 'sm'}
+                <button
+                  class="square-7 absolute right-6 flex center rounded text-gray-50 transition hover:(bg-gray-10 text-gray-60)"
+                  type="button"
+                  on:click={() => (open = false)}
+                >
+                  <span class="i-lc-x square-6" />
+                </button>
               {/if}
             </div>
-            {#if size !== 'sm'}
-              <button
-                class="square-7 absolute right-6 flex center rounded text-gray-50 transition hover:(bg-gray-10 text-gray-60)"
-                type="button"
-                on:click={() => (open = false)}
-              >
-                <span class="i-lc-x square-6" />
-              </button>
-            {/if}
-          </div>
-        {:else}
-          <button
-            class="absolute right-4 top-4 z-1 square-7 flex center rounded text-gray-50 transition hover:(bg-gray-10 text-gray-60)"
-            type="button"
-            on:click={() => (open = false)}
-          >
-            <span class="i-lc-x square-5" />
-          </button>
-        {/if}
+          {:else}
+            <button
+              class="absolute right-4 top-4 z-1 square-7 flex center rounded text-gray-50 transition hover:(bg-gray-10 text-gray-60)"
+              type="button"
+              on:click={() => (open = false)}
+            >
+              <span class="i-lc-x square-5" />
+            </button>
+          {/if}
 
-        <div class="overflow-x-hidden">
-          <slot />
+          <div class="overflow-x-hidden">
+            <slot />
+          </div>
+
+          {#if $$slots.action}
+            <div
+              class={clsx('flex items-center mt-4xl', size === 'md' && 'mt-4!')}
+            >
+              <slot name="action" />
+            </div>
+          {/if}
         </div>
-
-        {#if $$slots.action}
-          <div
-            class={clsx('flex items-center mt-4xl', size === 'md' && 'mt-4!')}
-          >
-            <slot name="action" />
-          </div>
-        {/if}
       </div>
     </div>
   </div>
