eefe54bc59acff7a55e3173e329525eb3f130fd8
kylie <kylie@penxle.io>
2023-09-19 16:59:36 +0900

    💄 (apps/penxle.com): Switch 스타일 수정

--- apps/penxle.com/src/lib/components/forms/Switch.svelte
+++ apps/penxle.com/src/lib/components/forms/Switch.svelte
@@ -2,16 +2,21 @@
   import { clsx } from 'clsx';
   import type { HTMLInputAttributes } from 'svelte/elements';
 
+  type $$Props = {
+    size: 'sm' | 'lg';
+  } & HTMLInputAttributes;
+
   export let checked: HTMLInputAttributes['checked'] = false;
   let _class: HTMLInputAttributes['class'] = undefined;
   export { _class as class };
-
-  type $$Props = HTMLInputAttributes;
+  export let size: 'sm' | 'lg' = 'lg';
 </script>
 
 <input
   class={clsx(
-    'relative flex w-10 aspect-2/1 p-1 rounded-full transition appearance-none bg-gray-20 justify-start checked:(bg-brand-50 justify-end) after:(rounded-full content-empty bg-white h-full aspect-1/1)',
+    'relative flex items-center aspect-2/1 p-1 rounded-full transition appearance-none bg-gray-20 justify-start cursor-pointer checked:(bg-green-50 justify-end) after:(rounded-full content-empty bg-white aspect-1/1)',
+    size === 'sm' && 'w-7.5 h-4 rounded-6 after:h-150%',
+    size === 'lg' && 'w-11 h-6 rounded-9 after:h-full',
     _class,
   )}
   type="checkbox"
