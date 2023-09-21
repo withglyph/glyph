563dcd129eab294670734d66b6cfa75902bad96c
kylie <kylie@penxle.io>
2023-09-21 16:12:10 +0900

    ✨ (apps/penxle.com): 데스크탑에서 모바일용 메뉴 페이지 접속 시 나의펜슬로 이동

---
+++ apps/penxle.com/src/routes/(default)/me/(index)/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+
+  onMount(() => {
+    if (window.innerWidth >= 800) {
+      goto('/me/drawer');
+    }
+  });
+</script>
