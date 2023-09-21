47bf8ee200c92397d14fcb327c4ff6c34de9dd7e
kylie <kylie@penxle.io>
2023-09-21 15:25:14 +0900

    ✨ (apps/penxle.com): 기존 비밀번호 존재하는지 확인

--- apps/penxle.com/src/routes/(default)/me/(index)/account/UpdatePasswordModal.svelte
+++ apps/penxle.com/src/routes/(default)/me/(index)/account/UpdatePasswordModal.svelte
@@ -1,12 +1,31 @@
 <script lang="ts">
-  import { graphql } from '$glitch';
+  import { fragment, graphql } from '$glitch';
   import { Button, Modal } from '$lib/components';
   import { FormField, PasswordInput } from '$lib/components/forms';
   import { createMutationForm } from '$lib/form';
   import { UpdatePasswordInputSchema } from '$lib/validations';
+  import type { AccountPage_UpdatePasswordModal_user } from '$glitch';
 
   export let open = false;
   let isSuccess = false;
+
+  let _user: AccountPage_UpdatePasswordModal_user;
+  export { _user as $user };
+
+  $: user = fragment(
+    _user,
+    graphql(`
+      fragment AccountPage_UpdatePasswordModal_user on User {
+        id
+
+        password {
+          id
+          createdAt
+        }
+      }
+    `),
+  );
+
   const { form } = createMutationForm({
     mutation: graphql(`
       mutation MeAccountPage_UpdatePassword_Mutation(
@@ -28,17 +47,21 @@
 <Modal size="md" bind:open>
   <svelte:fragment slot="title">새로운 비밀번호를 설정해주세요</svelte:fragment>
   <svelte:fragment slot="subtitle">
-    비밀번호를 재설정하면 현재 사용중인 세션을 제외한 모든 세션에서
-    로그아웃되어요.
+    {#if $user.password?.id}
+      비밀번호를 재설정하면 현재 사용중인 세션을 제외한 모든 세션에서
+      로그아웃되어요.
+    {/if}
   </svelte:fragment>
 
   <form class="space-y-4" use:form>
-    <FormField name="oldPassword" label="기존 비밀번호">
-      <PasswordInput
-        class="w-full font-bold"
-        placeholder="기존 비밀번호 입력"
-      />
-    </FormField>
+    {#if $user.password?.id}
+      <FormField name="oldPassword" label="기존 비밀번호">
+        <PasswordInput
+          class="w-full font-bold"
+          placeholder="기존 비밀번호 입력"
+        />
+      </FormField>
+    {/if}
 
     <FormField name="newPassword" label="새 비밀번호">
       <PasswordInput class="w-full font-bold" placeholder="새 비밀번호 입력" />
