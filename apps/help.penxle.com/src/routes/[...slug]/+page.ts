8020282927689ce8c2c84836ac6e0180528032be
finn <finn@penxle.io>
2023-09-22 17:57:14 +0900

    도움센터 마크다운 포맷 아주 살짝 수정

--- apps/help.penxle.com/src/routes/[...slug]/+page.ts
+++ apps/help.penxle.com/src/routes/[...slug]/+page.ts
@@ -1,9 +1,9 @@
 import { error } from '@sveltejs/kit';
 import type { ComponentType } from 'svelte';
 import type { PageMeta } from '$lib/types';
-import type { EntryGenerator, PageLoad } from './$types.js';
+import type { EntryGenerator, PageLoad } from './$types';
 
-export const load = (async ({ params }) => {
+export const load: PageLoad = async ({ params }) => {
   try {
     const post = (await import(`$pages/${params.slug}.md`)) as {
       default: ComponentType;
@@ -17,13 +17,13 @@ export const load = (async ({ params }) => {
   } catch {
     throw error(404);
   }
-}) satisfies PageLoad;
+};
 
-export const entries = (() => {
+export const entries: EntryGenerator = () => {
   const paths = Object.keys(import.meta.glob('$pages/**/*.md'));
   const slugs = paths.map((path) =>
     path.replaceAll(/(^\/src\/pages\/|\.md$)/g, ''),
   );
 
   return slugs.map((slug) => ({ slug }));
-}) satisfies EntryGenerator;
+};
