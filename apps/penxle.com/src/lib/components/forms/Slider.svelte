724f335b4c94915316e8a65398c261f7cf6331c1
finn <finn@penxle.io>
2023-10-07 11:28:12 +0900

    range slider 컴포넌트 구현

---
+++ apps/penxle.com/src/lib/components/forms/Slider.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { clsx } from 'clsx';
+  import { getFormContext } from '$lib/form';
+  import { clamp } from '$lib/utils';
+  import FormValidationMessage from './FormValidationMessage.svelte';
+  import type { PointerEventHandler } from 'svelte/elements';
+
+  export let min = 0;
+  export let max = 1;
+  export let value = min;
+  export let name: string | undefined = undefined;
+  let _class: string | undefined = undefined;
+  export { _class as class };
+
+  const { field } = getFormContext();
+
+  $: p = `${((value - min) / (max - min)) * 100}%`;
+
+  const handler: PointerEventHandler<HTMLElement> = (e) => {
+    if (!e.currentTarget.parentElement) {
+      return;
+    }
+
+    const { left: parentLeft, width: parentWidth } = e.currentTarget.parentElement.getBoundingClientRect();
+    const { clientX: pointerLeft } = e;
+    value = clamp(((pointerLeft - parentLeft) / parentWidth) * (max - min) + min, min, max);
+  };
+
+  if (field) {
+    name = field.name;
+  }
+</script>
+
+<div class={clsx('relative flex items-center h-4', _class)}>
+  <div class="w-full h-2 bg-gray-20 rounded-full overflow-hidden" on:pointerdown={handler}>
+    <div style:width={p} class="bg-black h-full" />
+  </div>
+  <div class="absolute h-2 w-full pointer-events-none">
+    <button
+      style:left={p}
+      class="absolute square-4 bg-gray-50 rounded-full top-1/2 -translate-y-1/2 -translate-x-1/2 pointer-events-auto touch-none"
+      type="button"
+      on:dragstart|preventDefault
+      on:pointerdown={(e) => e.currentTarget.setPointerCapture(e.pointerId)}
+      on:pointermove|preventDefault={(e) => {
+        if (e.currentTarget.hasPointerCapture(e.pointerId)) {
+          handler(e);
+        }
+      }}
+    />
+  </div>
+</div>
+
+{#if name}
+  <FormValidationMessage for={name} let:message>
+    <div class="flex items-center gap-1.5 mt-1.5 text-xs text-red-50">
+      <span class="i-lc-alert-triangle" />
+      {message}
+    </div>
+  </FormValidationMessage>
+{/if}
--- apps/penxle.com/src/lib/components/forms/index.ts
+++ apps/penxle.com/src/lib/components/forms/index.ts
@@ -1,5 +1,6 @@
 export { default as Checkbox } from './Checkbox.svelte';
 export { default as FormField } from './FormField.svelte';
 export { default as PasswordInput } from './PasswordInput.svelte';
+export { default as Slider } from './Slider.svelte';
 export { default as Switch } from './Switch.svelte';
 export { default as TextInput } from './TextInput.svelte';
