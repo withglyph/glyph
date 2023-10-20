cb70ce56384e35fefc05cd7027ab7151ee1cb668
finn <finn@penxle.io>
2023-10-20 13:04:26 +0900

    useCache 유틸리티 함수 구현

--- apps/penxle.com/src/lib/server/cache.ts
+++ apps/penxle.com/src/lib/server/cache.ts
@@ -2,3 +2,16 @@ import Redis from 'ioredis';
 import { PRIVATE_REDIS_URL } from '$env/static/private';
 
 export const redis = new Redis(PRIVATE_REDIS_URL);
+
+export const useCache = async <T>(key: string, fn: () => Promise<T>, ttl = 60 * 5) => {
+  const r = await redis.get(key);
+  if (r) {
+    return JSON.parse(r);
+  }
+
+  const value = await fn();
+  // spell-checker:disable-next-line
+  await redis.setex(key, ttl, JSON.stringify(value));
+
+  return value;
+};
