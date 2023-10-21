21c164acc0754b9e547ecac1e079c1e2b0c4cc08
kylie <kylie@penxle.io>
2023-10-21 16:20:21 +0900

    table 컴포넌트 추가

---
+++ apps/penxle.com/src/lib/components/table/TableHead.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import clsx from 'clsx';
+
+  let _class: string | undefined = undefined;
+  export { _class as class };
+</script>
+
+<th
+  class={clsx(
+    'border-y border-secondary py-2 first-of-type:(border-l rounded-l-lg pl-4) last-of-type:(border-r rounded-r-lg pr-4)',
+    _class,
+  )}
+  scope="col"
+>
+  <slot />
+</th>
