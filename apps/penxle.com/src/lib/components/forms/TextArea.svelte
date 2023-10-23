a6c4175da0dfc90082935b2ec91828d248b37dcc
kylie <kylie@penxle.io>
2023-10-23 12:15:34 +0900

    TextArea 컴포넌트 추가

---
+++ apps/penxle.com/src/lib/components/forms/TextArea.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { clsx } from 'clsx';
+  import { getFormContext } from '$lib/form';
+  import type { HTMLTextareaAttributes } from 'svelte/elements';
+
+  export let name: HTMLTextareaAttributes['name'] = undefined;
+  export let value: HTMLTextareaAttributes['value'] = undefined;
+  let _class: HTMLTextareaAttributes['class'] = undefined;
+  export { _class as class };
+
+  type $$Props = HTMLTextareaAttributes;
+
+  const { field } = getFormContext();
+
+  if (field) {
+    name = field.name;
+  }
+</script>
+
+<div class="relative flex items-center">
+  <textarea
+    id={name}
+    {name}
+    class={clsx('body-15-b resize-none', _class, 'right-icon' in $$slots && 'mb-4')}
+    on:input
+    bind:value
+    {...$$restProps}
+  />
+  {#if 'right-icon' in $$slots}
+    <div class="absolute right-0 bottom-0 flex center">
+      <slot name="right-icon" />
+    </div>
+  {/if}
+</div>
