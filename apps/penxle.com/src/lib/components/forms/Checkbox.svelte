b8aeb0995c00be8e6bbf02345ce2774dcebb0551
kylie <kylie@penxle.io>
2023-10-28 19:47:35 +0900

    결제금액 라디오버튼 클릭 시 충전안되는 문제 해결

--- apps/penxle.com/src/lib/components/forms/Checkbox.svelte
+++ apps/penxle.com/src/lib/components/forms/Checkbox.svelte
@@ -15,25 +15,27 @@
   }
 </script>
 
-<label class={clsx('flex items-center gap-1.5 select-none', _class)}>
-  <input
-    id={name}
-    {name}
-    class="relative flex center square-4.5 border border-gray-30 rounded cursor-pointer appearance-none transition checked:(border-none bg-gray-90) checked:after:(square-4 i-lc-check text-white content-empty) enabled:(aria-[invalid]:border-red-50 hover:border-gray-40!)"
-    type="checkbox"
-    on:change
-    bind:checked
-    {...$$restProps}
-  />
-  <span class="text-gray-90 cursor-pointer">
-    <slot />
-  </span>
-</label>
-{#if name}
-  <FormValidationMessage for={name} let:message>
-    <div class="flex items-center gap-1.5 mt-1.5 text-xs text-red-50">
-      <span class="i-lc-alert-triangle" />
-      {message}
-    </div>
-  </FormValidationMessage>
-{/if}
+<div>
+  <label class={clsx('flex items-center gap-1.5 select-none', _class)}>
+    <input
+      id={name}
+      {name}
+      class="relative flex center square-4.5 border border-gray-30 rounded cursor-pointer appearance-none transition checked:(border-none bg-gray-90) checked:after:(square-4 i-lc-check text-white content-empty) enabled:(aria-[invalid]:border-red-50 hover:border-gray-40!)"
+      type="checkbox"
+      on:change
+      bind:checked
+      {...$$restProps}
+    />
+    <span class="text-gray-90 cursor-pointer">
+      <slot />
+    </span>
+  </label>
+  {#if name}
+    <FormValidationMessage for={name} let:message>
+      <div class="flex items-center gap-1.5 mt-1.5 text-xs text-red-50">
+        <span class="i-lc-alert-triangle" />
+        {message}
+      </div>
+    </FormValidationMessage>
+  {/if}
+</div>
