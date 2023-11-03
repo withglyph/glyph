f53145effd14e63ccdb7e53fb4e54c36f73b9178
kylie <kylie@penxle.io>
2023-11-03 12:09:14 +0900

    포스트 조회수 gql 연결

--- apps/penxle.com/src/lib/utils/number.ts
+++ apps/penxle.com/src/lib/utils/number.ts
@@ -7,3 +7,19 @@ export const clamp = (value: number, min: number, max: number) => {
 export const comma = (value: number) => {
   return nf.format(value);
 };
+
+export const humanizeNumber = (value: number) => {
+  const lookup = [
+    { value: 1, symbol: '' },
+    { value: 1e3, symbol: '천' },
+    { value: 1e4, symbol: '만' },
+    { value: 1e8, symbol: '억' },
+  ];
+
+  const rx = /\.0+$|(\.\d*[1-9])0+$/;
+  const item = [...lookup].reverse().find(function (item) {
+    return value >= item.value;
+  });
+
+  return item ? (value / item.value).toFixed(1).replace(rx, '$1') + item.symbol : '0';
+};
