cdb1a05cd3588cfe1313b08e9cc633778b39e50f
finn <finn@penxle.io>
2023-11-04 13:59:10 +0900

    팁탭 에디터 출력 미리보기 추가 (개발 환경 전용)

--- apps/penxle.com/src/lib/tiptap/components/TiptapEditor.svelte
+++ apps/penxle.com/src/lib/tiptap/components/TiptapEditor.svelte
@@ -2,6 +2,7 @@
   import { Editor } from '@tiptap/core';
   import { clsx } from 'clsx';
   import { onMount } from 'svelte';
+  import { dev } from '$app/environment';
   import { extensions } from '$lib/tiptap';
   import type { JSONContent } from '@tiptap/core';
 
@@ -49,3 +50,11 @@
     </div>
   {/if}
 </div>
+
+{#if dev}
+  <div
+    class="fixed top-0 right-0 z-100 p-4 bg-gray-5 w-20vw font-mono whitespace-pre-wrap break-all text-xs h-screen overflow-scroll"
+  >
+    {JSON.stringify(content, null, 2)}
+  </div>
+{/if}
