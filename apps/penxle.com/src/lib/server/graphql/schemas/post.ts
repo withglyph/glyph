5c7fc1962ebd62ed96bcf558c65fda0339ffde5a
Bae Junehyeon <finn@penxle.io>
2023-11-08 18:36:54 +0900

    일부 mutation에 optimistic update 구현함 (#301)

--- apps/penxle.com/src/lib/server/graphql/schemas/post.ts
+++ apps/penxle.com/src/lib/server/graphql/schemas/post.ts
@@ -559,9 +559,18 @@ builder.mutationFields((t) => ({
         where: { id: input.postId },
         data: {
           likes: {
-            create: {
-              id: createId(),
-              userId: context.session.userId,
+            upsert: {
+              where: {
+                postId_userId: {
+                  postId: input.postId,
+                  userId: context.session.userId,
+                },
+              },
+              create: {
+                id: createId(),
+                userId: context.session.userId,
+              },
+              update: {},
             },
           },
         },
@@ -578,11 +587,8 @@ builder.mutationFields((t) => ({
         where: { id: input.postId },
         data: {
           likes: {
-            delete: {
-              postId_userId: {
-                postId: input.postId,
-                userId: context.session.userId,
-              },
+            deleteMany: {
+              userId: context.session.userId,
             },
           },
         },
