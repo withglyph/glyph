a73b6a09d77b237e61ef478145de89ceb44e6c86
Seohyun Roh <kylie@penxle.io>
2023-11-08 18:39:28 +0900

    이모지 반응 gql 연결 (#323)

--- apps/penxle.com/src/lib/emoji/Emoji.svelte
+++ apps/penxle.com/src/lib/emoji/Emoji.svelte
@@ -1,7 +1,39 @@
 <script lang="ts">
+  import { graphql } from '$glitch';
   import { emojiData } from '.';
 
+  export let postId: string;
   export let emoji: string;
+  export let mine: boolean;
+
+  const deletePostReaction = graphql(`
+    mutation Emoji_DeletePostReaction_Mutation($input: DeletePostReactionInput!) {
+      deletePostReaction(input: $input) {
+        id
+
+        reactions {
+          id
+          emoji
+          mine
+        }
+      }
+    }
+  `);
 </script>
 
-{emojiData.emojis[emoji].skins[0].native}
+{#if mine}
+  <button
+    class="bg-primary"
+    type="button"
+    on:click={async () => {
+      await deletePostReaction({
+        postId,
+        emoji,
+      });
+    }}
+  >
+    {emojiData.emojis[emoji].skins[0].native}
+  </button>
+{:else}
+  <span>{emojiData.emojis[emoji].skins[0].native}</span>
+{/if}
--- apps/penxle.com/src/lib/emoji/EmojiPicker.svelte
+++ apps/penxle.com/src/lib/emoji/EmojiPicker.svelte
@@ -4,18 +4,62 @@
   import { init, Picker } from 'emoji-mart';
   import { onMount, tick } from 'svelte';
   import { afterNavigate } from '$app/navigation';
+  import { fragment, graphql } from '$glitch';
   import { portal } from '$lib/svelte/actions';
-  import { emojiData as data } from '.';
+  import LoginRequireModal from '../../routes/(default)/LoginRequireModal.svelte';
   import i18n from './i18n.json';
+  import { emojiData as data } from './index';
   import type { Emoji } from '@emoji-mart/data';
+  import type { EmojiPicker_query } from '$glitch';
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   let picker: any;
   let open = false;
-  let exceptEmojis: string[] = [];
+  let loginRequireOpen = false;
 
   let targetEl: HTMLButtonElement;
   let pickerEl: HTMLDivElement;
+
+  let _query: EmojiPicker_query;
+  export { _query as $query };
+
+  $: query = fragment(
+    _query,
+    graphql(`
+      fragment EmojiPicker_query on Query {
+        me {
+          id
+        }
+
+        post(permalink: $permalink) {
+          id
+
+          reactions {
+            id
+            emoji
+            mine
+          }
+        }
+      }
+    `),
+  );
+
+  let exceptEmojis: (string | undefined)[] = [];
+  $: exceptEmojis = $query.post.reactions.filter((reaction) => reaction.mine).map((reaction) => reaction.emoji);
+
+  const createPostReaction = graphql(`
+    mutation EmojiPicker_CreatePostReaction_Mutation($input: CreatePostReactionInput!) {
+      createPostReaction(input: $input) {
+        id
+
+        reactions {
+          id
+          emoji
+          mine
+        }
+      }
+    }
+  `);
 
   const update = async () => {
     await tick();
@@ -47,10 +91,17 @@
       skinTonePosition: 'none',
       locale: 'kr',
       i18n,
-      onEmojiSelect: (emoji: Emoji) => {
-        exceptEmojis.push(emoji.id);
-        exceptEmojis = exceptEmojis;
+      onEmojiSelect: async (emoji: Emoji) => {
+        if (!$query.me) {
+          loginRequireOpen = true;
+          return;
+        }
         open = false;
+
+        await createPostReaction({
+          postId: $query.post.id,
+          emoji: emoji.id,
+        });
       },
       exceptEmojis,
       perLine: 8,
@@ -90,6 +141,8 @@
 {/if}
 
 <div bind:this={pickerEl} class={clsx('z-50 absolute h-100', !open && 'hidden')} />
+
+<LoginRequireModal bind:open={loginRequireOpen} />
 
 <style>
   :global(em-emoji-picker) {
