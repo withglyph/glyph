6e1063b32986534018fd1e26d163c6a14aa03060
Jiyu Park <yuki@penxle.io>
2023-11-08 18:38:27 +0900

    이모지 출력 컴포넌트 (#308)
    `<Emoji emoji={emojiId}>` 식으로 사용하실 수 있어요

---
+++ apps/penxle.com/src/lib/emoji/data.ts
@@ -0,0 +1,4 @@
+import data from './data.json';
+import type { EmojiMartData } from '@emoji-mart/data';
+
+export const emojiData: EmojiMartData = data;
--- apps/penxle.com/src/lib/emoji/index.ts
+++ apps/penxle.com/src/lib/emoji/index.ts
@@ -1,1 +1,3 @@
+export { emojiData } from './data';
+export { default as Emoji } from './Emoji.svelte';
 export { default as EmojiPicker } from './EmojiPicker.svelte';
