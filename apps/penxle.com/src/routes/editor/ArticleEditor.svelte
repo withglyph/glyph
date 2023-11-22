eca2ef5b63f08c302dc0be5aea166e5584aa5bea
Bae Junehyeon <337728+devunt@users.noreply.github.com>
2023-11-22 03:47:44 +0000

    에디터 하단 스크롤 마진 100px로 조정 (#465)

--- apps/penxle.com/src/routes/editor/ArticleEditor.svelte
+++ apps/penxle.com/src/routes/editor/ArticleEditor.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="mx-auto w-full max-w-230 flex grow">
-  <TiptapEditor class="my-12 max-w-full grow whitespace-pre-wrap" bind:editor bind:content />
+  <TiptapEditor class="mt-12 mb-100px max-w-full grow whitespace-pre-wrap" bind:editor bind:content />
 </div>
 
 {#if editor}
