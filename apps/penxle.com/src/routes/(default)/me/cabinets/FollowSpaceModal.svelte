47c06bb2824b904bf036bf8c16764ad4dce45cb2
Seohyun Roh <kylie@penxle.io>
2023-11-29 21:04:37 +0900

    관심 스페이스 목록 모달 버그 수정
    - 인풋 창에서 엔터 누를 시 모달 닫히지 않도록 수정
    - input value를 trim 한 후 필터링

--- apps/penxle.com/src/routes/(default)/me/cabinets/FollowSpaceModal.svelte
+++ apps/penxle.com/src/routes/(default)/me/cabinets/FollowSpaceModal.svelte
@@ -10,6 +10,7 @@
   export let open = false;
 
   let followedSpaces: typeof $user.followedSpaces;
+  let query = '';
 
   const setSpaces = () => {
     followedSpaces = $user.followedSpaces;
@@ -57,22 +58,16 @@
       }
     }
   `);
-
-  const onInput = (event: Event) => {
-    const { value } = event.currentTarget as HTMLInputElement;
-
-    followedSpaces = $user.followedSpaces.filter((space) => space.name.includes(value));
-  };
 </script>
 
 <Modal size="md" bind:open>
   <svelte:fragment slot="title">관심 스페이스</svelte:fragment>
 
-  <form class="relative h-11.5 w-full mb-4">
+  <form class="relative h-11.5 w-full mb-4" on:submit|preventDefault>
     <input
       class="rounded-2.5 h-11.5 w-full bg-primary py-1.75 pr-3.5 pl-11 w-full border border-bg-primary transition focus-within:border-tertiary!"
       type="text"
-      on:input={onInput}
+      on:input={(e) => (query = e.currentTarget.value.trim())}
     />
     <div class="absolute inset-y-0 left-3.5 flex center text-secondary h-100%">
       <span class="i-lc-search square-5 transition" />
@@ -80,7 +75,7 @@
   </form>
 
   <ul class="space-y-4">
-    {#each followedSpaces as space (space.id)}
+    {#each followedSpaces.filter((space) => space.name.includes(query)) as space (space.id)}
       <li class="flex items-center justify-between">
         <div class="flex gap-2 items-center truncate mr-2">
           <Image class="square-10.5 rounded-lg grow-0 shrink-0" $image={space.icon} />
