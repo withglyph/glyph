d4464558118d4776bd9b0a0ddb5f184425255561
Muhun Kim (김 무훈) <5278201+x86chi@users.noreply.github.com>
2023-11-29 09:30:31 +0000

    slug 참조를 graphQL query 에서 store 참조로 바꾸기 (#524)

--- apps/penxle.com/src/routes/(default)/[space]/dashboard/posts/+layout.svelte
+++ apps/penxle.com/src/routes/(default)/[space]/dashboard/posts/+layout.svelte
@@ -1,28 +1,19 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { graphql } from '$glitch';
+  import { page } from '$app/stores';
   import { TabHead, TabHeadItem } from '$lib/components/tab';
   import { pageSubTitle } from '$lib/stores';
 
   onMount(async () => {
     pageSubTitle.set('포스트 관리');
   });
-
-  $: query = graphql(`
-    query SpaceSettingPostsLayout_Query($slug: String!) {
-      space(slug: $slug) {
-        id
-        slug
-      }
-    }
-  `);
 </script>
 
 <section class="flex flex-col gap-4 px-6 py-8 rounded-xl bg-white border border-secondary">
   <h1 class="title-20-eb">포스트 관리</h1>
   <div>
     <TabHead class="border-none" variant="secondary">
-      <TabHeadItem id={0} href="/{$query.space.slug}/dashboard/posts">포스트</TabHeadItem>
+      <TabHeadItem id={0} href="/{$page.params.slug}/dashboard/posts">포스트</TabHeadItem>
       <!-- <TabHeadItem id={1} href="/{$query.space.slug}/dashboard/posts/collections">
         컬렉션
       </TabHeadItem> -->
