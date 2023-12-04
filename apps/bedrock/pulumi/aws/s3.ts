2a81617ff6766c21724fa83e02e56b6c101e3c06
Bae Junehyeon <337728+devunt@users.noreply.github.com>
2023-12-04 03:48:22 +0000

    더이상 쓰지 않는 s3 버킷 제거 (#589)

--- apps/bedrock/pulumi/aws/s3.ts
+++ apps/bedrock/pulumi/aws/s3.ts
@@ -1,30 +1,6 @@
 import * as aws from '@pulumi/aws';
 import * as pulumi from '@pulumi/pulumi';
 
-const artifacts = new aws.s3.Bucket('artifacts', {
-  bucket: 'penxle-artifacts',
-
-  lifecycleRules: [
-    { enabled: true, prefix: 'actions-cache/', expiration: { days: 7 } },
-    { enabled: true, prefix: 'lambda/', expiration: { days: 7 } },
-  ],
-});
-
-new aws.s3.BucketPolicy('artifacts', {
-  bucket: artifacts.bucket,
-  policy: {
-    Version: '2012-10-17',
-    Statement: [
-      {
-        Effect: 'Allow',
-        Principal: { Service: 'billingreports.amazonaws.com' },
-        Action: ['s3:GetBucketAcl', 's3:GetBucketPolicy', 's3:PutObject'],
-        Resource: [artifacts.arn, pulumi.interpolate`${artifacts.arn}/*`],
-      },
-    ],
-  },
-});
-
 const data = new aws.s3.Bucket('data', {
   bucket: 'penxle-data',
 });
@@ -59,15 +35,13 @@ const uploads = new aws.s3.Bucket('uploads', {
   ],
 });
 
-export const buckets = { artifacts, caches, data, uploads };
+export const buckets = { caches, data, uploads };
 
 export const outputs = {
-  AWS_S3_BUCKET_ARTIFACTS_BUCKET: artifacts.bucket,
   AWS_S3_BUCKET_CACHES_BUCKET: caches.bucket,
   AWS_S3_BUCKET_DATA_BUCKET: data.bucket,
   AWS_S3_BUCKET_UPLOADS_BUCKET: uploads.bucket,
 
-  AWS_S3_BUCKET_ARTIFACTS_ARN: artifacts.arn,
   AWS_S3_BUCKET_CACHES_ARN: caches.arn,
   AWS_S3_BUCKET_DATA_ARN: data.arn,
   AWS_S3_BUCKET_UPLOADS_ARN: uploads.arn,
