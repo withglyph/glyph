9dc7644721de4da8a2f290010c96791cb568b412
Bae Junehyeon <337728+devunt@users.noreply.github.com>
2023-12-08 05:11:49 +0000

    자잘한 푸터 정보 갱신 (#622)
    - 회사 주소 업데이트
    - 통신판매중개자 문구 삭제
    - 공식 스페이스 주소 `blog` 에서 `penxle` 로 변경

--- apps/penxle.com/src/routes/(default)/Footer.svelte
+++ apps/penxle.com/src/routes/(default)/Footer.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import { Link } from '@penxle/ui';
   import Wordmark from '$assets/branding/wordmark.svg?component';
-
-  let showDisclosure = false;
 </script>
 
 <footer class="border-t border-secondary px-4 py-10 text-xs tracking-wider text-secondary flex sm:px-7.5">
@@ -30,20 +28,11 @@
         </div>
 
         <div class="flex flex-wrap items-center">
-          <div>서울특별시 강남구 삼성로 212 (대치동, 은마아파트) 1동 908호</div>
-          <!-- <div>주소: 서울특별시 강남구 강남대로100길 14, 6층</div> -->
+          <div>주소: 서울특별시 강남구 강남대로100길 14, 6층</div>
           <div class="h-2 border-l border-gray-50 mx-1" />
           <div>전화: 070-7580-5205</div>
         </div>
       </section>
-
-      {#if showDisclosure}
-        <section class="font-light text-gray-30 mt-6">
-          (주)펜슬컴퍼니는 통신판매중개자이며, 통신판매의 당사자가 아닙니다.
-          <br />
-          콘텐츠 및 거래에 관한 의무와 책임은 콘텐츠 소유자 및 거래 당사자 각자에게 있습니다.
-        </section>
-      {/if}
 
       <section class="flex flex-wrap items-center mt-6">
         <Link class="hover:text-secondary" href="https://help.penxle.com/legal/terms">이용약관</Link>
@@ -52,14 +41,10 @@
           개인정보처리방침
         </Link>
         <div class="h-2 border-l border-gray-50 mx-1" />
-        <button class="hover:text-secondary" type="button" on:click={() => (showDisclosure = !showDisclosure)}>
-          통신판매중개 고지
-        </button>
-        <div class="h-2 border-l border-gray-50 mx-1" />
         <div class="flex flex-wrap items-center">
           <Link class="hover:text-secondary" href="https://help.penxle.com">도움 센터</Link>
           <div class="h-2 border-l border-gray-50 mx-1" />
-          <Link class="hover:text-secondary" href="/blog">펜슬 소식</Link>
+          <Link class="hover:text-secondary" href="/penxle">펜슬 소식</Link>
           <div class="h-2 border-l border-gray-50 mx-1" />
           <Link class="hover:text-secondary" href="https://penxle.io">펜슬컴퍼니 소개</Link>
         </div>
