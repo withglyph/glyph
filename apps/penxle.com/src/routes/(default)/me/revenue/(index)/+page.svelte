ff9e6e5c70dbe969a9f444cac576f7091349830a
Jiwon Choi <1890mah@gmail.com>
2023-12-12 03:51:00 +0900

    수익금 내역 페이지 퍼블리싱

---
+++ apps/penxle.com/src/routes/(default)/me/revenue/(index)/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import { Button } from '$lib/components';
+  import { PopupSearch } from '$lib/components/forms';
+  import { Table, TableData, TableHead, TableRow } from '$lib/components/table';
+</script>
+
+<div class="flex gap-2 justify-between">
+  <PopupSearch />
+  <div class="flex gap-2 items-end">
+    <Button size="md">내 포스트 관리하기</Button>
+  </div>
+</div>
+
+<Table class="mt-4 text-left border-separate border-spacing-y-0.125rem">
+  <TableRow>
+    <TableHead class="">포스트명</TableHead>
+    <TableHead class="">수익 발생일</TableHead>
+    <TableHead class="text-right pr-12!">수익금</TableHead>
+  </TableRow>
+  <TableRow>
+    <TableData class="">
+      <div class="flex flex-col gap-1">
+        <div class="text-primary subtitle-18-b">뉴진스 해린 그려봤어요</div>
+        <div class="text-secondary body-13-m">스페이스명</div>
+      </div>
+    </TableData>
+    <TableData class="text-secondary body-14-m">23.08.22 13:00:00</TableData>
+    <TableData class="text-right pr-12!">
+      <div class="flex flex-col">
+        <div class="text-primary subtitle-18-b">18,000원</div>
+        <div class="text-secondary body-13-m">21,000원</div>
+      </div>
+    </TableData>
+  </TableRow>
+  <TableRow>
+    <TableData class="">
+      <div class="flex flex-col gap-1">
+        <div class="text-primary subtitle-18-b">뉴진스 해린 그려봤어요</div>
+        <div class="text-secondary body-13-m">스페이스명</div>
+      </div>
+    </TableData>
+    <TableData class="text-secondary body-14-m">23.08.22 13:00:00</TableData>
+    <TableData class="text-right pr-12!">
+      <div class="flex flex-col">
+        <div class="text-primary subtitle-18-b">18,000원</div>
+        <div class="text-secondary body-13-m">21,000원</div>
+      </div>
+    </TableData>
+  </TableRow>
+</Table>
+
+<!-- TODO: Pagination -->
---
+++ apps/penxle.com/src/routes/(default)/me/revenue/+layout.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { Button } from '$lib/components';
+  import { TabHead, TabHeadItem } from '$lib/components/tab';
+  import { pageSubTitle } from '$lib/stores';
+  import { comma } from '$lib/utils';
+
+  onMount(async () => {
+    pageSubTitle.set('수익/정산');
+  });
+</script>
+
+<h1 class="title-20-b">수익/정산</h1>
+
+<div class="mt-6 flex items-center gap-6">
+  <div class="text-black body-16-m">카카오뱅크 3333-02-5063260</div>
+  <Button color="tertiary" size="sm" variant="outlined">출금계좌 변경</Button>
+</div>
+
+<div class="mt-6 flex flex-col py-4 bg-white border border-secondary rounded-2xl">
+  <div class="flex flex-col gap-2 px-8 pt-2">
+    <hr class="border-t-0 border-b-1 border-alphagray-10" />
+    <div class="flex gap-2 py-2">
+      <div class="subtitle-18-eb flex-1">누적 수익금</div>
+      <div class="subtitle-18-eb">{comma(0)}원</div>
+    </div>
+  </div>
+  <div class="flex flex-col gap-2 px-8 pt-2">
+    <hr class="border-t-0 border-b-1 border-alphagray-10" />
+    <div class="flex gap-2 py-2">
+      <div class="subtitle-18-eb flex-1">누적 정산액</div>
+      <div class="subtitle-18-eb">{comma(0)}원</div>
+    </div>
+  </div>
+  <div class="flex flex-col gap-2 px-8 pt-2">
+    <hr class="border-t-0 border-b-1 border-alphagray-10" />
+    <div class="flex gap-2 py-2">
+      <div class="subtitle-18-eb flex-1">지급 대기</div>
+      <div class="subtitle-18-eb">{comma(0)}원</div>
+    </div>
+  </div>
+  <div class="flex flex-col gap-2 px-8 pt-2">
+    <hr class="border-t-0 border-b-1 border-alphagray-10" />
+    <div class="flex gap-2 py-2">
+      <div class="subtitle-18-eb flex-1">정산 가능</div>
+      <div class="subtitle-18-eb">{comma(1234)}원</div>
+      <Button size="sm">받기</Button>
+    </div>
+  </div>
+</div>
+
+<div class="mt-6 flex flex-col gap-4 py-8 bg-white border border-secondary rounded-2xl">
+  <h2 class="title-20-b px-6">수익/정산 내역</h2>
+  <TabHead class="w-full px-6" variant="secondary">
+    <TabHeadItem id={1} href="/me/revenue">수익</TabHeadItem>
+    <TabHeadItem id={2} href="/me/revenue/settlement">정산</TabHeadItem>
+  </TabHead>
+  <div class="px-6">
+    <slot />
+  </div>
+</div>
