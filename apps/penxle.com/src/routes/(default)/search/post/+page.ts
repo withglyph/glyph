a49be5fca4ae20c76a1e647c5d8f95037d1cce7e
Seohyun Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2023-12-14 06:22:10 +0000

    검색필터 제대로 적용되지 않는 문제 해결 (#836)
    1. 성인물 제외 옵션을 선택해도 false가 아닌 true값을 요청으로 보내 성인물이 포함되는 문제 수정
    2. 포함 태그, 제외 태그, 트리거 목록이 배열이 아닌 string값으로 보내지는 문제 수정

--- apps/penxle.com/src/routes/(default)/search/post/+page.ts
+++ apps/penxle.com/src/routes/(default)/search/post/+page.ts
@@ -1,18 +1,51 @@
+import qs from 'query-string';
+import type { ContentFilterCategory, OrderByKind } from '$glitch';
 import type { PageLoadEvent } from './$types';
 
 export const _SearchPostPage_QueryVariables = (event: PageLoadEvent) => {
-  const includeTags = event.url.searchParams.get('include_tags') ?? [];
-  const excludeTags = event.url.searchParams.get('exclude_tags') ?? [];
-  const adult = event.url.searchParams.get('adult') === '' ? null : Boolean(event.url.searchParams.get('adult'));
-  const excludeTriggers = event.url.searchParams.get('exclude_triggers');
-  const orderBy = event.url.searchParams.get('order_by') ?? 'ACCURACY';
+  let includeTags: string[] = [];
+  let excludeTags: string[] = [];
+  const adultFilter =
+    event.url.searchParams.get('adult') === '' ? null : JSON.parse(event.url.searchParams.get('adult') as string);
+  let excludeContentFilters: ContentFilterCategory[] = [];
+  let orderBy: OrderByKind = 'ACCURACY';
+
+  if (qs.parseUrl(event.url.search)?.query) {
+    const parsedURL = qs.parseUrl(event.url.search).query;
+
+    if (parsedURL.include_tags) {
+      if (typeof parsedURL.include_tags === 'string') {
+        includeTags = [parsedURL.include_tags];
+      } else if (typeof parsedURL.include_tags === 'object') {
+        includeTags = parsedURL.include_tags as string[];
+      }
+    }
+
+    if (parsedURL.exclude_tags) {
+      if (typeof parsedURL.exclude_tags === 'string') {
+        excludeTags = [parsedURL.exclude_tags];
+      } else if (typeof parsedURL.exclude_tags === 'object') {
+        excludeTags = parsedURL.exclude_tags as string[];
+      }
+    }
+
+    if (parsedURL.exclude_triggers) {
+      if (typeof parsedURL.exclude_triggers === 'string') {
+        excludeContentFilters = [parsedURL.exclude_triggers] as ContentFilterCategory[];
+      } else if (typeof parsedURL.exclude_triggers === 'object') {
+        excludeContentFilters = parsedURL.exclude_triggers as ContentFilterCategory[];
+      }
+    }
+
+    orderBy = parsedURL.order_by === 'LATEST' ? 'LATEST' : 'ACCURACY';
+  }
 
   return {
     query: event.url.searchParams.get('q'),
     includeTags,
     excludeTags,
-    adultFilter: adult,
-    excludeContentFilters: excludeTriggers,
+    adultFilter,
+    excludeContentFilters,
     orderBy,
   };
 };
