f9edb9f756b73dcf884e49cad1f52ed60546e7dd
Muhun Kim (김 무훈) <iam@muhun.kim>
2023-12-21 12:11:09 +0000

    태그 수직 정렬이 lg 사이즈 및 체크박스 타입에서 밑으로 위치되는 문제 고치기 (#950)
    태그 요소의 높이 값을 지워 해결했습니다.

--- apps/penxle.com/src/lib/components/Tag.svelte
+++ apps/penxle.com/src/lib/components/Tag.svelte
@@ -14,19 +14,17 @@
   this={as}
   id={name}
   class={clsx(
-    'rounded-8 bg-gray-10 transition duration-300 border border-gray-10 truncate',
+    'rounded-8 bg-gray-10 transition duration-300 border border-gray-10 leading-none py-1 truncate',
     checked && 'border-gray-90!',
-    size === 'sm' && 'px-3 h-6.5 body-13-m',
-    size === 'lg' && 'py-1 px-4 h-8 body-16-sb',
+    size === 'sm' && 'px-2 body-13-m',
+    size === 'lg' && 'px-4 body-16-sb',
     as !== 'div' && 'hover:(bg-gray-20 border-gray-20)',
     _class,
   )}
   {...$$restProps}
 >
   {#if as === 'label'}
-    <input class="hidden" aria-checked={checked} type="checkbox" on:change bind:checked />
+    <input class="appearance-none" aria-checked={checked} type="checkbox" on:change bind:checked />
   {/if}
-  <span class="align-text-top">
-    <slot />
-  </span>
+  <slot />
 </svelte:element>
