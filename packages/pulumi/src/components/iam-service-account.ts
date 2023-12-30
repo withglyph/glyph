a74328bc24a9503e0d5e7617e6b88fdbc19248b1
Bae Junehyeon <337728+devunt@users.noreply.github.com>
2023-12-30 11:46:38 +0000

    IAMServiceAccount 리소스에 parent 바인딩이 안 되어 있던 이슈 수정 (#1010)
    바인딩을 깜빡한 바람에 리소스가 따로 놀고 있어서 `{ parent: this }` 바인딩 추가함
    머지 이후 리소스 수동 재생성 필요

--- packages/pulumi/src/components/iam-service-account.ts
+++ packages/pulumi/src/components/iam-service-account.ts
@@ -26,40 +26,52 @@ export class IAMServiceAccount extends pulumi.ComponentResource {
 
     const oidcProviderUrl = bedrockRef('AWS_EKS_CLUSTER_OIDC_PROVIDER_URL');
 
-    const role = new aws.iam.Role(`${name}@eks`, {
-      name: pulumi.interpolate`${args.metadata.name}+${args.metadata.namespace}@eks`,
-      assumeRolePolicy: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Effect: 'Allow',
-            Principal: { Federated: bedrockRef('AWS_EKS_CLUSTER_OIDC_PROVIDER_ARN') },
-            Action: 'sts:AssumeRoleWithWebIdentity',
-            Condition: oidcProviderUrl.apply((url) => ({
-              StringEquals: {
-                [`${url}:aud`]: 'sts.amazonaws.com',
-                [`${url}:sub`]: pulumi.interpolate`system:serviceaccount:${args.metadata.namespace}:${args.metadata.name}`,
-              },
-            })),
-          },
-        ],
-      },
-    });
-
-    new aws.iam.RolePolicy(`${name}@eks`, {
-      role: role.name,
-      policy: args.spec.policy,
-    });
-
-    const serviceAccount = new k8s.core.v1.ServiceAccount(name, {
-      metadata: {
-        name: args.metadata.name,
-        namespace: args.metadata.namespace,
-        annotations: {
-          'eks.amazonaws.com/role-arn': role.arn,
+    const role = new aws.iam.Role(
+      `${name}@eks`,
+      {
+        name: pulumi.interpolate`${args.metadata.name}+${args.metadata.namespace}@eks`,
+        assumeRolePolicy: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Effect: 'Allow',
+              Principal: { Federated: bedrockRef('AWS_EKS_CLUSTER_OIDC_PROVIDER_ARN') },
+              Action: 'sts:AssumeRoleWithWebIdentity',
+              Condition: oidcProviderUrl.apply((url) => ({
+                StringEquals: {
+                  [`${url}:aud`]: 'sts.amazonaws.com',
+                  [`${url}:sub`]: pulumi.interpolate`system:serviceaccount:${args.metadata.namespace}:${args.metadata.name}`,
+                },
+              })),
+            },
+          ],
         },
       },
-    });
+      { parent: this },
+    );
+
+    new aws.iam.RolePolicy(
+      `${name}@eks`,
+      {
+        role: role.name,
+        policy: args.spec.policy,
+      },
+      { parent: this },
+    );
+
+    const serviceAccount = new k8s.core.v1.ServiceAccount(
+      name,
+      {
+        metadata: {
+          name: args.metadata.name,
+          namespace: args.metadata.namespace,
+          annotations: {
+            'eks.amazonaws.com/role-arn': role.arn,
+          },
+        },
+      },
+      { parent: this },
+    );
 
     this.metadata = pulumi.output({
       name: serviceAccount.metadata.name,
