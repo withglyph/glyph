a4c66f3b1dd3b16c7b4b0bf8ddd10f3521062316
Jiyu Park <8201350+robin-maki@users.noreply.github.com>
2024-01-15 03:17:23 +0000

    빌드 시간에 AES 키를 참조하려 해서 생기는 오류 수정 (#1075)

--- apps/penxle.com/src/lib/server/utils/aes.ts
+++ apps/penxle.com/src/lib/server/utils/aes.ts
@@ -1,15 +1,19 @@
 import { webcrypto } from 'node:crypto';
+import * as R from 'radash';
 import { env } from '$env/dynamic/private';
 
-const key = await webcrypto.subtle.importKey(
-  'jwk',
-  JSON.parse(atob(env.PRIVATE_AES_SECRET_JWK)),
-  { name: 'AES-GCM' },
-  true,
-  ['encrypt', 'decrypt'],
+const getAESKey = R.memo(() =>
+  webcrypto.subtle.importKey(
+    'jwk',
+    JSON.parse(Buffer.from(env.PRIVATE_AES_SECRET_JWK, 'base64').toString()),
+    { name: 'AES-GCM' },
+    true,
+    ['encrypt', 'decrypt'],
+  ),
 );
 
 export const encryptAES = async (text: string) => {
+  const key = await getAESKey();
   const nonce = webcrypto.getRandomValues(new Uint8Array(12));
 
   const encrypted = await webcrypto.subtle.encrypt({ name: 'AES-GCM', iv: nonce }, key, new TextEncoder().encode(text));
@@ -21,6 +25,7 @@ export const encryptAES = async (text: string) => {
 };
 
 export const decryptAES = async (encrypted: string, nonce: string) => {
+  const key = await getAESKey();
   const nonceByteArray = Buffer.from(nonce, 'base64');
 
   const decrypted = await webcrypto.subtle.decrypt(
