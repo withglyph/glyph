6e5bc74d78971bd8405f6220926ace724d457677
Seohyun Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-01-16 05:25:23 +0000

    페이지네이션 컴포넌트 개선 (#1081)
    로직에서 한 페이지 당 보여줄 게시물 갯수와 한 페이지 그룹에 들어가는 페이지 갯수가 섞여있어 다른 변수로 분리함

--- apps/penxle.com/src/lib/components/Pagination.svelte
+++ apps/penxle.com/src/lib/components/Pagination.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
   export let initialPage: number;
   export let totalItems: number;
+  export let itemsPerPage = 10;
   export let displayPage = 10;
   export let onChange: (page: number) => void;
 
   let currentPage = initialPage;
 
-  $: maxPage = Math.ceil(totalItems / displayPage);
+  $: maxPage = Math.ceil(totalItems / itemsPerPage);
   $: currentPageGroup = Math.ceil(currentPage / displayPage);
-  $: totalPageGroup = Math.ceil(maxPage / 10);
-  $: remainingItems = maxPage - (currentPageGroup - 1) * displayPage;
+  $: totalPageGroup = Math.ceil(maxPage / displayPage);
+  $: remainingPage = maxPage - (currentPageGroup - 1) * displayPage;
 
-  $: range = [...Array.from({ length: remainingItems < displayPage ? remainingItems : displayPage }).keys()].map(
+  $: range = [...Array.from({ length: remainingPage < displayPage ? remainingPage : displayPage }).keys()].map(
     (index) => (currentPageGroup - 1) * displayPage + (index + 1),
   );
 
@@ -26,8 +27,8 @@
     disabled={currentPage <= displayPage}
     type="button"
     on:click={() => {
-      const previousPage = currentPageGroup - 1;
-      currentPage = previousPage * displayPage;
+      const previousPageGroup = currentPageGroup - 1;
+      currentPage = previousPageGroup * displayPage;
     }}
   >
     <i class="i-lc-chevron-left square-3.5" aria-label="왼쪽 화살표 아이콘" />
@@ -46,11 +47,11 @@
   {/each}
   <button
     class="square-7 flex center disabled:(text-disabled cursor-not-allowed)"
-    disabled={maxPage <= displayPage || currentPageGroup === totalPageGroup}
+    disabled={currentPageGroup === totalPageGroup}
     type="button"
     on:click={() => {
-      const nextPage = currentPageGroup + 1;
-      currentPage = nextPage * displayPage - (displayPage - 1);
+      const nextPageGroup = currentPageGroup + 1;
+      currentPage = nextPageGroup * displayPage - (displayPage - 1);
     }}
   >
     <i class="i-lc-chevron-right square-3.5" aria-label="오른쪽 화살표 아이콘" />
