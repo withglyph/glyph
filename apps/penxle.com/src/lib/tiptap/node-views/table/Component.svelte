84c91edbd90dfe644c7cdc89d389e158d7ead3bd
Muhun Kim <iam@muhun.kim>
2024-01-26 16:44:04 +0900

    행 및 열 추가 등 표 서식 편집을 위한 스벨트 기반 노드 뷰 추가
    ref: https://stackblitz.com/edit/tiptap-table?file=Extensions%2Ftable.ts,Components%2FTableControllers.tsx
    
    해당 노드 뷰 추가 이후에 리사이징 및 셀 선택이 UI 상에 안보이는 문제가 있음

---
+++ apps/penxle.com/src/lib/tiptap/node-views/table/Component.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { NodeView, NodeViewContentEditable } from '$lib/tiptap';
+  import type { NodeViewProps } from '$lib/tiptap';
+
+  type $$Props = NodeViewProps;
+  $$restProps;
+
+  export let editor: NodeViewProps['editor'] | undefined;
+</script>
+
+<NodeView class="w-fit p-4 relative">
+  <button
+    class="h-full text-center w-1rem bg-[rgba(33,33,33,0.1)] right-0 absolute"
+    type="button"
+    on:click={editor?.commands.addColumnAfter}
+  >
+    +
+  </button>
+  <NodeViewContentEditable />
+  <button
+    class="absolute bottom-0 w-full text-center h-1rem bg-[rgba(33,33,33,0.1)]"
+    type="button"
+    on:click={editor?.commands.addRowAfter}
+  >
+    +
+  </button>
+</NodeView>
