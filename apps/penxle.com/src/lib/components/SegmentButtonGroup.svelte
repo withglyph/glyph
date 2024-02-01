7ed3611643a8a193081331c9e62bd6e7d19eb1aa
Muhun <iam@muhun.kim>
2024-02-01 07:32:50 +0000

    세그맨트 버튼 그룹 보더 스타일링 개선 (#1173)
    선택한 버튼의 오른쪽 보더가 없었던 스타일링 사항을 해결했습니다.

--- apps/penxle.com/src/lib/components/SegmentButtonGroup.svelte
+++ apps/penxle.com/src/lib/components/SegmentButtonGroup.svelte
@@ -1,9 +1,22 @@
+<script lang="ts">
+  import clsx from 'clsx';
+
+  const firstChild = ['[&>*:first-child]:(rounded-r-none)', '[&>*:first-child:not(:has(:checked))]:(border-r-none)'];
+  const notFirstOrLastChild = '[&>*:not(:first-child,:last-child)]:rounded-(l-none r-none)';
+  const resetBorder = '[&>*:not(:first-child,:last-child,:has(:checked))]:border-r-none';
+  const resetSelectedChildNextSibling = '[&>*:has(:checked)+*]:border-l-none';
+  const lastChild = '[&>*:last-child]:rounded-l-none';
+</script>
+
 <div
-  class="flex [&>*]:flex-1
-    [&>*:first-child]:(rounded-r-none border-r-none)
-    [&>*:not(:first-child,:last-child)]:(rounded-(l-none r-none) border-r-none)
-    [&>*:last-child]:rounded-l-none
-    "
+  class={clsx(
+    'flex [&>*]:flex-1',
+    firstChild,
+    notFirstOrLastChild,
+    resetBorder,
+    resetSelectedChildNextSibling,
+    lastChild,
+  )}
   role="group"
 >
   <slot />
