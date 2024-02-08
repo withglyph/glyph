de539c4d1028f54e090b37cbea74d1b75bc662ae
Bae Junehyeon <337728+devunt@users.noreply.github.com>
2024-02-08 12:33:54 +0000

    모바일에서 팁탭 노드뷰 터치 스크롤 안 되는 이슈 수정 (#1289)
    안드로이드에서 노드뷰 이동 구현하려고 넣었던거라 빼도 무관함 (동작 안했음)
    이거때매 모바일에서 웹툰같은거 스크롤이 안 되는 중

--- apps/penxle.com/src/lib/tiptap/lib/NodeView.svelte
+++ apps/penxle.com/src/lib/tiptap/lib/NodeView.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
-  import clsx from 'clsx';
   import { getContext } from 'svelte';
   import type { DragEventHandler } from 'svelte/elements';
 
   const onDragStart = getContext<DragEventHandler<HTMLDivElement>>('onDragStart');
 </script>
 
-<div
-  {...$$restProps}
-  class={clsx('touch-none', $$restProps.class)}
-  data-node-view
-  role="presentation"
-  on:dragstart={onDragStart}
-  on:touchmove|nonpassive|preventDefault
-  on:contextmenu|preventDefault
->
+<div {...$$restProps} data-node-view role="presentation" on:dragstart={onDragStart}>
   <slot />
 </div>
