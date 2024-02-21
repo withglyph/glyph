c152afda3ec3ed218e6543471053b6454be60987
Muhun Kim <iam@muhun.kim>
2024-02-21 18:30:30 +0900

    다크 모드 실험 중

---
+++ apps/penxle.com/src/routes/(default)/[space]/[post]/+layout@.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { onMount, setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+  import Footer from '../../Footer.svelte';
+  import Header from './Header.svelte';
+
+  let colorScheme = writable<'light' | 'dark' | undefined>(undefined);
+  let focusMode = writable(false);
+
+  setContext('colorScheme', colorScheme);
+  setContext('focusMode', focusMode);
+
+  onMount(() => {
+    $colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  });
+</script>
+
+<main class={$colorScheme}>
+  <Header />
+  <slot />
+</main>
+
+<Footer />
