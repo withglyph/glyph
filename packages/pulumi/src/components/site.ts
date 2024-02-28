491c4662fe9d603add81e0b7c23a89bf1bf9e9e9
Bae Junehyeon <337728+devunt@users.noreply.github.com>
2024-02-28 06:50:09 +0000

    프리뷰 도메인 effit.dev로 변경 (#1461)
    `pnxl.site` 이제 사용 안 함

--- packages/pulumi/src/components/site.ts
+++ packages/pulumi/src/components/site.ts
@@ -60,9 +60,9 @@ export class Site extends pulumi.ComponentResource {
 
     const domainName = match(stack)
       .with('prod', () => args.domain.production)
-      .with('staging', () => args.domain.staging ?? pulumi.interpolate`${args.name}-stg.pnxl.site`)
-      .with('dev', () => args.domain.dev ?? pulumi.interpolate`${args.name}-dev.pnxl.site`)
-      .otherwise(() => pulumi.interpolate`${args.name}-${stack}.pnxl.site`);
+      .with('staging', () => args.domain.staging ?? pulumi.interpolate`${args.name}-stg.effit.dev`)
+      .with('dev', () => args.domain.dev ?? pulumi.interpolate`${args.name}-dev.effit.dev`)
+      .otherwise(() => pulumi.interpolate`${args.name}-${stack}.effit.dev`);
 
     const namespace = match(stack)
       .with('prod', () => 'prod')
