5fde875f274f136d763bbcfc2be9dd2021f84815
kylie <kylie@penxle.io>
2024-03-04 18:01:18 +0900

    panda css 전환

--- apps/penxle.com/src/routes/(default)/search/(index)/+page.svelte
+++ apps/penxle.com/src/routes/(default)/search/(index)/+page.svelte
@@ -9,6 +9,8 @@
   import { Icon, Image, PostCard, Tag } from '$lib/components';
   import { TabHead, TabHeadItem } from '$lib/components/tab';
   import { toast } from '$lib/notification';
+  import { css } from '$styled-system/css';
+  import { center, flex } from '$styled-system/patterns';
   import LoginRequireModal from '../../LoginRequireModal.svelte';
 
   let loginRequireOpen = false;
@@ -76,45 +78,107 @@
 
 <Helmet description={`펜슬에서 ${keyword} 검색을 한 결과에요`} title={`${keyword} 검색 결과`} />
 
-<div class="body-14-m text-secondary mt-3 <sm:m-l-4">
+<div
+  class={css({
+    marginTop: '12px',
+    fontSize: '14px',
+    fontWeight: 'medium',
+    color: 'gray.500',
+    smDown: { marginLeft: '16px' },
+  })}
+>
   약 {$query.searchPosts.count}개의 검색결과가 있어요!
 </div>
 
-<TabHead class="mt-9 mb-4 w-full <sm:(sticky top-61px z-1)" {search} variant="secondary">
+<TabHead
+  style={css.raw({
+    marginTop: '36px',
+    marginBottom: '16px',
+    width: 'full',
+    smDown: {
+      position: 'sticky',
+      zIndex: '1',
+      top: '56px',
+    },
+  })}
+  {search}
+  variant="secondary"
+>
   <TabHeadItem id={0} pathname="/search">전체</TabHeadItem>
   <TabHeadItem id={1} pathname="/search/post">포스트</TabHeadItem>
   <TabHeadItem id={2} pathname="/search/space">스페이스</TabHeadItem>
   <TabHeadItem id={3} pathname="/search/tag">태그</TabHeadItem>
 </TabHead>
 
-<div class="my-9 flex gap-9 <sm:flex-col">
-  <div class="grow w-50% <sm:(w-full px-4)">
+<div class={flex({ gap: '36px', marginY: '36px', smDown: { flexDirection: 'column' } })}>
+  <div class={css({ flexGrow: '1', width: '[50%]', smDown: { paddingX: '16px', width: 'full' } })}>
     <svelte:element
       this={$query.searchSpaces.length === 0 ? 'h2' : 'a'}
-      class="flex title-20-b mb-4 items-center"
+      class={flex({ align: 'center', marginBottom: '16px', fontSize: '20px', fontWeight: 'bold' })}
       href={$query.searchSpaces.length === 0 ? undefined : `/search/space?q=${$page.url.searchParams.get('q')}`}
     >
       스페이스
       {#if $query.searchSpaces.length > 0}
-        <Icon class="square-6 text-secondary" icon={IconChevronRight} />
+        <Icon style={css.raw({ color: 'gray.500', size: '24px' })} icon={IconChevronRight} />
       {/if}
     </svelte:element>
 
     {#if $query.searchSpaces.length === 0}
-      <div class="text-secondary body-15-b text-center flex center min-h-50">검색 결과가 없어요</div>
+      <div
+        class={center({
+          fontSize: '15px',
+          fontWeight: 'bold',
+          color: 'gray.500',
+          textAlign: 'center',
+          minHeight: '200px',
+        })}
+      >
+        검색 결과가 없어요
+      </div>
     {:else}
-      <div class="space-y-3">
+      <div class={flex({ flexDirection: 'column', gap: '12px' })}>
         {#each $query.searchSpaces.slice(0, 2) as space (space.id)}
-          <div class="p-1 rounded-lg hover:bg-surface-primary flex items-center gap-4">
+          <div
+            class={flex({
+              align: 'center',
+              gap: '16px',
+              borderRadius: '8px',
+              padding: '4px',
+              _hover: { backgroundColor: 'gray.100' },
+            })}
+          >
             <a href={`/${space.slug}`}>
-              <Image class="square-15 rounded-5 border border-secondary" $image={space.icon} />
+              <Image
+                style={css.raw({ borderWidth: '1px', borderColor: 'gray.200', borderRadius: '[20px]', size: '60px' })}
+                $image={space.icon}
+              />
             </a>
 
-            <a class="flex flex-col gap-1 grow truncate" href={`/${space.slug}`}>
-              <p class="body-15-b whitespace-pre-line line-clamp-1 break-all">
+            <a
+              class={flex({ flexDirection: 'column', gap: '4px', flexGrow: '1', truncate: true })}
+              href={`/${space.slug}`}
+            >
+              <p
+                class={css({
+                  fontSize: '15px',
+                  fontWeight: 'bold',
+                  whiteSpace: 'pre-line',
+                  lineClamp: '1',
+                  wordBreak: 'break-all',
+                })}
+              >
                 {space.name}
               </p>
-              <p class="body-14-m text-secondary whitespace-pre-line line-clamp-2 break-all">
+              <p
+                class={css({
+                  fontSize: '14px',
+                  fontWeight: 'medium',
+                  color: 'gray.500',
+                  whiteSpace: 'pre-line',
+                  lineClamp: '2',
+                  wordBreak: 'break-all',
+                })}
+              >
                 {space.description ?? ''}
               </p>
             </a>
@@ -122,7 +186,18 @@
             {#if !space.meAsMember}
               {#if space.followed}
                 <button
-                  class="py-1.5 px-2 rounded-12 border border-secondary body-13-m flex items-center gap-1 shrink-0"
+                  class={flex({
+                    align: 'center',
+                    gap: '4px',
+                    shrink: '0',
+                    borderWidth: '1px',
+                    borderColor: 'gray.200',
+                    borderRadius: '[48px]',
+                    paddingX: '8px',
+                    paddingY: '6px',
+                    fontSize: '13px',
+                    fontWeight: 'medium',
+                  })}
                   type="button"
                   on:click={async () => {
                     await unfollowSpace({ spaceId: space.id });
@@ -135,7 +210,16 @@
                 </button>
               {:else}
                 <button
-                  class="py-1.5 px-2 rounded-12 bg-gray-90 text-gray-5 body-13-m flex items-center gap-1 shrink-0"
+                  class={flex({
+                    align: 'center',
+                    gap: '4px',
+                    shrink: '0',
+                    borderRadius: '[48px]',
+                    paddingX: '8px',
+                    paddingY: '6px',
+                    color: 'gray.50',
+                    backgroundColor: 'gray.900',
+                  })}
                   type="button"
                   on:click={async () => {
                     if (!$query.me) {
@@ -159,22 +243,32 @@
     {/if}
   </div>
 
-  <div class="grow w-50% <sm:(w-full px-4)">
+  <div class={css({ flexGrow: '1', width: '[50%]', smDown: { paddingX: '16px', width: 'full' } })}>
     <svelte:element
       this={$query.searchTags.length === 0 ? 'h2' : 'a'}
-      class="flex title-20-b mb-4 items-center"
+      class={flex({ align: 'center', marginBottom: '16px', fontSize: '20px', fontWeight: 'bold' })}
       href={$query.searchTags.length === 0 ? undefined : `/search/tag?q=${$page.url.searchParams.get('q')}`}
     >
       태그
       {#if $query.searchTags.length > 0}
-        <Icon class="square-6 text-secondary" icon={IconChevronRight} />
+        <Icon style={css.raw({ color: 'gray.500', size: '24px' })} icon={IconChevronRight} />
       {/if}
     </svelte:element>
 
     {#if $query.searchTags.length === 0}
-      <div class="text-secondary body-15-b text-center flex center min-h-50">검색 결과가 없어요</div>
+      <div
+        class={center({
+          fontSize: '15px',
+          fontWeight: 'bold',
+          color: 'gray.500',
+          textAlign: 'center',
+          minHeight: '200px',
+        })}
+      >
+        검색 결과가 없어요
+      </div>
     {:else}
-      <div class="flex flex-wrap gap-3">
+      <div class={flex({ flexWrap: 'wrap', gap: '12px' })}>
         {#each $query.searchTags.slice(0, 6) as tag (tag.id)}
           <Tag href={`/tag/${tag.name}`} size="lg">#{tag.name}</Tag>
         {/each}
@@ -186,21 +280,31 @@
 <div>
   <svelte:element
     this={$query.searchPosts.count === 0 ? 'h2' : 'a'}
-    class="title-20-b <sm:px-4"
+    class={flex({ align: 'center', fontSize: '20px', fontWeight: 'bold', smDown: { paddingX: '16px' } })}
     href={$query.searchPosts.count === 0 ? undefined : `/search/post?q=${$page.url.searchParams.get('q')}`}
   >
     포스트
     {#if $query.searchPosts.count > 0}
-      <Icon class="square-6 text-secondary" icon={IconChevronRight} />
+      <Icon style={css.raw({ color: 'gray.500', size: '24px' })} icon={IconChevronRight} />
     {/if}
   </svelte:element>
 
   {#if $query.searchPosts.count === 0}
-    <div class="text-secondary body-15-b text-center flex center min-h-50">검색 결과가 없어요</div>
+    <div
+      class={center({
+        fontSize: '15px',
+        fontWeight: 'bold',
+        color: 'gray.500',
+        textAlign: 'center',
+        minHeight: '200px',
+      })}
+    >
+      검색 결과가 없어요
+    </div>
   {:else}
-    <div class="<sm:bg-primary">
+    <div class={css({ smDown: { backgroundColor: 'gray.50' } })}>
       {#each $query.searchPosts.posts as post (post.id)}
-        <PostCard class="mt-4" $post={post} />
+        <PostCard style={css.raw({ marginTop: '16px' })} $post={post} />
       {/each}
     </div>
   {/if}
