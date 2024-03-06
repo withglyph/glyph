184a7308c4163223f9ae34598b103240d79ba583
kylie <kylie@penxle.io>
2024-03-06 20:02:37 +0900

    pandacss 전환

--- apps/penxle.com/src/routes/(default)/[space]/dashboard/subscribers/blocked/+page.svelte
+++ apps/penxle.com/src/routes/(default)/[space]/dashboard/subscribers/blocked/+page.svelte
@@ -9,6 +9,8 @@
   import { Icon } from '$lib/components';
   import { TabHead, TabHeadItem } from '$lib/components/tab';
   import { pageSubTitle } from '$lib/stores';
+  import { css } from '$styled-system/css';
+  import { center, flex } from '$styled-system/patterns';
 
   onMount(async () => {
     pageSubTitle.set('독자 관리');
@@ -45,44 +47,89 @@
   `);
 </script>
 
-<h1 class="sm:(title-20-eb mb-6) <sm:hidden">독자 관리</h1>
+<h1 class={css({ marginBottom: '24px', fontSize: '20px', fontWeight: 'bold', hideBelow: 'sm' })}>독자 관리</h1>
 
-<div class="<sm:bg-white">
-  <TabHead class="border-none mb-6" variant="secondary">
+<div class={css({ smDown: { backgroundColor: 'white' } })}>
+  <TabHead style={css.raw({ borderStyle: 'none', marginBottom: '24px' })} variant="secondary">
     <TabHeadItem id={0} pathname="/{$page.params.space}/dashboard/subscribers/blocked">차단 계정</TabHeadItem>
   </TabHead>
 </div>
 
 {#if $query.space.blockedMasquerades.length === 0}
-  <div class="flex flex-col gap-1 center grow my-30">
-    <Icon class="square-7 block" icon={IconAlertTriangle} />
-    <p class="text-center my-auto text-16-sb">차단 유저가 없어요</p>
+  <div class={center({ flexDirection: 'column', gap: '4px', flexGrow: '1', marginY: '120px' })}>
+    <Icon style={css.raw({ size: '28px' })} icon={IconAlertTriangle} />
+    <p class={css({ marginY: 'auto', fontWeight: 'semibold', textAlign: 'center' })}>차단 유저가 없어요</p>
   </div>
 {:else}
-  <section class="flex flex-col gap-4 p-4 rounded-xl bg-white sm:p-6 <sm:(my-5 mx-4)">
-    <table class="border-spacing-0">
+  <section
+    class={flex({
+      flexDirection: 'column',
+      gap: '16px',
+      borderRadius: '12px',
+      padding: { base: '16px', sm: '24px' },
+      backgroundColor: 'white',
+      smDown: { marginY: '20px', marginX: '16px' },
+    })}
+  >
+    <table class={css({ borderSpacing: '0' })}>
       <thead>
-        <tr class="bg-gray-50 text-14-sb">
-          <th class="py-2 pl-5 lg:w-120px <sm:hidden">
-            <button class="flex items-center gap-1" type="button">
+        <tr class={css({ fontSize: '14px', fontWeight: 'semibold', backgroundColor: 'gray.50' })}>
+          <th class={css({ paddingY: '8px', paddingLeft: '20px', lg: { width: '120px' }, hideBelow: 'sm' })}>
+            <button class={flex({ align: 'center', gap: '4px' })} type="button">
               차단일자
-              <Icon class="square-3.5" icon={IconSelector} />
+              <Icon style={css.raw({ size: '14px' })} icon={IconSelector} />
             </button>
           </th>
-          <th class="py-2 px-2.5 text-left">아이디(익명)</th>
-          <th class="py-2" />
+          <th class={css({ paddingX: '10px', paddingY: '8px', textAlign: 'left' })}>아이디(익명)</th>
+          <th class={css({ paddingY: '8px' })} />
         </tr>
       </thead>
       <tbody>
         {#each $query.space.blockedMasquerades as masquerade (masquerade.id)}
           <tr>
-            <td class="border-t border-gray-200 text-14-r text-gray-600 pl-5 <sm:hidden">
+            <td
+              class={css({
+                borderTopWidth: '1px',
+                borderColor: 'gray.200',
+                paddingLeft: '20px',
+                fontSize: '14px',
+                color: 'gray.600',
+                hideBelow: 'sm',
+              })}
+            >
               {dayjs(masquerade.blockedAt).formatAsDate()}
             </td>
-            <td class="text-14-r py-3 px-2.5 border-t border-gray-200 text-gray-600">{masquerade.profile.name}</td>
-            <td class="py-3 pr-1.5 border-t border-gray-200 text-right">
+            <td
+              class={css({
+                borderTopWidth: '1px',
+                borderColor: 'gray.200',
+                paddingX: '10px',
+                paddingY: '12px',
+                fontSize: '14px',
+                color: 'gray.600',
+              })}
+            >
+              {masquerade.profile.name}
+            </td>
+            <td
+              class={css({
+                borderTopWidth: '1px',
+                borderColor: 'gray.200',
+                paddingY: '12px',
+                paddingRight: '6px',
+                textAlign: 'right',
+              })}
+            >
               <button
-                class="text-11-sb text-error-900 py-1.5 px-3.5 rounded hover:bg-gray-50"
+                class={css({
+                  borderRadius: '4px',
+                  paddingX: '14px',
+                  paddingY: '6px',
+                  fontSize: '11px',
+                  fontWeight: 'semibold',
+                  color: '[#DC2626]',
+                  _hover: { backgroundColor: 'gray.50' },
+                })}
                 type="button"
                 on:click={async () => {
                   await unblockMasquerade({ spaceId: $query.space.id, masqueradeId: masquerade.id });
