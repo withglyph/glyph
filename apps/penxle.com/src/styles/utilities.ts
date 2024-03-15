f4387390ca3a0ad9fbbc2fc004ae3c6a90e559f6
Seohyun Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-03-15 06:53:16 +0000

    pandacss size utility가 동작하지 않는 문제 수정 (#1545)

--- apps/penxle.com/src/styles/utilities.ts
+++ apps/penxle.com/src/styles/utilities.ts
@@ -2,7 +2,6 @@ import { defineUtility } from '@pandacss/dev';
 
 export const utilities = {
   size: defineUtility({
-    className: 's',
     values: 'sizes',
     transform: (value) => ({
       width: value,
