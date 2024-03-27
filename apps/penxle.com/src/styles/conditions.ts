4a187e15ba08177aee25824f9df22bee05d4ee3d
mu-hun <iam@muhun.kim>
2024-03-27 03:13:16 +0000

    링크 타입 버튼에 _enabled 내부 스타일 값이 무시되는 문제 고치기 (#1606)
    링크는 `:enabled` 상태가 없어 발생하는 문제로 a 엘리먼트의 경우 bypass 하도록 선택자를 덮어씌워서 해결했습니다.

--- apps/penxle.com/src/styles/conditions.ts
+++ apps/penxle.com/src/styles/conditions.ts
@@ -1,4 +1,5 @@
 export const conditions = {
   invalid: '&:is(:invalid, [data-invalid], [aria-invalid="true"])',
   active: '&:is(:active:active, [data-active])',
+  enabled: '&:is(:enabled, a)',
 };
