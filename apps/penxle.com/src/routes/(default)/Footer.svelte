b1b990626b9de3d6e52255a3ec7d3c76e1e70245
mu-hun <iam@muhun.kim>
2024-03-31 12:15:52 +0000

    푸터 리뉴얼 (#1625)
    변경될 트위터 및 인스타그램 아이디, 헬프센터 주소 등이 아직 확정되지
    않아 우선 Draft 로 올려둡니다.

--- apps/penxle.com/src/routes/(default)/Footer.svelte
+++ apps/penxle.com/src/routes/(default)/Footer.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import IconEffit from '~icons/effit/effit';
   import IconGithub from '~icons/simple-icons/github';
   import IconInstagram from '~icons/simple-icons/instagram';
   import IconTwitter from '~icons/simple-icons/twitter';
@@ -12,9 +11,8 @@
 <footer
   class={css({
     borderTopWidth: '1px',
-    paddingX: { base: '16px', sm: '30px' },
+    paddingX: '20px',
     paddingY: '40px',
-    color: 'gray.500',
     fontSize: '12px',
     letterSpacing: '[0.05em]',
   })}
@@ -22,75 +20,84 @@
   <div
     class={flex({
       justify: 'space-between',
-      gap: '24px',
+      gap: '32px',
       wrap: 'wrap',
       marginX: 'auto',
       width: 'full',
-      maxWidth: '1200px',
+      maxWidth: '1280px',
     })}
   >
     <div>
-      <section class={flex({ justify: 'space-between', align: 'center', marginBottom: '16px' })}>
-        <Logo class={css({ height: '22px', color: 'gray.900' })} />
+      <Logo class={css({ height: '22px', color: 'gray.900', marginBottom: '12px' })} />
+
+      <section
+        class={flex({
+          align: 'center',
+          gap: '12px',
+          wrap: 'wrap',
+          fontSize: '14px',
+          marginBottom: '20px',
+          color: 'gray.600',
+          lineHeight: 'none',
+        })}
+      >
+        <Link href="TODO">글리프 소식</Link>
+        <div class={css({ height: '12px', borderLeftWidth: '1px', borderLeftColor: 'gray.150', marginX: '4px' })} />
+        <Link href="TODO">글리프 소개</Link>
+        <div class={css({ height: '12px', borderLeftWidth: '1px', borderLeftColor: 'gray.150', marginX: '4px' })} />
+        <Link href="https://help.withglyph.com/">도움센터</Link>
       </section>
 
-      <section class={flex({ direction: 'column', gap: '4px' })}>
-        <div class={flex({ align: 'center', wrap: 'wrap' })}>
-          <div>(주)펜슬컴퍼니</div>
-          <div class={css({ height: '8px', borderLeftWidth: '1px', borderLeftColor: 'gray.600', marginX: '4px' })} />
-          <div>대표: 배준현</div>
-          <div class={css({ height: '8px', borderLeftWidth: '1px', borderLeftColor: 'gray.600', marginX: '4px' })} />
-          <div>개인정보관리책임자: 배준현</div>
-        </div>
+      <ul class={flex({ direction: 'column', gap: '6px', fontSize: '13px', marginBottom: '32px', color: 'gray.500' })}>
+        <li>(주)펜슬컴퍼니</li>
+        <li>대표: 배준현</li>
+        <li>개인정보관리책임자: 배준현</li>
+        <li>사업자등록번호: 610-88-03078</li>
+        <li>주소: 서울특별시 강남구 강남대로100길 14, 6층</li>
+        <li>
+          전화: <a href="tel:02-565-7695">02-565-7695</a>
+        </li>
+      </ul>
 
-        <div class={flex({ align: 'center', wrap: 'wrap' })}>
-          <div>사업자등록번호: 610-88-03078</div>
-          <div class={css({ height: '8px', borderLeftWidth: '1px', borderLeftColor: 'gray.600', marginX: '4px' })} />
-          <Link href="https://www.ftc.go.kr/bizCommPop.do?wrkr_no=6108803078">통신판매업신고: 2023-서울강남-4541</Link>
-        </div>
-
-        <div class={flex({ align: 'center', wrap: 'wrap' })}>
-          <div>주소: 서울특별시 강남구 강남대로100길 14, 6층</div>
-          <div class={css({ height: '8px', borderLeftWidth: '1px', borderLeftColor: 'gray.600', marginX: '4px' })} />
-          <div>전화: 02-565-7695</div>
-        </div>
-      </section>
-
-      <section class={flex({ align: 'center', wrap: 'wrap', marginTop: '16px' })}>
-        <Link href="https://help.penxle.com/legal/terms">이용약관</Link>
-        <div class={css({ height: '8px', borderLeftWidth: '1px', borderLeftColor: 'gray.600', marginX: '4px' })} />
-        <Link style={css.raw({ fontWeight: 'medium' })} href="https://help.penxle.com/legal/privacy">
+      <section
+        class={flex({
+          align: 'center',
+          wrap: 'wrap',
+          marginTop: '16px',
+          fontSize: '13px',
+          gap: '20px',
+          color: 'gray.500',
+          lineHeight: 'none',
+        })}
+      >
+        <Link style={css.raw({ fontWeight: 'medium' })} href="https://help.withglyph.com/legal/privacy">
           개인정보처리방침
         </Link>
-        <div class={css({ height: '8px', borderLeftWidth: '1px', borderLeftColor: 'gray.600', marginX: '4px' })} />
-        <div class={flex({ align: 'center', wrap: 'wrap' })}>
-          <Link href="https://penxle.channel.io">고객 센터</Link>
-          <div class={css({ height: '8px', borderLeftWidth: '1px', borderLeftColor: 'gray.600', marginX: '4px' })} />
-          <Link href="/pencil">펜슬 소식</Link>
-          <div class={css({ height: '8px', borderLeftWidth: '1px', borderLeftColor: 'gray.600', marginX: '4px' })} />
-          <Link href="https://penxle.io">펜슬컴퍼니 소개</Link>
-        </div>
+        <div class={css({ height: '12px', borderLeftWidth: '1px', borderLeftColor: 'gray.150', marginX: '4px' })} />
+        <Link href="https://help.withglyph.com/legal/terms">이용약관</Link>
+        <div class={css({ height: '12px', borderLeftWidth: '1px', borderLeftColor: 'gray.150', marginX: '4px' })} />
+        <Link href="https://www.ftc.go.kr/bizCommPop.do?wrkr_no=6108803078">통신판매업신고: 2023-서울강남-4541</Link>
       </section>
     </div>
 
-    <div>
-      <div class={flex({ align: 'center', gap: '16px' })}>
-        <Link href="/pencil">
-          <Icon style={css.raw({ color: { _hover: 'gray.900' } })} icon={IconEffit} />
-        </Link>
+    <div
+      class={flex({
+        'align': 'center',
+        'gap': '12px',
+        'color': 'gray.500',
+        '&>a': { size: '34px', display: 'flex', justifyContent: 'center', alignItems: 'center' },
+      })}
+    >
+      <Link href="https://www.instagram.com/withglyph">
+        <Icon style={css.raw({ color: { _hover: '[#E4405F]' } })} icon={IconInstagram} />
+      </Link>
 
-        <Link href="https://twitter.com/pencil_site">
-          <Icon style={css.raw({ color: { _hover: '[#1DA1F2]' } })} icon={IconTwitter} />
-        </Link>
-
-        <Link href="https://www.instagram.com/penxle.team/">
-          <Icon style={css.raw({ color: { _hover: '[#E4405F]' } })} icon={IconInstagram} />
-        </Link>
-
-        <Link href="https://github.com/penxle">
-          <Icon style={css.raw({ color: { _hover: '[#181717]' } })} icon={IconGithub} />
-        </Link>
-      </div>
+      <Link href="https://twitter.com/withglyph">
+        <Icon style={css.raw({ color: { _hover: '[#1DA1F2]' } })} icon={IconTwitter} />
+      </Link>
+      <Link href="https://github.com/withglyph">
+        <Icon style={css.raw({ color: { _hover: '[#181717]' } })} icon={IconGithub} />
+      </Link>
     </div>
   </div>
 </footer>
