592ac90c1d6cb3499d0e8a204de3935d012e4957
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-04-02 09:59:38 +0000

    개인 추천 피드 재구현 (#1649)

--- apps/penxle.com/src/lib/server/graphql/schemas/feed.ts
+++ apps/penxle.com/src/lib/server/graphql/schemas/feed.ts
@@ -1,3 +1,16 @@
+import { and, desc, eq, inArray } from 'drizzle-orm';
+import * as R from 'radash';
+import { database, Posts, PostTags, PostViews, Spaces, TagFollows, UserPersonalIdentities } from '$lib/server/database';
+import { elasticSearch, indexName } from '$lib/server/search';
+import {
+  getMutedSpaceIds,
+  getMutedTagIds,
+  isAdulthood,
+  isGte15,
+  makeQueryContainers,
+  searchResultToIds,
+  useFirstRow,
+} from '$lib/server/utils';
 import { builder } from '../builder';
 import { Post } from './post';
 import { Tag } from './tag';
@@ -9,8 +22,149 @@ import { Tag } from './tag';
 builder.queryFields((t) => ({
   recommendFeed: t.field({
     type: [Post],
-    resolve: async () => {
-      return [];
+    resolve: async (_, __, context) => {
+      const searchResult = await (async () => {
+        if (context.session) {
+          const [mutedTagIds, mutedSpaceIds, followingTagIds, viewedPostIds, userBirthday] = await Promise.all([
+            getMutedTagIds({ userId: context.session.userId }),
+            getMutedSpaceIds({ userId: context.session.userId }),
+            database
+              .select({ tagId: TagFollows.tagId })
+              .from(TagFollows)
+              .where(eq(TagFollows.userId, context.session.userId))
+              .then((rows) => rows.map((row) => row.tagId)),
+            database
+              .select({ postId: PostViews.postId })
+              .from(PostViews)
+              .where(eq(PostViews.userId, context.session.userId))
+              .orderBy(desc(PostViews.viewedAt))
+              .limit(100)
+              .then((rows) => rows.map((row) => row.postId)),
+            database
+              .select({ birthday: UserPersonalIdentities.birthday })
+              .from(UserPersonalIdentities)
+              .where(eq(UserPersonalIdentities.userId, context.session.userId))
+              .then(useFirstRow)
+              .then((row) => row?.birthday),
+          ]);
+
+          const viewedTagIds = await database
+            .select({ tagId: PostTags.tagId })
+            .from(PostTags)
+            .where(inArray(PostTags.postId, viewedPostIds))
+            .groupBy(PostTags.tagId)
+            .then((rows) => rows.map((row) => row.tagId));
+
+          const allowedAgeRating = userBirthday
+            ? R.sift([isAdulthood(userBirthday) && 'R19', isGte15(userBirthday) && 'R15', 'ALL'])
+            : ['ALL'];
+
+          return elasticSearch.search({
+            index: indexName('posts'),
+            query: {
+              function_score: {
+                query: {
+                  bool: {
+                    must_not: makeQueryContainers([
+                      {
+                        query: { terms: { ['tags.id']: mutedTagIds } },
+                        condition: mutedTagIds.length > 0,
+                      },
+                      {
+                        query: { terms: { spaceId: mutedSpaceIds } },
+                        condition: mutedSpaceIds.length > 0,
+                      },
+                      {
+                        query: {
+                          ids: { values: viewedPostIds },
+                        },
+                      },
+                    ]),
+                    should: makeQueryContainers([
+                      {
+                        query: { rank_feature: { field: 'reputation' } },
+                      },
+                      {
+                        query: { terms: { ['tags.id']: followingTagIds } },
+                        condition: followingTagIds.length > 0,
+                      },
+                      {
+                        query: {
+                          terms: {
+                            ['tags.id']: viewedTagIds,
+                            boost: 0.2,
+                          },
+                        },
+                        condition: viewedTagIds.length > 0,
+                      },
+                    ]),
+                    filter: [{ terms: { ageRating: allowedAgeRating } }],
+                  },
+                },
+                functions: [
+                  {
+                    random_score: { seed: Math.floor(Math.random() * 1000), field: '_seq_no' },
+                  },
+                  {
+                    exp: {
+                      publishedAt: {
+                        scale: '30d',
+                        offset: '7d',
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+
+            size: 6,
+          });
+        } else {
+          return elasticSearch.search({
+            index: indexName('posts'),
+            query: {
+              function_score: {
+                query: {
+                  bool: {
+                    should: [{ rank_feature: { field: 'reputation', boost: 2 } }],
+                    filter: [{ term: { ageRating: 'ALL' } }],
+                  },
+                },
+                functions: [
+                  {
+                    random_score: { seed: Math.floor(Math.random() * 1000), field: '_seq_no' },
+                  },
+                  {
+                    exp: {
+                      publishedAt: {
+                        scale: '30d',
+                        offset: '7d',
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+
+            size: 6,
+          });
+        }
+      })();
+
+      return await database
+        .select({ Posts })
+        .from(Posts)
+        .innerJoin(Spaces, eq(Posts.spaceId, Spaces.id))
+        .where(
+          and(
+            inArray(Posts.id, searchResultToIds(searchResult)),
+            eq(Posts.state, 'PUBLISHED'),
+            eq(Posts.visibility, 'PUBLIC'),
+            eq(Spaces.state, 'ACTIVE'),
+            eq(Spaces.visibility, 'PUBLIC'),
+          ),
+        )
+        .then((rows) => rows.map((row) => row.Posts));
     },
   }),
 
