54f835012ec0529cd577a22c675a779ebcc77b50
devunt <337728+devunt@users.noreply.github.com>
2024-04-03 07:12:31 +0000

    graphql 스키마 임포트 버그 재수정 (#1662)
    일단 다시 원래대로 돌림...

--- apps/penxle.com/src/lib/server/graphql/schemas/index.ts
+++ apps/penxle.com/src/lib/server/graphql/schemas/index.ts
@@ -1,26 +1,24 @@
-import { building, dev } from '$app/environment';
-import { builder } from '../builder';
+import './bookmark';
+import './collection';
+import './comment';
+import './embed';
+import './enums';
+import './feed';
+import './file';
+import './image';
+import './internal';
+import './notification';
+import './playground';
+import './point';
+import './post';
+import './revenue';
+import './search';
+import './space';
+import './tag';
+import './user';
 
-if (!building) {
-  await import('./bookmark');
-  await import('./collection');
-  await import('./comment');
-  await import('./embed');
-  await import('./enums');
-  await import('./feed');
-  await import('./file');
-  await import('./image');
-  await import('./internal');
-  await import('./notification');
-  await import('./playground');
-  await import('./point');
-  await import('./post');
-  await import('./revenue');
-  await import('./search');
-  await import('./space');
-  await import('./tag');
-  await import('./user');
-}
+import { dev } from '$app/environment';
+import { builder } from '../builder';
 
 export const schema = builder.toSchema();
 
