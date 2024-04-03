b68dbcefb97ff8d43c2fa0892dc5b2f81556aa04
devunt <337728+devunt@users.noreply.github.com>
2024-04-03 06:35:04 +0000

    빌드시에는 graphql schema 임포트하지 않도록 함 (#1661)
    빌드 안 되는 버그 수정함 (아마도)

--- apps/penxle.com/src/lib/server/graphql/schemas/index.ts
+++ apps/penxle.com/src/lib/server/graphql/schemas/index.ts
@@ -1,24 +1,26 @@
-import { dev } from '$app/environment';
+import { building, dev } from '$app/environment';
 import { builder } from '../builder';
 
-await import('./bookmark');
-await import('./collection');
-await import('./comment');
-await import('./embed');
-await import('./enums');
-await import('./feed');
-await import('./file');
-await import('./image');
-await import('./internal');
-await import('./notification');
-await import('./playground');
-await import('./point');
-await import('./post');
-await import('./revenue');
-await import('./search');
-await import('./space');
-await import('./tag');
-await import('./user');
+if (!building) {
+  await import('./bookmark');
+  await import('./collection');
+  await import('./comment');
+  await import('./embed');
+  await import('./enums');
+  await import('./feed');
+  await import('./file');
+  await import('./image');
+  await import('./internal');
+  await import('./notification');
+  await import('./playground');
+  await import('./point');
+  await import('./post');
+  await import('./revenue');
+  await import('./search');
+  await import('./space');
+  await import('./tag');
+  await import('./user');
+}
 
 export const schema = builder.toSchema();
 
