c31cd290902fcaca5b521df1a13784deaa58c8d5
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-04-11 13:33:23 +0000

    TextInput left icon의 레이아웃이 겹치는 문제 수정 (#1728)

--- apps/penxle.com/src/lib/components/v2/forms/TextInput.svelte
+++ apps/penxle.com/src/lib/components/v2/forms/TextInput.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { getFormContext } from '$lib/form';
-  import { css, cx, sva } from '$styled-system/css';
+  import { css, cva, cx } from '$styled-system/css';
   import { flex } from '$styled-system/patterns';
   import type { HTMLInputAttributes } from 'svelte/elements';
   import type { RecipeVariant, RecipeVariantProps, SystemStyleObject } from '$styled-system/types';
@@ -19,8 +19,6 @@
     blur: FocusEvent & { currentTarget: HTMLInputElement };
   };
 
-  let leftIconEl: HTMLElement | null = null;
-
   const { field } = getFormContext();
 
   if (field) {
@@ -28,102 +26,69 @@
   }
 
   type Variants = RecipeVariant<typeof recipe>;
-  const recipe = sva({
-    className: 'input',
-    slots: ['root', 'rightIcon'],
+  const recipe = cva({
     base: {
-      root: {
-        borderWidth: '1px',
-        borderColor: { base: 'gray.50', _enabled: { _hover: 'gray.100', _focus: 'gray.400' } },
-        fontSize: '16px',
-        fontWeight: 'medium',
-        width: 'full',
-        backgroundColor: { base: 'gray.50', _enabled: { _hover: 'gray.100', _focus: 'gray.5' }, _disabled: 'gray.100' },
-        transition: 'common',
-        _disabled: {
-          color: 'gray.400',
-          _placeholder: { color: 'gray.400' },
-        },
-        _invalid: {
-          borderColor: '[red.600!]',
-          color: 'red.600',
-          _placeholder: { color: 'red.600' },
-        },
+      'display': 'flex',
+      'alignItems': 'center',
+      'borderWidth': '1px',
+      'borderColor': { base: 'gray.50', _enabled: { _hover: 'gray.100', _focus: 'gray.400' } },
+      'fontSize': '16px',
+      'fontWeight': 'medium',
+      'backgroundColor': {
+        base: 'gray.50',
+        _enabled: { _hover: 'gray.100', _focus: 'gray.5' },
+        _disabled: 'gray.100',
       },
-      rightIcon: { position: 'absolute', insetY: '0', display: 'flex', alignItems: 'center', justifyContent: 'center' },
+      'transition': 'common',
+      '_disabled': {
+        color: 'gray.400',
+        _placeholder: { color: 'gray.400' },
+      },
+      '&:has(input[aria-invalid])': {
+        borderColor: '[red.600!]',
+        color: 'red.600',
+        _placeholder: { color: 'red.600' },
+      },
     },
     variants: {
       size: {
         xs: {
-          root: {
-            paddingX: '12px',
-            paddingY: '8px',
-            fontSize: '14px',
-            height: '34px',
-          },
-          rightIcon: { right: '12px' },
+          paddingX: '12px',
+          paddingY: '8px',
+          fontSize: '14px',
+          height: '34px',
         },
         sm: {
-          root: {
-            paddingX: '12px',
-            paddingY: '10px',
-            fontSize: '14px',
-            height: '44px',
-          },
-          rightIcon: { right: '12px' },
+          paddingX: '12px',
+          paddingY: '10px',
+          fontSize: '14px',
+          height: '44px',
         },
         md: {
-          root: {
-            padding: '14px',
-            height: '48px',
-          },
-          rightIcon: { right: '14px' },
+          padding: '14px',
+          height: '48px',
         },
         lg: {
-          root: {
-            padding: '14px',
-            height: '52px',
-          },
-          rightIcon: { right: '14px' },
+          padding: '14px',
+          height: '52px',
         },
       },
     },
   });
-
-  $: classes = recipe({ size });
 </script>
 
-<div class={flex({ align: 'center', position: 'relative' })}>
+<div class={cx(recipe({ size }), css(style))}>
   {#if 'left-icon' in $$slots}
-    <div
-      bind:this={leftIconEl}
-      style:left={size === 'xs' || size === 'sm' ? '12px' : '14px'}
-      class={css({
-        position: 'absolute',
-        insetY: '0',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      })}
-    >
+    <label class={flex({ align: 'center', marginRight: '6px' })} for={name}>
       <slot name="left-icon" />
-    </div>
+    </label>
   {/if}
+
   <input
     bind:this={inputEl}
     id={name}
     {name}
-    style:padding-left={leftIconEl &&
-      `calc(${leftIconEl.getBoundingClientRect().width + 6}px + ${leftIconEl.style.left})`}
-    class={cx(
-      classes.root,
-      css(
-        style,
-        'right-icon' in $$slots && {
-          paddingRight: size === 'xs' ? '32px' : size === 'lg' ? '40px' : '34px',
-        },
-      ),
-    )}
+    class={css({ flexGrow: '1' })}
     type="text"
     on:input
     on:keydown
@@ -131,9 +96,10 @@
     bind:value
     {...$$restProps}
   />
+
   {#if 'right-icon' in $$slots}
-    <div class={classes.rightIcon}>
+    <label class={flex({ align: 'center', marginLeft: '6px' })} for={name}>
       <slot name="right-icon" />
-    </div>
+    </label>
   {/if}
 </div>
