461526ec863486781cc078aac212133bd832f874
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-04-13 02:23:25 +0000

    검색 결과가 안 뜨는 오류 수정 (#1825)

--- apps/penxle.com/src/lib/server/graphql/schemas/search.ts
+++ apps/penxle.com/src/lib/server/graphql/schemas/search.ts
@@ -33,8 +33,8 @@ builder.objectType(SearchResult, {
 
     posts: t.field({
       type: [Post],
-      resolve: async () => {
-        return [];
+      resolve: ({ result }) => {
+        return result.hits.hits.map((hit) => hit._id);
       },
     }),
   }),
