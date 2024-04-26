8a4ac9f4bc9704480a075a15406bcdef09ceddc8
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-04-26 06:12:23 +0000

    dayjs 날짜 포맷 변경 (#1969)
    날짜 사이 공백 제거, 날짜와 시간 사이에 `.` 추가

--- apps/website/src/lib/datetime/format-as/index.ts
+++ apps/website/src/lib/datetime/format-as/index.ts
@@ -2,11 +2,11 @@ import type { PluginFunc } from 'dayjs';
 
 export const formatAs: PluginFunc = (_, Dayjs) => {
   Dayjs.prototype.formatAsDate = function () {
-    return this.format('YYYY. MM. DD');
+    return this.format('YYYY.MM.DD');
   };
 
   Dayjs.prototype.formatAsDateTime = function () {
-    return this.format('YYYY. MM. DD HH:mm');
+    return this.format('YYYY.MM.DD. HH:mm');
   };
 
   Dayjs.prototype.formatAsTime = function () {
