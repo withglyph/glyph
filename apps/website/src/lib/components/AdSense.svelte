62b482ec40c1f30456d7474d6ab75f5f5d36cde3
devunt <337728+devunt@users.noreply.github.com>
2024-04-28 06:54:05 +0000

    애드센스 테스트 (#1996)

--- apps/website/src/lib/components/AdSense.svelte
+++ apps/website/src/lib/components/AdSense.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import IconHelpLine from '~icons/glyph/help-line';
   import { css } from '$styled-system/css';
+  import { flex } from '$styled-system/patterns';
+  import Icon from './Icon.svelte';
+  import Link from './Link.svelte';
+  import Tooltip from './Tooltip.svelte';
   import type { SystemStyleObject } from '$styled-system/types';
 
   export let style: SystemStyleObject | undefined = undefined;
   export let slotId: string;
+
+  onMount(() => {
+    // @ts-expect-error - Google AdSense
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  });
 </script>
 
-<div class={css({ backgroundColor: 'gray.200' }, style)}>
+<div class={css({ position: 'relative', backgroundColor: 'gray.200' }, style)}>
   <ins
     style="display:block"
     class="adsbygoogle"
@@ -15,4 +26,27 @@
     data-ad-slot={slotId}
     data-full-width-responsive="true"
   ></ins>
+
+  <div
+    class={flex({
+      position: 'absolute',
+      top: '0',
+      right: '0',
+      justify: 'flex-end',
+      align: 'center',
+      gap: '2px',
+      translate: 'auto',
+      translateY: '-full',
+    })}
+  >
+    <Link class={css({ fontSize: '12px', color: 'gray.400' })} external href="https://withglyph.com/glyph/1109158689">
+      광고 테스트를 진행하고 있어요
+    </Link>
+    <Tooltip message="일부 이용자 대상으로 5월 22일까지 랜덤하게 노출돼요">
+      <Icon
+        style={css.raw({ 'color': 'gray.400', 'size': '14px', '& *': { strokeWidth: '[1]' } })}
+        icon={IconHelpLine}
+      />
+    </Tooltip>
+  </div>
 </div>
