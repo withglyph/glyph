9f4fd1477a743babb2a2784eb7aa31764151b56f
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-04-29 04:09:30 +0000

    애드센스 툴팁 메시지 잘못들어가있던 문제 수정 (#2013)

--- apps/website/src/lib/components/AdSense.svelte
+++ apps/website/src/lib/components/AdSense.svelte
@@ -39,7 +39,7 @@
     <Link class={css({ fontSize: '12px', color: 'gray.400' })} external href="https://withglyph.com/glyph/1109158689">
       광고 테스트를 진행하고 있어요
     </Link>
-    <Tooltip message="일부 이용자 대상으로 5월 22일까지 랜덤하게 노출돼요" offset={4} placement="top">
+    <Tooltip message="일부 이용자 대상으로 5월 12일까지 랜덤하게 노출돼요" offset={4} placement="top">
       <Icon
         style={css.raw({ 'color': 'gray.400', 'size': '12px', '& *': { strokeWidth: '[1]' } })}
         icon={IconHelpLine}
