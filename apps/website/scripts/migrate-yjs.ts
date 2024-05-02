b60d8bf8bcc0d3b496e810dfef87fe84ad8273f1
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-05-02 07:57:00 +0000

    yjs 마이그레이션 스크립트 병렬 실행 리팩터링 (#2044)
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/OIyHyV8T3If5OJv5ys3X/3c55fa21-c3fc-4fae-9bc3-a559372ded2d.png)

--- apps/website/scripts/migrate-yjs.ts
+++ apps/website/scripts/migrate-yjs.ts
@@ -41,188 +41,194 @@ for (let i = 0; ; i++) {
     break;
   }
 
-  for (const post of posts) {
-    await database.transaction(
-      async (tx) => {
-        // 각 포스트의 마지막 리비전 가져옴
-        const lastRevisions = await tx
-          .select({
-            id: PostRevisions.id,
-            title: PostRevisions.title,
-            subtitle: PostRevisions.subtitle,
-            freeContentId: PostRevisions.freeContentId,
-            paidContentId: PostRevisions.paidContentId,
-            price: PostRevisions.price,
-            paragraphIndent: PostRevisions.paragraphIndent,
-            paragraphSpacing: PostRevisions.paragraphSpacing,
-          })
-          .from(PostRevisions)
-          .where(eq(PostRevisions.postId, post.id))
-          .orderBy(asc(PostRevisions.createdAt))
-          .limit(1);
+  console.time(`Migrated ${i * 100 + posts.length} posts`);
 
-        let title: string | null = null;
-        let subtitle: string | null = null;
-        let content: JSONContent;
+  await Promise.all(
+    posts.map((post) =>
+      database.transaction(
+        async (tx) => {
+          // 각 포스트의 마지막 리비전 가져옴
+          const lastRevisions = await tx
+            .select({
+              id: PostRevisions.id,
+              title: PostRevisions.title,
+              subtitle: PostRevisions.subtitle,
+              freeContentId: PostRevisions.freeContentId,
+              paidContentId: PostRevisions.paidContentId,
+              price: PostRevisions.price,
+              paragraphIndent: PostRevisions.paragraphIndent,
+              paragraphSpacing: PostRevisions.paragraphSpacing,
+            })
+            .from(PostRevisions)
+            .where(eq(PostRevisions.postId, post.id))
+            .orderBy(asc(PostRevisions.createdAt))
+            .limit(1);
 
-        // 리비전이 없음 -> 빈 yDoc 생성
-        if (lastRevisions.length === 0) {
-          content = createEmptyTiptapDocumentNode().toJSON();
-        } else {
-          const [lastRevision] = lastRevisions;
+          let title: string | null = null;
+          let subtitle: string | null = null;
+          let content: JSONContent;
 
-          // 마지막 리비전 기준으로 yjs 초기 상태 생성 시작
+          // 리비전이 없음 -> 빈 yDoc 생성
+          if (lastRevisions.length === 0) {
+            content = createEmptyTiptapDocumentNode().toJSON();
+          } else {
+            const [lastRevision] = lastRevisions;
 
-          // document 재구축 시작
-          const nodes = [];
+            // 마지막 리비전 기준으로 yjs 초기 상태 생성 시작
 
-          if (lastRevision.freeContentId) {
-            const [content] = await tx
-              .select({ data: PostRevisionContents.data })
-              .from(PostRevisionContents)
-              .where(eq(PostRevisionContents.id, lastRevision.freeContentId));
+            // document 재구축 시작
+            const nodes = [];
 
-            nodes.push(...content.data);
+            if (lastRevision.freeContentId) {
+              const [content] = await tx
+                .select({ data: PostRevisionContents.data })
+                .from(PostRevisionContents)
+                .where(eq(PostRevisionContents.id, lastRevision.freeContentId));
+
+              nodes.push(...content.data);
+            }
+
+            nodes.push({
+              type: 'access_barrier',
+              attrs: {
+                price: lastRevision.price,
+              },
+            });
+
+            if (lastRevision.paidContentId) {
+              const [content] = await tx
+                .select({ data: PostRevisionContents.data })
+                .from(PostRevisionContents)
+                .where(eq(PostRevisionContents.id, lastRevision.paidContentId));
+
+              nodes.push(...content.data);
+            }
+
+            const _proseMirrorDoc = {
+              type: 'doc',
+              content: [
+                {
+                  type: 'document',
+                  attrs: {
+                    documentParagraphIndent: lastRevision.paragraphIndent / 100,
+                    documentParagraphSpacing: lastRevision.paragraphSpacing / 100,
+                  },
+                  content: nodes,
+                },
+              ],
+            };
+            // document 재구축 끝
+
+            // document 구조 새 스키마에 맞춰 마이그레이션함
+            content = migrateContent(_proseMirrorDoc);
+
+            title = lastRevision.title;
+            subtitle = lastRevision.subtitle;
           }
 
-          nodes.push({
-            type: 'access_barrier',
-            attrs: {
-              price: lastRevision.price,
-            },
+          // 마이그레이션한 document로 yjs document 생성함
+          const yDoc = prosemirrorJSONToYDoc(schema, content, 'content');
+          yDoc.getText('title').insert(0, title ?? '');
+          yDoc.getText('subtitle').insert(0, subtitle ?? '');
+
+          // 초기 상태 생성 & 저장함
+          const update = Y.encodeStateAsUpdateV2(yDoc);
+          const vector = Y.encodeStateVector(yDoc);
+          const { text, characters, images, files } = getMetadataFromTiptapDocument(content);
+
+          await tx.insert(PostContentStates).values({
+            postId: post.id,
+            update,
+            vector,
+            upToSeq: 0n,
+            title,
+            subtitle,
+            content,
+            text,
+            characters,
+            images,
+            files,
           });
 
-          if (lastRevision.paidContentId) {
-            const [content] = await tx
-              .select({ data: PostRevisionContents.data })
-              .from(PostRevisionContents)
-              .where(eq(PostRevisionContents.id, lastRevision.paidContentId));
+          // 초기 상태의 스냅샷 생성 & 저장함
+          const snapshot = Y.encodeSnapshotV2(Y.snapshot(yDoc));
+          await tx.insert(PostContentSnapshots).values({
+            postId: post.id,
+            data: snapshot,
+          });
 
-            nodes.push(...content.data);
-          }
+          // 마지막 리비전 기준으로 yjs 초기 상태 생성 끝
 
-          const _proseMirrorDoc = {
-            type: 'doc',
-            content: [
-              {
-                type: 'document',
-                attrs: {
-                  documentParagraphIndent: lastRevision.paragraphIndent / 100,
-                  documentParagraphSpacing: lastRevision.paragraphSpacing / 100,
-                },
-                content: nodes,
-              },
-            ],
-          };
-          // document 재구축 끝
-
-          // document 구조 새 스키마에 맞춰 마이그레이션함
-          content = migrateContent(_proseMirrorDoc);
-
-          title = lastRevision.title;
-          subtitle = lastRevision.subtitle;
-        }
-
-        // 마이그레이션한 document로 yjs document 생성함
-        const yDoc = prosemirrorJSONToYDoc(schema, content, 'content');
-        yDoc.getText('title').insert(0, title ?? '');
-        yDoc.getText('subtitle').insert(0, subtitle ?? '');
-
-        // 초기 상태 생성 & 저장함
-        const update = Y.encodeStateAsUpdateV2(yDoc);
-        const vector = Y.encodeStateVector(yDoc);
-        const { text, characters, images, files } = getMetadataFromTiptapDocument(content);
-
-        await tx.insert(PostContentStates).values({
-          postId: post.id,
-          update,
-          vector,
-          upToSeq: 0n,
-          title,
-          subtitle,
-          content,
-          text,
-          characters,
-          images,
-          files,
-        });
-
-        // 초기 상태의 스냅샷 생성 & 저장함
-        const snapshot = Y.encodeSnapshotV2(Y.snapshot(yDoc));
-        await tx.insert(PostContentSnapshots).values({
-          postId: post.id,
-          data: snapshot,
-        });
-
-        // 마지막 리비전 기준으로 yjs 초기 상태 생성 끝
-
-        // PUBLISHED 혹은 ARCHIVED 상태가 아닌 리비전 삭제
-        await tx
-          .delete(PostRevisions)
-          .where(and(eq(PostRevisions.postId, post.id), notInArray(PostRevisions.kind, ['PUBLISHED', 'ARCHIVED'])));
-
-        // 남아있는 리비전 다시 가져옴
-        const revisions = await tx
-          .select({
-            id: PostRevisions.id,
-            freeContentId: PostRevisions.freeContentId,
-            paidContentId: PostRevisions.paidContentId,
-            paragraphIndent: PostRevisions.paragraphIndent,
-            paragraphSpacing: PostRevisions.paragraphSpacing,
-          })
-          .from(PostRevisions)
-          .where(eq(PostRevisions.postId, post.id));
-
-        for (const revision of revisions) {
-          // attributes 마이그레이션
+          // PUBLISHED 혹은 ARCHIVED 상태가 아닌 리비전 삭제
           await tx
-            .update(PostRevisions)
-            .set({
-              attributes: {
-                documentParagraphIndent: revision.paragraphIndent / 100,
-                documentParagraphSpacing: revision.paragraphSpacing / 100,
-              },
+            .delete(PostRevisions)
+            .where(and(eq(PostRevisions.postId, post.id), notInArray(PostRevisions.kind, ['PUBLISHED', 'ARCHIVED'])));
+
+          // 남아있는 리비전 다시 가져옴
+          const revisions = await tx
+            .select({
+              id: PostRevisions.id,
+              freeContentId: PostRevisions.freeContentId,
+              paidContentId: PostRevisions.paidContentId,
+              paragraphIndent: PostRevisions.paragraphIndent,
+              paragraphSpacing: PostRevisions.paragraphSpacing,
             })
-            .where(eq(PostRevisions.id, revision.id));
+            .from(PostRevisions)
+            .where(eq(PostRevisions.postId, post.id));
 
-          // 컨텐츠 마이그레이션
-          if (revision.freeContentId) {
-            const [content] = await tx
-              .select({ data: PostRevisionContents.data })
-              .from(PostRevisionContents)
-              .where(eq(PostRevisionContents.id, revision.freeContentId));
+          await Promise.all(
+            revisions.map(async (revision) => {
+              // attributes 마이그레이션
+              await tx
+                .update(PostRevisions)
+                .set({
+                  attributes: {
+                    documentParagraphIndent: revision.paragraphIndent / 100,
+                    documentParagraphSpacing: revision.paragraphSpacing / 100,
+                  },
+                })
+                .where(eq(PostRevisions.id, revision.id));
 
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            const nodes = migrateContent({ type: 'document', content: content.data }).content!;
-            const id = await makePostContentId(tx, nodes);
-            if (id) {
-              await tx.update(PostRevisions).set({ freeContentId: id }).where(eq(PostRevisions.id, revision.id));
-            }
-          }
+              // 컨텐츠 마이그레이션
+              if (revision.freeContentId) {
+                const [content] = await tx
+                  .select({ data: PostRevisionContents.data })
+                  .from(PostRevisionContents)
+                  .where(eq(PostRevisionContents.id, revision.freeContentId));
 
-          if (revision.paidContentId) {
-            const [content] = await tx
-              .select({ data: PostRevisionContents.data })
-              .from(PostRevisionContents)
-              .where(eq(PostRevisionContents.id, revision.paidContentId));
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                const nodes = migrateContent({ type: 'document', content: content.data }).content!;
+                const id = await makePostContentId(tx, nodes);
+                if (id) {
+                  await tx.update(PostRevisions).set({ freeContentId: id }).where(eq(PostRevisions.id, revision.id));
+                }
+              }
 
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            const nodes = migrateContent({ type: 'document', content: content.data }).content!;
-            const id = await makePostContentId(tx, nodes);
-            if (id) {
-              await tx.update(PostRevisions).set({ paidContentId: id }).where(eq(PostRevisions.id, revision.id));
-            }
-          }
-        }
+              if (revision.paidContentId) {
+                const [content] = await tx
+                  .select({ data: PostRevisionContents.data })
+                  .from(PostRevisionContents)
+                  .where(eq(PostRevisionContents.id, revision.paidContentId));
 
-        // 마이그레이션 완료 마크
-        await tx.update(Posts).set({ migrated: true }).where(eq(Posts.id, post.id));
-      },
-      { isolationLevel: 'repeatable read' },
-    );
-  }
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                const nodes = migrateContent({ type: 'document', content: content.data }).content!;
+                const id = await makePostContentId(tx, nodes);
+                if (id) {
+                  await tx.update(PostRevisions).set({ paidContentId: id }).where(eq(PostRevisions.id, revision.id));
+                }
+              }
+            }),
+          );
 
-  console.log(`Migrated ${i * 100 + posts.length} posts`);
+          // 마이그레이션 완료 마크
+          await tx.update(Posts).set({ migrated: true }).where(eq(Posts.id, post.id));
+        },
+        { isolationLevel: 'repeatable read' },
+      ),
+    ),
+  );
+
+  console.timeEnd(`Migrated ${i * 100 + posts.length} posts`);
 }
 
 console.log('Migration complete');
