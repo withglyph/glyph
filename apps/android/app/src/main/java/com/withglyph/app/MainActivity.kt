453e0ec801a678a3c897fde63fb3d24ccf0c8fcd
devunt <337728+devunt@users.noreply.github.com>
2024-05-03 10:55:24 +0000

    안드로이드 프로젝트를 ios 프로젝트 구조에 맞춤 (#2055)

--- apps/android/app/src/main/java/com/withglyph/app/MainActivity.kt
+++ apps/android/app/src/main/java/com/withglyph/app/MainActivity.kt
@@ -4,12 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +14,7 @@ class MainActivity : ComponentActivity() {
 
     enableEdgeToEdge()
     setContent {
-      Box(modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars)) {
-        ChromeWebView(modifier = Modifier.fillMaxSize())
-      }
+      MainView()
     }
   }
 }
