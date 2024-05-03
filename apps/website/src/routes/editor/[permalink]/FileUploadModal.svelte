81d3d3b2b3f24d733f4c873d728950c3ef963c46
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-05-03 10:53:19 +0000

    파일 업로드 모달 디자인 반영 및 드래그앤드롭 업로드 기능 추가 (#2054)
    - 퍼블리싱
    - 여러 파일 한번에 올릴 수 있도록 수정
    - 파일마다 로딩상태 추가해 로딩 상태 보여줌
    - 드래그 앤 드롭으로 파일 추가할 수 있도록 함

--- apps/website/src/routes/editor/[permalink]/FileUploadModal.svelte
+++ apps/website/src/routes/editor/[permalink]/FileUploadModal.svelte
@@ -2,12 +2,15 @@
   import ky from 'ky';
   import { nanoid } from 'nanoid';
   import { writable } from 'svelte/store';
-  import FilePlusIcon from '~icons/tabler/file-plus';
+  import IconCircleCheck from '~icons/tabler/circle-check';
+  import IconFileSymlink from '~icons/tabler/file-symlink';
+  import IconTrash from '~icons/tabler/trash';
   import { graphql } from '$glitch';
   import { Icon } from '$lib/components';
-  import { Modal } from '$lib/components/v2';
-  import Button from '$lib/components/v2/Button.svelte';
+  import { RingSpinner } from '$lib/components/spinners';
+  import { Button, Modal } from '$lib/components/v2';
   import { css } from '$styled-system/css';
+  import { flex } from '$styled-system/patterns';
   import { getEditorContext } from './context';
 
   export let open = false;
@@ -32,9 +35,10 @@
   const { state } = getEditorContext();
 
   let inputEl: HTMLInputElement;
-  let files: { ref: string; file: File; id?: string }[] = [];
+  let files: { ref: string; file: File; id?: string; loading: boolean }[] = [];
 
   let uploadingCount = writable(0);
+  let dragging: EventTarget | null = null;
 
   const handleSelect = () => {
     if (!inputEl.files?.length) {
@@ -54,7 +58,7 @@
       uploadingCount.update((count) => count + 1);
 
       const ref = nanoid();
-      files = [...files, { ref, file }];
+      files = [...files, { ref, file, loading: true }];
 
       const { key, presignedUrl } = await prepareFileUpload();
       await ky.put(presignedUrl, { body: file });
@@ -63,6 +67,7 @@
       files = files.map((f) => (f.ref === ref ? { ...f, id: resp.id } : f));
     } finally {
       uploadingCount.update((count) => count - 1);
+      files = files.map((f) => (f.file === file ? { ...f, loading: false } : f));
     }
   };
 
@@ -79,39 +84,133 @@
     }
     chain.run();
 
+    files = [];
     open = false;
   };
 </script>
 
-<Modal bind:open>
+<Modal
+  style={css.raw({ paddingTop: '0' })}
+  actionStyle={css.raw({ borderTopWidth: '1px', borderTopColor: 'gray.100' })}
+  bind:open
+>
   <svelte:fragment slot="title">파일 선택</svelte:fragment>
-  <button
+
+  <div
     class={css({
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      gap: '4px',
-      width: 'full',
-      height: '200px',
-      borderWidth: '2px',
-      borderStyle: 'dashed',
-      borderColor: 'gray.300',
+      position: 'sticky',
+      top: '0',
+      marginX: '-20px',
+      paddingTop: '16px',
+      paddingX: '20px',
+      paddingBottom: '8px',
+      backgroundColor: 'gray.5',
+      width: 'auto',
+      flexGrow: '1',
     })}
-    type="button"
-    on:click={() => inputEl.click()}
   >
-    <Icon style={css.raw({ color: 'gray.500' })} icon={FilePlusIcon} size={32} />
-    <div class={css({ fontSize: '14px', color: 'gray.500' })}>파일을 끌어다 놓거나 클릭해서 업로드해주세요</div>
-  </button>
+    <div
+      class={css(
+        {
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '8px',
+          borderWidth: '1px',
+          borderStyle: 'dashed',
+          borderColor: 'brand.400',
+          paddingX: '34px',
+          backgroundColor: 'gray.50',
+          width: 'full',
+          height: '200px',
+          transition: 'common',
+        },
+        dragging && { backgroundColor: 'brand.200' },
+      )}
+      role="button"
+      tabindex="-1"
+      on:dragenter|preventDefault|stopPropagation={({ target, dataTransfer }) => {
+        if (dataTransfer?.types.includes('Files')) dragging = target;
+      }}
+      on:dragover|preventDefault|stopPropagation={({ dataTransfer }) => {
+        if (dataTransfer) {
+          dataTransfer.dropEffect = dataTransfer.types.includes('Files') ? 'copy' : 'none';
+        }
+      }}
+      on:drop|preventDefault|stopPropagation={async ({ dataTransfer }) => {
+        dragging = null;
+        if (dataTransfer) {
+          const f = [...dataTransfer.files];
 
-  {#each files as { ref: key, file } (key)}
-    <div>{file.name}</div>
-  {/each}
+          for (const file of f) {
+            uploadFile(file);
+          }
+        }
+      }}
+      on:dragleave|preventDefault|stopPropagation={({ target }) => {
+        if (target === dragging) dragging = null;
+      }}
+    >
+      <Icon style={css.raw({ color: 'brand.400' })} icon={IconFileSymlink} size={32} />
+      <p class={css({ fontSize: '14px', color: 'gray.500' })}>파일을 드래그해 업로드해주세요</p>
+      <span class={css({ fontSize: '13px', color: 'gray.400' })}>또는</span>
+      <Button
+        style={css.raw({ backgroundColor: 'gray.5' })}
+        size="sm"
+        variant="gray-outline"
+        on:click={() => inputEl.click()}
+      >
+        파일 업로드
+      </Button>
+    </div>
 
-  <input bind:this={inputEl} class={css({ display: 'none' })} type="file" on:change={handleSelect} />
+    <!-- prettier-ignore -->
+    <p class={css({ marginTop: '16px', fontSize: '13px' })}>
+      총 <mark class={css({ color: 'brand.400' })}>{files.length}</mark>개의 파일
+      {#if $uploadingCount > 0}
+        <span class={css({ color: 'gray.400' })}>({$uploadingCount}개의 파일 업로드중..)</span>
+      {/if}
+    </p>
+  </div>
 
-  <Button slot="action" style={css.raw({ width: 'full' })} disabled={$uploadingCount > 0} on:click={handleInsert}>
+  <ul class={css({ overflowY: 'auto', scrollbar: 'hidden' })}>
+    {#each files as { ref: key, file, loading } (key)}
+      <li
+        class={flex({
+          align: 'center',
+          justify: 'space-between',
+          gap: '16px',
+          paddingY: '12px',
+          fontSize: '13px',
+          truncate: true,
+        })}
+      >
+        <div class={flex({ align: 'center', gap: '4px', truncate: true })}>
+          {#if loading}
+            <RingSpinner style={css.raw({ size: '16px', color: 'gray.300' })} />
+          {:else}
+            <Icon style={css.raw({ color: 'brand.400' })} icon={IconCircleCheck} />
+          {/if}
+          <p class={css({ truncate: true })}>{file.name}</p>
+        </div>
+
+        <button
+          class={css({ flex: 'none' })}
+          type="button"
+          on:click={() => {
+            files = files.filter((f) => f.ref !== key);
+          }}
+        >
+          <Icon style={css.raw({ color: 'gray.600' })} icon={IconTrash} size={20} />
+        </button>
+      </li>
+    {/each}
+  </ul>
+
+  <input bind:this={inputEl} class={css({ display: 'none' })} multiple type="file" on:change={handleSelect} />
+
+  <Button slot="action" style={css.raw({ width: 'full' })} loading={$uploadingCount > 0} on:click={handleInsert}>
     삽입
   </Button>
 </Modal>
