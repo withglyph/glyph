52feecca1ee661e173ce41372486c7993f40dac3
devunt <337728+devunt@users.noreply.github.com>
2024-05-04 08:05:31 +0000

    팁탭 노드 뷰 SSR 동작 방식 문서화 (#2060)
    문서화되지 않은 특수 동작에 의존하던 기능에 추가적으로 설명 주석 달았음

--- apps/website/src/lib/tiptap/lib/create.ts
+++ apps/website/src/lib/tiptap/lib/create.ts
@@ -29,6 +29,10 @@ export const createNodeView = <Options = any, Storage = any>(
           selected: false,
         });
 
+        // spell-checker:disable-next-line
+        // prosemirror-model이 ssr시에 zeed-dom을 이용하고, zeed-dom은 html attribute를 특별하게 처리하는 로직이 있어
+        // html attribute에 들어간 값을 innerHTML에 그대로 렌더링함. 아래 코드는 해당 동작에 의존함 (문서화되지 않은 기능 / 편법에 가까움)
+        // https://github.com/holtwick/zeed-dom/blob/6a2d6694ed879da9444d9b7c1826cef4b23c8a17/src/html.ts#L139-L140
         return ['node-view', mergeAttributes(HTMLAttributes, { html, style: 'white-space: normal;' })];
       }
     },
