e74ae375bfa28d91efed0a354e6016f4b687a707
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-05-09 05:57:28 +0000

    dangling PostRevisionContent 삭제 스크립트 (#2085)

---
+++ apps/website/scripts/delete-dangling-post-content.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import { and, eq, isNull, sql } from 'drizzle-orm';
+import { alias } from 'drizzle-orm/pg-core';
+import { database, inArray, PostRevisionContents, PostRevisions } from '$lib/server/database';
+
+await database.execute(sql`set session_replication_role = replica`);
+
+for (let i = 0; ; i++) {
+  console.time(`Elapsed Time`);
+
+  const RevisionsAsFreeContent = alias(PostRevisions, 'RevisionsAsFreeContent');
+  const RevisionsAsPaidContent = alias(PostRevisions, 'RevisionsAsPaidContent');
+  const posts = await database
+    .select({ id: PostRevisionContents.id })
+    .from(PostRevisionContents)
+    .leftJoin(RevisionsAsFreeContent, eq(PostRevisionContents.id, RevisionsAsFreeContent.freeContentId))
+    .leftJoin(RevisionsAsPaidContent, eq(PostRevisionContents.id, RevisionsAsPaidContent.paidContentId))
+    .where(and(isNull(RevisionsAsFreeContent.id), isNull(RevisionsAsPaidContent.id)))
+    .limit(100);
+
+  if (posts.length === 0) {
+    break;
+  }
+
+  const ids = posts.map((post) => post.id);
+  await database.delete(PostRevisionContents).where(inArray(PostRevisionContents.id, ids));
+
+  console.log(`Deleted ${i * 100 + posts.length} contents`);
+  console.timeEnd(`Elapsed Time`);
+}
+
+console.log('Done');
+process.exit(0);
