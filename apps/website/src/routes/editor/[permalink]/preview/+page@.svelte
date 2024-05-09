d591f16394854336d0e4f917e1bc5eda0b9228b7
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-05-09 12:28:14 +0000

    에디터 미리보기 헤더에 잘못들어가있는 스타일 수정 (#2104)

--- apps/website/src/routes/editor/[permalink]/preview/+page@.svelte
+++ apps/website/src/routes/editor/[permalink]/preview/+page@.svelte
@@ -5,7 +5,6 @@
   import * as Y from 'yjs';
   import IconDeviceDesktop from '~icons/tabler/device-desktop';
   import IconDeviceMobile from '~icons/tabler/device-mobile';
-  import { page } from '$app/stores';
   import FullLogo from '$assets/logos/full.svg?component';
   import { graphql } from '$glitch';
   import { Helmet, Icon } from '$lib/components';
@@ -16,8 +15,6 @@
   import type { Editor } from '@tiptap/core';
 
   let mode: 'desktop' | 'mobile' = 'desktop';
-
-  $: hideHeader = $page.url.searchParams.get('hideHeader') === 'true';
 
   $: query = graphql(`
     query EditorPermalinkPreviewPage_Query($permalink: String!) {
@@ -54,83 +51,76 @@
 
 <Helmet description="포스트 미리보기" title={`${title || '(제목 없음)'} 미리보기`} />
 
-{#if !hideHeader}
-  <header
-    class={center({
-      position: 'sticky',
-      top: '0',
-      borderBottomWidth: '1px',
-      borderBottomColor: 'gray.200',
-      paddingX: '20px',
-      paddingY: '12px',
-      height: '61px',
-      backgroundColor: 'gray.5',
-      zIndex: '10',
-    })}
-  >
-    <nav class={css({ width: 'full', maxWidth: '1200px' })}>
-      <section class={flex({ position: 'relative', justify: 'space-between', align: 'center' })}>
-        <div
-          class={flex({
-            position: 'absolute',
-            left: '0',
-            align: 'center',
-            gap: '8px',
-            marginTop: '8px',
-            marginBottom: '4px',
-            marginRight: { base: '14px', sm: '16px' },
-            transition: 'common',
-          })}
-        >
-          <FullLogo class={css({ height: '25px', color: 'gray.900' })} />
-        </div>
-        <div class={center({ flexGrow: '1' })}>
-          <button
-            class={flex({
-              align: 'center',
-              gap: '4px',
-              paddingX: '20px',
-              paddingY: '20px',
-              height: '37px',
-              zIndex: '1',
-              backgroundColor: mode === 'desktop' ? 'gray.900' : 'gray.100',
-              color: mode === 'desktop' ? 'gray.50' : 'gray.800',
-              transition: 'colors',
-            })}
-            type="button"
-            on:click={() => {
-              mode = 'desktop';
-            }}
-          >
-            <Icon icon={IconDeviceDesktop} size={16} />
-          </button>
-          <button
-            class={flex({
-              align: 'center',
-              gap: '4px',
-              paddingX: '20px',
-              paddingY: '20px',
-              height: '37px',
-              zIndex: '1',
-              backgroundColor: mode === 'mobile' ? 'gray.900' : 'gray.100',
-              color: mode === 'mobile' ? 'gray.50' : 'gray.800',
-              transition: 'colors',
-            })}
-            type="button"
-            on:click={() => {
-              mode = 'mobile';
-            }}
-          >
-            <Icon icon={IconDeviceMobile} size={20} />
-          </button>
-        </div>
-        <Button style={css.raw({ width: '68px' })} size="sm" variant="gray-outline" on:click={() => window.close()}>
-          확인
-        </Button>
-      </section>
-    </nav>
-  </header>
-{/if}
+<header
+  class={center({
+    position: 'sticky',
+    top: '0',
+    borderBottomWidth: '1px',
+    borderBottomColor: 'gray.100',
+    paddingX: '20px',
+    paddingY: '12px',
+    height: '62px',
+    backgroundColor: 'gray.5',
+    zIndex: '10',
+  })}
+>
+  <section class={flex({ position: 'relative', justify: 'space-between', align: 'center', width: 'full' })}>
+    <div
+      class={flex({
+        position: 'absolute',
+        left: '0',
+        align: 'center',
+        justify: 'space-between',
+        transition: 'common',
+        width: 'full',
+      })}
+    >
+      <FullLogo class={css({ marginTop: '20px', marginBottom: '17px', height: '25px', color: 'gray.900' })} />
+
+      <Button style={css.raw({ width: '68px' })} size="sm" variant="gray-outline" on:click={() => window.close()}>
+        확인
+      </Button>
+    </div>
+    <div class={center({ flexGrow: '1' })}>
+      <button
+        class={flex({
+          align: 'center',
+          gap: '4px',
+          paddingX: '20px',
+          paddingY: '20px',
+          height: '37px',
+          zIndex: '1',
+          backgroundColor: mode === 'desktop' ? 'gray.900' : 'gray.100',
+          color: mode === 'desktop' ? 'gray.50' : 'gray.800',
+        })}
+        type="button"
+        on:click={() => {
+          mode = 'desktop';
+        }}
+      >
+        <Icon icon={IconDeviceDesktop} size={16} />
+      </button>
+      <button
+        class={flex({
+          align: 'center',
+          gap: '4px',
+          paddingX: '20px',
+          paddingY: '20px',
+          height: '37px',
+          zIndex: '1',
+          backgroundColor: mode === 'mobile' ? 'gray.900' : 'gray.100',
+          color: mode === 'mobile' ? 'gray.50' : 'gray.800',
+        })}
+        type="button"
+        on:click={() => {
+          mode = 'mobile';
+        }}
+      >
+        <Icon icon={IconDeviceMobile} size={20} />
+      </button>
+    </div>
+  </section>
+</header>
 
 <main
   class={flex({
