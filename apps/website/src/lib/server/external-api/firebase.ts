7a3ca6c2d963b9481098918e1e292d6885d3fbf5
devunt <337728+devunt@users.noreply.github.com>
2024-05-19 08:49:29 +0000

    firebase 빌드 오류 수정 재시도 (#2165)
    흑흑흑흑흑

--- apps/website/src/lib/server/external-api/firebase.ts
+++ apps/website/src/lib/server/external-api/firebase.ts
@@ -1,6 +1,7 @@
 import { eq } from 'drizzle-orm';
 import { cert, deleteApp, initializeApp } from 'firebase-admin/app';
 import { getMessaging } from 'firebase-admin/messaging';
+import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import { database, UserPushNotificationTokens } from '../database';
 import { finalizeResource, setResourceFinalizer } from '../utils';
@@ -8,7 +9,7 @@ import { finalizeResource, setResourceFinalizer } from '../utils';
 await finalizeResource('firebase');
 
 const app = initializeApp({
-  credential: cert(JSON.parse(env.PRIVATE_FIREBASE_SERVICE_ACCOUNT ?? '{}')),
+  credential: building ? undefined : cert(JSON.parse(env.PRIVATE_FIREBASE_SERVICE_ACCOUNT)),
 });
 
 setResourceFinalizer('firebase', async () => await deleteApp(app));
