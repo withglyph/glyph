075ed850e01063cfe4c55c340ce3637c368a5307
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-05-21 06:17:49 +0000

    HTML 블럭 높이 0 문제 수정 (#2185)

--- apps/website/src/lib/tiptap/node-views/html/utils.ts
+++ apps/website/src/lib/tiptap/node-views/html/utils.ts
@@ -2,6 +2,15 @@ export const makeIFrameContent = (content: string) => {
   return `
     <style>html, body { margin: 0 }</style>
     ${content}
-    <script>parent.postMessage({ type: 'resize', height: document.body.scrollHeight }, '*')</script>
+    <script>
+      (() => {
+        const interval = setInterval(() => {
+          if(document.body.scrollHeight > 0) {
+            parent.postMessage({ type: 'resize', height: document.body.scrollHeight }, '*');
+            clearInterval(interval);
+          }
+        }, 100);
+      })();
+    </script>
   `;
 };
