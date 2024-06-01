54669ff669d06bbffca6f57968dbec98c7932ae1
devunt <337728+devunt@users.noreply.github.com>
2024-06-01 05:55:24 +0000

    플러터 master 버전 이용 (#2280)

--- apps/mobile/ios/Runner/AppDelegate.swift
+++ apps/mobile/ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import Flutter
 import NaverThirdPartyLogin
 import UIKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
