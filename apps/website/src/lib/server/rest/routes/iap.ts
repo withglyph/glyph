2172c404b34af152eca724ae8724146534c63408
devunt <337728+devunt@users.noreply.github.com>
2024-06-11 01:18:07 +0000

    iap endpoint의 http method를 post로 변경 (#2337)

--- apps/website/src/lib/server/rest/routes/iap.ts
+++ apps/website/src/lib/server/rest/routes/iap.ts
@@ -3,14 +3,14 @@ import { createRouter } from '../router';
 
 export const iap = createRouter();
 
-iap.get('/iap/google', async (_, context) => {
+iap.post('/iap/google', async (_, context) => {
   const payload = await context.event.request.json();
   console.log(payload);
 
   return status(200);
 });
 
-iap.get('/iap/apple', async (_, context) => {
+iap.post('/iap/apple', async (_, context) => {
   const payload = await context.event.request.json();
   console.log(payload);
 
