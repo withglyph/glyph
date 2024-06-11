9980cc7bce8e83d44ba469c556b644128b96b1cd
devunt <337728+devunt@users.noreply.github.com>
2024-06-11 02:26:28 +0000

    iap 정보 validation 코드 추가 (안드로이드) (#2338)

--- apps/website/src/lib/server/external-api/google.ts
+++ apps/website/src/lib/server/external-api/google.ts
@@ -1,7 +1,17 @@
-import { OAuth2Client } from 'google-auth-library';
+import { GoogleAuth, OAuth2Client } from 'google-auth-library';
+import { google } from 'googleapis';
+import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import type { RequestEvent } from '@sveltejs/kit';
 import type { ExternalUser } from './types';
+
+const androidPublisher = google.androidpublisher({
+  version: 'v3',
+  auth: new GoogleAuth({
+    credentials: building ? undefined : JSON.parse(env.PRIVATE_GOOGLE_SERVICE_ACCOUNT),
+    scopes: ['https://www.googleapis.com/auth/androidpublisher'],
+  }),
+});
 
 const createOAuthClient = (event: RequestEvent) => {
   return new OAuth2Client({
@@ -46,4 +56,15 @@ export const authorizeUser = async (event: RequestEvent, code: string): Promise<
     name: userinfo.data.name,
     avatarUrl: userinfo.data.picture,
   };
+};
+
+type GetInAppPurchaseParams = { productId: string; token: string };
+export const getInAppPurchase = async ({ productId, token }: GetInAppPurchaseParams) => {
+  const resp = await androidPublisher.purchases.products.get({
+    packageName: 'com.withglyph.app',
+    productId,
+    token,
+  });
+
+  return resp.data;
 };
--- apps/website/src/lib/server/rest/routes/iap.ts
+++ apps/website/src/lib/server/rest/routes/iap.ts
@@ -1,18 +1,29 @@
 import { status } from 'itty-router';
+import { fromBase64 } from 'js-base64';
+import { google } from '$lib/server/external-api';
 import { createRouter } from '../router';
 
 export const iap = createRouter();
 
 iap.post('/iap/google', async (_, context) => {
   const payload = await context.event.request.json();
-  console.log(payload);
+  const data = JSON.parse(fromBase64(payload.message.data));
+
+  const resp = await google.getInAppPurchase({
+    productId: data.oneTimeProductNotification.sku,
+    token: data.oneTimeProductNotification.purchaseToken,
+  });
+
+  console.log(resp);
 
   return status(200);
 });
 
 iap.post('/iap/apple', async (_, context) => {
   const payload = await context.event.request.json();
-  console.log(payload);
+  const jwt = fromBase64(payload.signedPayload);
+
+  console.log(jwt);
 
   return status(200);
 });
