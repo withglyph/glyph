9a2cfc7037e1f79298a8ffcf428178aa9df0cb33
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-06-21 08:11:11 +0000

    주간 창작 2주차 보상 스크립트 (#2421)

--- apps/website/scripts/202406-event-reward.ts
+++ apps/website/scripts/202406-event-reward.ts
@@ -1,57 +1,74 @@
+/* eslint-disable unicorn/no-process-exit */
 import dayjs from 'dayjs';
-import { and, eq, isNull } from 'drizzle-orm';
-import { database, inArray, PointBalances, PointTransactions, UserEventEnrollments } from '$lib/server/database';
+import { and, eq, isNotNull } from 'drizzle-orm';
+import {
+  database,
+  inArray,
+  PointBalances,
+  PointTransactions,
+  UserEventEnrollments,
+  UserPersonalIdentities,
+} from '$lib/server/database';
 
-let eventEnrollmentCount = 0;
-do {
+for (let i = 0; ; i++) {
   await database.transaction(async (tx) => {
-    const eventEnrollments = await tx
-      .select({ id: UserEventEnrollments.id, userId: UserEventEnrollments.userId })
+    const eventEnrollment = await tx
+      .select({ UserEventEnrollments })
       .from(UserEventEnrollments)
-      .where(
-        and(
-          eq(UserEventEnrollments.eventCode, 'weekly_challenge_24061'),
-          eq(UserEventEnrollments.eligible, true),
-          isNull(UserEventEnrollments.rewardedAt),
-        ),
-      )
-      .limit(100)
-      .for('update');
+      .innerJoin(UserPersonalIdentities, eq(UserEventEnrollments.userId, UserPersonalIdentities.userId))
+      .where(and(eq(UserEventEnrollments.eventCode, 'weekly_challenge_24062'), eq(UserEventEnrollments.eligible, true)))
+      .orderBy(UserEventEnrollments.id)
+      .limit(1)
+      .offset(i)
+      .for('update')
+      .then((rows) => rows[0]?.UserEventEnrollments);
 
-    eventEnrollmentCount = eventEnrollments.length;
-    if (eventEnrollmentCount === 0) {
+    if (!eventEnrollment) {
+      console.log('끗!!');
+      process.exit(0);
+    }
+
+    if (eventEnrollment.rewardedAt) {
       return;
     }
 
-    // 2주차부터는 전주차에서 리워드 받았는지 체크할것
+    const beforeRewardedEnrollments = await tx
+      .select({ id: UserEventEnrollments.id })
+      .from(UserEventEnrollments)
+      .where(
+        and(
+          inArray(UserEventEnrollments.eventCode, ['weekly_challenge_24061']),
+          eq(UserEventEnrollments.userId, eventEnrollment.userId),
+          isNotNull(UserEventEnrollments.rewardedAt),
+        ),
+      )
+      .for('update');
+
+    if (beforeRewardedEnrollments.length > 0) {
+      console.log(`${eventEnrollment.userId} 이미 전주차에 보상 받음`);
+      return;
+    }
 
     await tx
       .update(UserEventEnrollments)
       .set({ rewardedAt: dayjs() })
-      .where(
-        inArray(
-          UserEventEnrollments.id,
-          eventEnrollments.map((enrollment) => enrollment.id),
-        ),
-      );
+      .where(eq(UserEventEnrollments.id, eventEnrollment.id));
 
-    await tx.insert(PointBalances).values(
-      eventEnrollments.map((enrollment) => ({
-        userId: enrollment.userId,
-        kind: 'FREE' as const,
-        initial: 2000,
-        leftover: 2000,
-        expiresAt: dayjs().add(1, 'years'),
-      })),
-    );
+    await tx.insert(PointBalances).values({
+      userId: eventEnrollment.userId,
+      kind: 'FREE' as const,
+      initial: 2000,
+      leftover: 2000,
+      expiresAt: dayjs().add(1, 'years'),
+    });
 
-    await tx.insert(PointTransactions).values(
-      eventEnrollments.map((enrollment) => ({
-        userId: enrollment.userId,
-        amount: 2000,
-        cause: 'EVENT_REWARD' as const,
-        targetId: enrollment.id,
-      })),
-    );
+    await tx.insert(PointTransactions).values({
+      userId: eventEnrollment.userId,
+      amount: 2000,
+      cause: 'EVENT_REWARD' as const,
+      targetId: eventEnrollment.id,
+    });
+
+    console.log(`${eventEnrollment.userId} 2000포인트 지급`);
   });
-} while (eventEnrollmentCount > 0);
+}
