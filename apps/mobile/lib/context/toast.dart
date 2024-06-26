d0fa41581a27137b139d08663d47220aa6dc2ba7
devunt <337728+devunt@users.noreply.github.com>
2024-06-26 12:39:06 +0000

    토스트 코드 리팩토링 (#2454)

--- apps/mobile/lib/context/toast.dart
+++ apps/mobile/lib/context/toast.dart
@@ -2,60 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:gap/gap.dart';
 import 'package:glyph/icons/tabler.dart';
 import 'package:glyph/themes/colors.dart';
 
 enum ToastType {
-  Success,
-  Error,
+  success,
+  error,
 }
 
-const ToastIcon = {
-  ToastType.Success: Icon(
-    Tabler.circle_check_filled,
-    color: Color(0xFF2DB362),
-    size: 16,
-  ),
-  ToastType.Error: Icon(
-    Tabler.alert_circle_filled,
-    color: Color(0xFFFFC750),
-    size: 16,
-  ),
-};
+class _TextToastWidget extends ConsumerWidget {
+  const _TextToastWidget({
+    required this.type,
+    required this.message,
+  });
 
-class TextToastWidget extends ConsumerWidget {
-  const TextToastWidget({required this.message, required this.type, super.key});
-
-  final String message;
   final ToastType type;
+  final String message;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      padding: const EdgeInsets.all(14),
       decoration: BoxDecoration(
         color: BrandColors.gray_600,
         borderRadius: BorderRadius.circular(4),
       ),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 2),
-            child: ToastIcon[type]!,
+            padding: const EdgeInsets.only(top: 2),
+            child: switch (type) {
+              ToastType.success => const Icon(
+                  Tabler.circle_check_filled,
+                  color: Color(0xFF2DB362),
+                  size: 16,
+                ),
+              ToastType.error => const Icon(
+                  Tabler.alert_circle_filled,
+                  color: Color(0xFFFFC750),
+                  size: 16,
+                ),
+            },
           ),
-          const SizedBox(width: 6),
+          const Gap(6),
           Expanded(
             child: Text(
               message,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
               style: const TextStyle(
-                color: BrandColors.gray_0,
                 fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: BrandColors.gray_0,
               ),
-              softWrap: true,
             ),
           ),
         ],
-        crossAxisAlignment: CrossAxisAlignment.start,
       ),
     );
   }
@@ -96,15 +100,15 @@ class ToastController {
   final BuildContext context;
   FToast get _ftoast => ToastScope.of(context)!.ftoast;
 
-  void show(String message, {ToastType type = ToastType.Success}) {
+  void show(String message, {ToastType type = ToastType.success}) {
     _ftoast.showToast(
-      child: TextToastWidget(message: message, type: type),
+      child: _TextToastWidget(type: type, message: message),
       positionedToastBuilder: (context, child) {
         final height = MediaQuery.of(context).padding.bottom;
         return Positioned(
-          bottom: height + 28,
-          left: 16,
-          right: 16,
+          bottom: height + 12,
+          left: 20,
+          right: 20,
           child: child,
         );
       },
