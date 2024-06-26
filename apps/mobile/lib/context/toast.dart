eb75dd075606d358deb6714ba892b184210a7257
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-06-26 10:46:20 +0000

    토스트 디자인 적용 (#2451)
    - 타입 추가(긍정피드백/에러)
    - 길어졌을 때 여러줄 대응

--- apps/mobile/lib/context/toast.dart
+++ apps/mobile/lib/context/toast.dart
@@ -2,27 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:glyph/icons/tabler.dart';
 import 'package:glyph/themes/colors.dart';
 
+enum ToastType {
+  Success,
+  Error,
+}
+
+const ToastIcon = {
+  ToastType.Success: Icon(
+    Tabler.circle_check_filled,
+    color: Color(0xFF2DB362),
+    size: 16,
+  ),
+  ToastType.Error: Icon(
+    Tabler.alert_circle_filled,
+    color: Color(0xFFFFC750),
+    size: 16,
+  ),
+};
+
 class TextToastWidget extends ConsumerWidget {
-  const TextToastWidget({required this.message, super.key});
+  const TextToastWidget({required this.message, required this.type, super.key});
 
   final String message;
+  final ToastType type;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
       decoration: BoxDecoration(
-        color: BrandColors.gray_900,
+        color: BrandColors.gray_600,
         borderRadius: BorderRadius.circular(4),
       ),
-      child: Text(
-        message,
-        style: const TextStyle(
-          color: BrandColors.gray_0,
-          fontSize: 15,
-        ),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2),
+            child: ToastIcon[type]!,
+          ),
+          const SizedBox(width: 6),
+          Expanded(
+            child: Text(
+              message,
+              style: const TextStyle(
+                color: BrandColors.gray_0,
+                fontSize: 14,
+              ),
+              softWrap: true,
+            ),
+          ),
+        ],
+        crossAxisAlignment: CrossAxisAlignment.start,
       ),
     );
   }
@@ -63,9 +96,9 @@ class ToastController {
   final BuildContext context;
   FToast get _ftoast => ToastScope.of(context)!.ftoast;
 
-  void show(String message) {
+  void show(String message, {ToastType type = ToastType.Success}) {
     _ftoast.showToast(
-      child: TextToastWidget(message: message),
+      child: TextToastWidget(message: message, type: type),
       positionedToastBuilder: (context, child) {
         final height = MediaQuery.of(context).padding.bottom;
         return Positioned(
