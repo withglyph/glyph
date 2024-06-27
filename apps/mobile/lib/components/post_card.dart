5a7d0dea5c5abde1d6791908cf172d04a176b2c0
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-06-27 09:05:46 +0000

    PostCard 디자인 변경사항 반영 (#2467)

--- apps/mobile/lib/components/post_card.dart
+++ apps/mobile/lib/components/post_card.dart
@@ -8,7 +8,7 @@ import 'package:glyph/components/pressable.dart';
 import 'package:glyph/components/tag.dart';
 import 'package:glyph/extensions/iterable.dart';
 import 'package:glyph/graphql/fragments/__generated__/post_card_post.data.gql.dart';
-import 'package:glyph/icons/tabler.dart';
+// import 'package:glyph/icons/tabler.dart';
 import 'package:glyph/routers/app.gr.dart';
 import 'package:glyph/themes/colors.dart';
 import 'package:jiffy/jiffy.dart';
@@ -40,74 +40,62 @@ class PostCard extends ConsumerWidget {
                   children: [
                     Text(
                       post.publishedRevision!.title ?? '(제목 없음)',
-                      maxLines: 2,
                       overflow: TextOverflow.ellipsis,
                       style: const TextStyle(
                         fontSize: 15,
                         fontWeight: FontWeight.w700,
                       ),
                     ),
-                    const Gap(2),
-                    Row(
-                      children: [
-                        if (post.publishedRevision!.subtitle != null)
-                          Flexible(
-                            child: Text(
-                              post.publishedRevision!.subtitle!,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                fontSize: 13,
-                                fontWeight: FontWeight.w500,
-                                color: BrandColors.gray_800,
+                    SizedBox(
+                      height: 17,
+                      child: post.publishedRevision!.subtitle == null
+                          ? Expanded(
+                              child: Text(
+                                post.publishedRevision!.previewText,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 13,
+                                  color: BrandColors.gray_500,
+                                ),
+                              ),
+                            )
+                          : Flexible(
+                              child: Text(
+                                post.publishedRevision!.subtitle!,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 13,
+                                  color: BrandColors.gray_800,
+                                ),
                               ),
                             ),
-                          ),
-                        if (post.publishedRevision!.subtitle != null &&
-                            post.publishedRevision!.previewText != '') ...[
-                          const Gap(4),
-                          Container(
-                            width: 1,
-                            height: 12,
-                            color: BrandColors.gray_100,
-                          ),
-                          const Gap(4),
-                        ],
-                        if (post.publishedRevision!.previewText != '')
-                          Expanded(
-                            child: Text(
-                              post.publishedRevision!.previewText,
-                              overflow: TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                fontSize: 13,
-                                color: BrandColors.gray_600,
-                              ),
-                            ),
-                          ),
-                      ],
                     ),
-                    const Gap(10),
-                    UnconstrainedBox(
-                      clipBehavior: Clip.hardEdge,
-                      alignment: Alignment.centerLeft,
-                      child: Row(
-                        children: post.tags
-                            .map((tag) => Tag(tag.tag))
-                            .intersperse(const Gap(4))
-                            .toList(),
+                    const Gap(8),
+                    SizedBox(
+                      height: 20,
+                      child: UnconstrainedBox(
+                        clipBehavior: Clip.hardEdge,
+                        alignment: Alignment.centerLeft,
+                        child: Row(
+                          children: post.tags
+                              .map((tag) => Tag(tag.tag))
+                              .intersperse(const Gap(4))
+                              .toList(),
+                        ),
                       ),
                     ),
-                    const Gap(16),
+                    const Gap(10),
                     Row(
                       children: [
                         Img(post.space!.icon, width: 18, height: 18),
-                        const Gap(6),
+                        const Gap(4),
                         Flexible(
                           child: Text(
                             post.space!.name,
                             overflow: TextOverflow.ellipsis,
                             style: const TextStyle(
                               fontSize: 12,
-                              color: BrandColors.gray_500,
+                              color: BrandColors.gray_600,
                             ),
                           ),
                         ),
@@ -119,7 +107,8 @@ class PostCard extends ConsumerWidget {
                         ),
                         const Gap(6),
                         Text(
-                          Jiffy.parse(post.publishedAt!.value).fromNow(),
+                          Jiffy.parse(post.publishedAt!.value)
+                              .format(pattern: 'yyyy.MM.dd'),
                           style: const TextStyle(
                             fontSize: 12,
                             color: BrandColors.gray_500,
@@ -130,22 +119,34 @@ class PostCard extends ConsumerWidget {
                   ],
                 ),
               ),
-              const Gap(24),
+              const Gap(14),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Img(
-                    post.thumbnail,
-                    width: 104,
-                    aspectRatio: 16 / 10,
-                    borderRadius: 0,
+                  // ignore: use_decorated_box
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 0.8,
+                        color: BrandColors.gray_50,
+                      ),
+                      borderRadius: BorderRadius.circular(2),
+                    ),
+                    child: Img(
+                      post.thumbnail,
+                      width: 78,
+                      aspectRatio: 16 / 10,
+                    ),
                   ),
-                  const Spacer(),
-                  const Icon(
-                    Tabler.dots_vertical,
-                    size: 20,
-                    color: BrandColors.gray_400,
-                  ),
+                  // const Spacer(),
+                  // Pressable(
+                  //   child: const Icon(
+                  //     Tabler.dots_vertical,
+                  //     size: 20,
+                  //     color: BrandColors.gray_400,
+                  //   ),
+                  //   onPressed: () {},
+                  // ),
                 ],
               ),
             ],
