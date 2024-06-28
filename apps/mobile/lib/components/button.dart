8c0acb1b5f3ff09c46857487a814bd4a75b29143
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-06-28 07:38:13 +0000

    버튼 컴포넌트 테마 추가 (#2473)

--- apps/mobile/lib/components/button.dart
+++ apps/mobile/lib/components/button.dart
@@ -2,16 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:glyph/components/pressable.dart';
 import 'package:glyph/themes/colors.dart';
 
+enum ButtonTheme {
+  Primary,
+  Secondary,
+  Accent,
+}
+
+Color _getButtonColor(ButtonTheme theme, bool enabled) {
+  if (!enabled) {
+    return BrandColors.gray_150;
+  }
+
+  switch (theme) {
+    case ButtonTheme.Primary:
+      return BrandColors.gray_900;
+    case ButtonTheme.Secondary:
+      return BrandColors.gray_50;
+    case ButtonTheme.Accent:
+      return BrandColors.brand_400;
+  }
+}
+
+Color _getButtonTextColor(ButtonTheme theme, bool enabled) {
+  if (!enabled) {
+    return BrandColors.gray_400;
+  }
+
+  switch (theme) {
+    case ButtonTheme.Primary:
+      return BrandColors.gray_0;
+    case ButtonTheme.Secondary:
+      return BrandColors.gray_900;
+    case ButtonTheme.Accent:
+      return BrandColors.gray_0;
+  }
+}
+
 class Button extends StatelessWidget {
   const Button(
     this.title, {
     super.key,
     this.enabled = true,
+    this.theme = ButtonTheme.Primary,
     this.onPressed,
   });
 
   final String title;
   final bool enabled;
+  final ButtonTheme theme;
   final Function()? onPressed;
 
   @override
@@ -21,7 +59,7 @@ class Button extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
         decoration: BoxDecoration(
-          color: enabled ? BrandColors.brand_400 : BrandColors.gray_150,
+          color: _getButtonColor(theme, enabled),
           borderRadius: BorderRadius.circular(4),
         ),
         child: Center(
@@ -30,7 +68,7 @@ class Button extends StatelessWidget {
             style: TextStyle(
               fontSize: 15,
               fontWeight: FontWeight.w700,
-              color: enabled ? BrandColors.gray_0 : BrandColors.gray_400,
+              color: _getButtonTextColor(theme, enabled),
             ),
           ),
         ),
