b2a85c85928cb305c141174fca844a7090dc3836
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-07-01 06:39:09 +0000

    버튼 컴포넌트 업그레이드 (#2497)
    - 디자인 수정사항 반영
    - secondary-outline, danger kind 추가
    - 아이콘 기능 추가

--- apps/mobile/lib/components/button.dart
+++ apps/mobile/lib/components/button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:glyph/components/pressable.dart';
 import 'package:glyph/themes/colors.dart';
 
@@ -6,12 +7,26 @@ enum ButtonKind {
   primary,
   secondary,
   accent,
+  secondaryOutline,
+  danger,
 }
 
 enum ButtonSize {
   medium,
   large,
   small,
+}
+
+Color _textColor(bool enabled, ButtonKind kind) {
+  return enabled
+      ? switch (kind) {
+          ButtonKind.primary => BrandColors.gray_0,
+          ButtonKind.secondary => BrandColors.gray_600,
+          ButtonKind.accent => BrandColors.gray_0,
+          ButtonKind.secondaryOutline => BrandColors.gray_600,
+          ButtonKind.danger => BrandColors.red_600,
+        }
+      : BrandColors.gray_400;
 }
 
 class Button extends StatelessWidget {
@@ -21,6 +36,8 @@ class Button extends StatelessWidget {
     this.enabled = true,
     this.kind = ButtonKind.primary,
     this.size = ButtonSize.medium,
+    this.iconLeft,
+    this.iconRight,
     this.onPressed,
   });
 
@@ -28,6 +45,8 @@ class Button extends StatelessWidget {
   final bool enabled;
   final ButtonKind kind;
   final ButtonSize size;
+  final IconData? iconLeft;
+  final IconData? iconRight;
   final Function()? onPressed;
 
   @override
@@ -37,7 +56,7 @@ class Button extends StatelessWidget {
       child: Container(
         padding: switch (size) {
           ButtonSize.large => const EdgeInsets.symmetric(vertical: 15),
-          ButtonSize.medium => const EdgeInsets.symmetric(vertical: 10),
+          ButtonSize.medium => const EdgeInsets.symmetric(vertical: 11),
           ButtonSize.small => const EdgeInsets.symmetric(vertical: 7),
         },
         decoration: BoxDecoration(
@@ -46,6 +65,8 @@ class Button extends StatelessWidget {
                   ButtonKind.primary => BrandColors.gray_900,
                   ButtonKind.secondary => BrandColors.gray_50,
                   ButtonKind.accent => BrandColors.brand_400,
+                  ButtonKind.secondaryOutline => BrandColors.gray_0,
+                  ButtonKind.danger => BrandColors.gray_0,
                 }
               : BrandColors.gray_150,
           borderRadius: switch (size) {
@@ -53,25 +74,54 @@ class Button extends StatelessWidget {
             ButtonSize.medium => BorderRadius.circular(4),
             ButtonSize.small => BorderRadius.circular(2),
           },
+          border: enabled
+              ? switch (kind) {
+                  ButtonKind.secondaryOutline || ButtonKind.danger => Border.all(color: BrandColors.gray_150),
+                  _ => null,
+                }
+              : null,
         ),
         child: Center(
-          child: Text(
-            title,
-            style: TextStyle(
-              fontSize: switch (size) {
-                ButtonSize.large => 16,
-                ButtonSize.medium => 14,
-                ButtonSize.small => 12,
-              },
-              fontWeight: FontWeight.w700,
-              color: enabled
-                  ? switch (kind) {
-                      ButtonKind.primary => BrandColors.gray_0,
-                      ButtonKind.secondary => BrandColors.gray_900,
-                      ButtonKind.accent => BrandColors.gray_0,
-                    }
-                  : BrandColors.gray_400,
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (iconLeft != null) ...[
+                Icon(
+                  iconLeft,
+                  size: switch (size) {
+                    ButtonSize.large => 20,
+                    ButtonSize.medium => 18,
+                    ButtonSize.small => 14,
+                  },
+                  color: _textColor(enabled, kind),
+                ),
+                const Gap(3),
+              ],
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: switch (size) {
+                    ButtonSize.large => 16,
+                    ButtonSize.medium => 14,
+                    ButtonSize.small => 12,
+                  },
+                  fontWeight: FontWeight.w700,
+                  color: _textColor(enabled, kind),
+                ),
+              ),
+              if (iconRight != null) ...[
+                const Gap(3),
+                Icon(
+                  iconRight,
+                  size: switch (size) {
+                    ButtonSize.large => 20,
+                    ButtonSize.medium => 18,
+                    ButtonSize.small => 14,
+                  },
+                  color: _textColor(enabled, kind),
+                ),
+              ],
+            ],
           ),
         ),
       ),
