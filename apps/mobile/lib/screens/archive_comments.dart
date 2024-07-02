863b20f306adbcd8bb1f2c43e9bb7edcff1b2316
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-07-02 08:54:26 +0000

    댓글단 포스트 목록 표시 (#2528)

--- apps/mobile/lib/screens/archive_comments.dart
+++ apps/mobile/lib/screens/archive_comments.dart
@@ -1,13 +1,17 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
 import 'package:glyph/components/empty_state.dart';
 import 'package:glyph/components/horizontal_divider.dart';
-import 'package:glyph/components/post_card.dart';
+import 'package:glyph/components/img.dart';
+import 'package:glyph/components/pressable.dart';
 import 'package:glyph/ferry/widget.dart';
 import 'package:glyph/graphql/__generated__/archive_comments_screen_query.req.gql.dart';
 import 'package:glyph/icons/tabler_bold.dart';
+import 'package:glyph/routers/app.gr.dart';
 import 'package:glyph/themes/colors.dart';
+import 'package:jiffy/jiffy.dart';
 
 @RoutePage()
 class ArchiveCommentsScreen extends ConsumerWidget {
@@ -18,9 +22,9 @@ class ArchiveCommentsScreen extends ConsumerWidget {
     return GraphQLOperation(
       operation: GArchiveCommentsScreen_QueryReq(),
       builder: (context, client, data) {
-        final posts = data.me!.comments.map((e) => e.post).nonNulls.toList();
+        final comments = data.me!.comments;
 
-        return posts.isEmpty
+        return comments.isEmpty
             ? const EmptyState(
                 icon: TablerBold.message_circle_off,
                 title: '아직 댓글을 남긴 포스트가 없어요',
@@ -30,12 +34,89 @@ class ArchiveCommentsScreen extends ConsumerWidget {
                 physics: const AlwaysScrollableScrollPhysics(
                   parent: BouncingScrollPhysics(),
                 ),
-                itemCount: posts.length,
+                itemCount: comments.length,
                 itemBuilder: (context, index) {
-                  return PostCard(
-                    posts[index],
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
-                    dots: false,
+                  final comment = comments[index];
+
+                  return Pressable(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 16, 20, 18),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${Jiffy.parse(comment.createdAt.value).format(pattern: 'yyyy.MM.dd hh:mm')}에 작성',
+                            style: const TextStyle(
+                              fontSize: 11,
+                              fontWeight: FontWeight.w500,
+                              color: BrandColors.gray_500,
+                            ),
+                          ),
+                          const Gap(4),
+                          Text(
+                            comment.content,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          const Gap(10),
+                          Row(
+                            children: [
+                              Img(
+                                comment.post?.thumbnail,
+                                width: 50,
+                                aspectRatio: 16 / 10,
+                              ),
+                              const Gap(8),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    comment.post != null
+                                        ? comment.post?.publishedRevision?.title ?? '(제목 없음)'
+                                        : '삭제된 포스트',
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500,
+                                      color: comment.post != null ? BrandColors.gray_500 : BrandColors.gray_400,
+                                    ),
+                                  ),
+                                  const Gap(3),
+                                  Row(
+                                    children: [
+                                      Img(
+                                        comment.post?.space?.icon,
+                                        width: 18,
+                                        aspectRatio: 1 / 1,
+                                      ),
+                                      const Gap(4),
+                                      Text(
+                                        comment.post?.space?.name ?? '스페이스',
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: comment.post != null ? BrandColors.gray_500 : BrandColors.gray_400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const Gap(3),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    onPressed: () async {
+                      if (comment.post != null) {
+                        await context.pushRoute(PostRoute(permalink: comment.post!.permalink));
+                      }
+                    },
                   );
                 },
                 separatorBuilder: (context, index) {
