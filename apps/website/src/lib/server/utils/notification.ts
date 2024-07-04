1a0476be8959664e70b789d2f15299306f626f72
robin-maki <8201350+robin-maki@users.noreply.github.com>
2024-07-04 07:18:16 +0000

    푸시 알림 서버측 구현 (#2594)

--- apps/website/src/lib/server/utils/notification.ts
+++ apps/website/src/lib/server/utils/notification.ts
@@ -1,7 +1,20 @@
 import { and, eq } from 'drizzle-orm';
 import * as R from 'radash';
+import { match } from 'ts-pattern';
 import { UserNotificationCategory, UserNotificationMethod } from '$lib/enums';
-import { database, inArray, UserNotificationPreferences, UserNotifications } from '../database';
+import {
+  database,
+  inArray,
+  PostComments,
+  PostRevisions,
+  Posts,
+  Profiles,
+  Spaces,
+  UserNotificationPreferences,
+  UserNotifications,
+} from '../database';
+import { firebase } from '../external-api';
+import { useFirstRowOrThrow } from './database';
 import type { Notification } from '$lib/utils';
 
 type CheckNotificationPreferencesParams = {
@@ -42,7 +55,8 @@ type CreateNotificationParams = Notification & {
   origin: string;
 };
 
-export const createNotification = async ({ userId, category, actorId, data }: CreateNotificationParams) => {
+export const createNotification = async (params: CreateNotificationParams) => {
+  const { userId, category, actorId, data } = params;
   const preferences = await checkNotificationPreferences({ userId, category });
 
   if (preferences.WEBSITE) {
@@ -54,6 +68,75 @@ export const createNotification = async ({ userId, category, actorId, data }: Cr
       state: 'UNREAD',
     });
   }
+
+  const actorProfile = await database
+    .select()
+    .from(Profiles)
+    .where(eq(Profiles.id, actorId))
+    .then(useFirstRowOrThrow());
+
+  await firebase.sendPushNotification({
+    userId,
+    ...(await match(params)
+      .with({ category: 'COMMENT' }, async ({ data }) => {
+        const notificationData = await database
+          .select({ postTitle: PostRevisions.title, commentContent: PostComments.content })
+          .from(PostComments)
+          .innerJoin(Posts, eq(PostComments.postId, Posts.id))
+          .innerJoin(PostRevisions, eq(Posts.publishedRevisionId, PostRevisions.id))
+          .where(eq(PostComments.id, data.commentId))
+          .then(useFirstRowOrThrow());
+
+        return {
+          title: notificationData.postTitle ?? '(제목 없음)',
+          body: `${actorProfile.name}님이 "${notificationData.commentContent}" 댓글을 남겼어요.`,
+        };
+      })
+      .with({ category: 'EMOJI_REACTION' }, async ({ data }) => {
+        const notificationData = await database
+          .select({ postTitle: PostRevisions.title })
+          .from(Posts)
+          .innerJoin(PostRevisions, eq(Posts.publishedRevisionId, PostRevisions.id))
+          .where(eq(Posts.id, data.postId))
+          .then(useFirstRowOrThrow());
+
+        return {
+          title: notificationData.postTitle ?? '(제목 없음)',
+          body: `${actorProfile.name}님이 이모지를 남겼어요.`,
+        };
+      })
+      .with({ category: 'PURCHASE' }, async ({ data }) => {
+        const notificationData = await database
+          .select({ postTitle: PostRevisions.title })
+          .from(Posts)
+          .innerJoin(PostRevisions, eq(Posts.publishedRevisionId, PostRevisions.id))
+          .where(eq(Posts.id, data.postId))
+          .then(useFirstRowOrThrow());
+
+        return {
+          title: notificationData.postTitle ?? '(제목 없음)',
+          body: `${actorProfile.name}님이 포스트를 구매했어요.`,
+        };
+      })
+      .with({ category: 'SUBSCRIBE' }, async ({ data }) => {
+        const notificationData = await database
+          .select({ spaceName: Spaces.name })
+          .from(Spaces)
+          .where(eq(Spaces.id, data.spaceId))
+          .then(useFirstRowOrThrow());
+
+        return {
+          title: notificationData.spaceName,
+          body: `${actorProfile.name}님이 스페이스를 구독했어요.`,
+        };
+      })
+      .otherwise(() => {
+        return {
+          title: '알림이 도착했어요.',
+          body: '앱에서 자세한 내용을 확인해보세요.',
+        };
+      })),
+  });
 
   // 이메일 알림 일단 비활성화
   // if (preferences.EMAIL) {
