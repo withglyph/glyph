dd89a18a64fe536a8fd3639c561811bae85673b9
devunt <337728+devunt@users.noreply.github.com>
2024-07-05 10:56:17 +0000

    본인인증 스크린 구현 (#2650)

--- apps/mobile/lib/screens/identification.dart
+++ apps/mobile/lib/screens/identification.dart
@@ -1,13 +1,19 @@
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:glyph/components/btn.dart';
-import 'package:glyph/context/dialog.dart';
+import 'package:glyph/context/alert.dart';
+import 'package:glyph/ferry/error.dart';
+import 'package:glyph/ferry/extension.dart';
 import 'package:glyph/ferry/widget.dart';
 import 'package:glyph/graphql/__generated__/identification_screen_query.req.gql.dart';
+import 'package:glyph/graphql/__generated__/identification_screen_verify_personal_identity_mutation.req.gql.dart';
 import 'package:glyph/routers/app.gr.dart';
 import 'package:glyph/shells/default.dart';
+import 'package:glyph/themes/colors.dart';
+import 'package:jiffy/jiffy.dart';
 
 @RoutePage()
 class IdentificationScreen extends StatelessWidget {
@@ -22,23 +28,95 @@ class IdentificationScreen extends StatelessWidget {
       child: GraphQLOperation(
         operation: GIdentificationScreen_QueryReq(),
         builder: (context, client, data) {
+          final identified = data.me?.personalIdentity != null;
+
           return Padding(
             padding: const Pad(horizontal: 20, vertical: 16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('본인인증 여부: ${data.me!.personalIdentity != null}'),
-                if (data.me!.personalIdentity != null) Text('본인인증 이름: ${data.me!.personalIdentity!.name}'),
-                const Gap(32),
-                Btn(
-                  '본인인증',
-                  onPressed: () async {
-                    final result = await context.router.push(const IdentificationIamportRoute());
-                    if (context.mounted) {
-                      await context.showDialog(title: '결과', content: result.toString());
-                    }
-                  },
+                Text(
+                  identified ? '본인인증이\n완료되었어요' : '본인인증을\n진행해주세요',
+                  style: const TextStyle(fontSize: 22, fontWeight: FontWeight.w700),
                 ),
+                const Gap(8),
+                const Text(
+                  '- 본인인증은 개인당 1개의 계정에만 가능해요.\n'
+                  '- 계정을 분실한 경우, 본인인증을 통해 계정을 찾을 수 있어요.\n'
+                  '- 본인인증을 통해 연령제한 콘텐츠를 이용할 수 있어요.\n',
+                  style: TextStyle(fontSize: 12, color: BrandColors.gray_500),
+                ),
+                if (identified) ...[
+                  const Gap(24),
+                  Container(
+                    width: double.infinity,
+                    padding: const Pad(horizontal: 16, top: 32, bottom: 28),
+                    decoration: BoxDecoration(
+                      color: BrandColors.gray_50,
+                      borderRadius: BorderRadius.circular(2),
+                    ),
+                    child: Column(
+                      children: [
+                        const Text(
+                          '이용가능 콘텐츠',
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                            color: BrandColors.gray_500,
+                          ),
+                        ),
+                        const Gap(2),
+                        Text(
+                          data.me!.isAdulthood ? '15세 / 성인' : '15세',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        const Gap(20),
+                        Text(
+                          '유효기간: ${Jiffy.parse(data.me!.personalIdentity!.expiresAt!.value, isUtc: true).format(pattern: 'yyyy.MM.dd')}까지',
+                          style: const TextStyle(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w500,
+                            color: BrandColors.gray_400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ] else ...[
+                  const Spacer(),
+                  Btn(
+                    '본인인증하기',
+                    size: BtnSize.large,
+                    onPressed: () async {
+                      final dynamic result = await context.router.push(const IdentificationIamportRoute());
+                      if (result == null || result['imp_uid'] == null) {
+                        if (context.mounted) {
+                          await context.showAlert(title: '본인인증 오류', content: '본인인증에 실패했어요.');
+                        }
+
+                        return;
+                      }
+
+                      try {
+                        final req = GIdentificationScreen_VerifyPersonalIdentity_MutationReq(
+                          (b) => b..vars.input.uid = result['imp_uid'],
+                        );
+                        await client.req(req);
+                      } on IntentionalError catch (e) {
+                        if (context.mounted) {
+                          await context.showAlert(title: '본인인증 오류', content: e.message);
+                        }
+
+                        return;
+                      }
+
+                      client.requestController.add(GIdentificationScreen_QueryReq());
+                    },
+                  ),
+                ],
               ],
             ),
           );
