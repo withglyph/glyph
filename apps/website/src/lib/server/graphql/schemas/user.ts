2f5b79e16e4d54e2affb39d5c06800df892c0c6e
devunt <337728+devunt@users.noreply.github.com>
2024-07-05 10:26:51 +0000

    본인인증 graphql mutation 생성 (#2648)

--- apps/website/src/lib/server/graphql/schemas/user.ts
+++ apps/website/src/lib/server/graphql/schemas/user.ts
@@ -60,7 +60,7 @@ import {
 } from '$lib/server/database';
 import { sendEmail } from '$lib/server/email';
 import { LoginUser, UpdateUserEmail } from '$lib/server/email/templates';
-import { apple, coocon, google, naver, twitter } from '$lib/server/external-api';
+import { apple, coocon, google, naver, portone, twitter } from '$lib/server/external-api';
 import {
   createAccessToken,
   createRandomAvatar,
@@ -838,6 +838,12 @@ const DeleteUserInput = builder.inputType('DeleteUserInput', {
   validate: { schema: DeleteUserSchema },
 });
 
+const VerifyPersonalIdentityInput = builder.inputType('VerifyPersonalIdentityInput', {
+  fields: (t) => ({
+    uid: t.string(),
+  }),
+});
+
 const VerifySettlementIdentityInput = builder.inputType('VerifySettlementIdentityInput', {
   fields: (t) => ({
     residentRegistrationNumberBack: t.string(),
@@ -1446,6 +1452,50 @@ builder.mutationFields((t) => ({
       });
 
       context.event.cookies.delete('glyph-at', { path: '/' });
+    },
+  }),
+
+  verifyPersonalIdentity: t.withAuth({ user: true }).field({
+    type: UserPersonalIdentity,
+    args: { input: t.arg({ type: VerifyPersonalIdentityInput }) },
+    resolve: async (_, { input }, context) => {
+      const resp = await portone.getCertification(input.uid);
+      if (resp.code !== 0) {
+        throw new Error(resp.message);
+      }
+
+      if (!resp.response.certified) {
+        throw new IntentionalError('본인인증에 실패했어요');
+      }
+
+      const identities = await database
+        .select({ id: UserPersonalIdentities.id, userId: UserPersonalIdentities.userId })
+        .from(UserPersonalIdentities)
+        .innerJoin(Users, eq(Users.id, UserPersonalIdentities.userId))
+        .where(and(eq(UserPersonalIdentities.ci, resp.response.unique_key), eq(Users.state, 'ACTIVE')));
+
+      if (identities.length > 0) {
+        if (identities[0].userId === context.session.userId) {
+          return identities[0].id;
+        } else {
+          throw new IntentionalError('이미 인증된 다른 계정이 있어요');
+        }
+      }
+
+      const [identity] = await database
+        .insert(UserPersonalIdentities)
+        .values({
+          userId: context.session.userId,
+          kind: 'PHONE',
+          name: resp.response.name,
+          birthday: dayjs.kst(resp.response.birthday, 'YYYY-MM-DD'),
+          phoneNumber: resp.response.phone,
+          ci: resp.response.unique_key,
+          expiresAt: dayjs().add(1, 'year'),
+        })
+        .returning({ id: UserPersonalIdentities.id });
+
+      return identity.id;
     },
   }),
 
