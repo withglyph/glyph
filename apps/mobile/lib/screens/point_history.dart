16329d8a929f19460c028c548a0e1fecbd7c7bc6
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-07-08 03:47:38 +0000

    포인트 내역 스크린에 empty state 문구 추가 (#2678)

--- apps/mobile/lib/screens/point_history.dart
+++ apps/mobile/lib/screens/point_history.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:glyph/components/Img.dart';
+import 'package:glyph/components/empty_state.dart';
 import 'package:glyph/components/horizontal_divider.dart';
 import 'package:glyph/components/pressable.dart';
 import 'package:glyph/context/bottom_menu.dart';
@@ -12,6 +13,7 @@ import 'package:glyph/graphql/__generated__/point_history_screen_query.data.gql.
 import 'package:glyph/graphql/__generated__/point_history_screen_query.req.gql.dart';
 import 'package:glyph/graphql/__generated__/schema.schema.gql.dart';
 import 'package:glyph/icons/tabler.dart';
+import 'package:glyph/icons/tabler_bold.dart';
 import 'package:glyph/routers/app.gr.dart';
 import 'package:glyph/themes/colors.dart';
 import 'package:jiffy/jiffy.dart';
@@ -25,7 +27,7 @@ class PointHistoryScreen extends ConsumerStatefulWidget {
 }
 
 class _PointHistoryState extends ConsumerState<PointHistoryScreen> {
-  _PointFilter _selectedFeedType = _PointFilter.total;
+  _PointFilter _pointFilterType = _PointFilter.total;
 
   final _pointTransactionCause = {
     GPointTransactionCause.EXPIRE: '만료',
@@ -56,10 +58,10 @@ class _PointHistoryState extends ConsumerState<PointHistoryScreen> {
     return GraphQLOperation(
       operation: GPointHistoryScreen_QueryReq(
         (b) => b
-          ..vars.amountFilter = switch (_selectedFeedType) {
+          ..vars.amountFilter = switch (_pointFilterType) {
             _PointFilter.total => 0,
-            _PointFilter.charge => -1,
-            _PointFilter.usage => 1,
+            _PointFilter.usage => -1,
+            _PointFilter.charge => 1,
           },
       ),
       builder: (context, client, data) {
@@ -73,10 +75,10 @@ class _PointHistoryState extends ConsumerState<PointHistoryScreen> {
                 child: Row(
                   children: [
                     Text(
-                      switch (_selectedFeedType) {
+                      switch (_pointFilterType) {
                         _PointFilter.total => '전체',
-                        _PointFilter.charge => '사용',
-                        _PointFilter.usage => '충전'
+                        _PointFilter.usage => '사용',
+                        _PointFilter.charge => '충전'
                       },
                       style: const TextStyle(
                         fontSize: 15,
@@ -95,7 +97,7 @@ class _PointHistoryState extends ConsumerState<PointHistoryScreen> {
                 onPressed: () async {
                   await context.showBottomSelectMenu(
                     title: '필터',
-                    value: _selectedFeedType,
+                    value: _pointFilterType,
                     items: [
                       BottomSelectMenuItem(
                         label: '전체',
@@ -103,16 +105,16 @@ class _PointHistoryState extends ConsumerState<PointHistoryScreen> {
                       ),
                       BottomSelectMenuItem(
                         label: '사용',
-                        value: _PointFilter.charge,
+                        value: _PointFilter.usage,
                       ),
                       BottomSelectMenuItem(
                         label: '충전',
-                        value: _PointFilter.usage,
+                        value: _PointFilter.charge,
                       ),
                     ],
                     onSelected: (value) {
                       setState(() {
-                        _selectedFeedType = value;
+                        _pointFilterType = value;
                       });
                     },
                   );
@@ -120,100 +122,113 @@ class _PointHistoryState extends ConsumerState<PointHistoryScreen> {
               ),
             ),
             Expanded(
-              child: ListView.separated(
-                physics: const AlwaysScrollableScrollPhysics(
-                  parent: BouncingScrollPhysics(),
-                ),
-                itemCount: points.length,
-                itemBuilder: (context, index) {
-                  final point = points[index];
+              child: points.isEmpty
+                  ? EmptyState(
+                      icon: TablerBold.coin_off,
+                      title: '포인트를 ${switch (_pointFilterType) {
+                        _PointFilter.total => '이용한',
+                        _PointFilter.charge => '충전한',
+                        _PointFilter.usage => '사용한'
+                      }} 내역이 없어요',
+                      description: switch (_pointFilterType) {
+                        _PointFilter.total => '마음에 드는 포스트를 구매해보세요.\n구매한 포스트는 영구 소장이 가능해요',
+                        _PointFilter.charge => '포인트를 충전하고 마음에 드는\n포스트를 감상해보세요',
+                        _PointFilter.usage => '마음에 드는 포스트를 구매해보세요.\n구매한 포스트는 영구 소장이 가능해요',
+                      })
+                  : ListView.separated(
+                      physics: const AlwaysScrollableScrollPhysics(
+                        parent: BouncingScrollPhysics(),
+                      ),
+                      itemCount: points.length,
+                      itemBuilder: (context, index) {
+                        final point = points[index];
 
-                  return Pressable(
-                    child: Padding(
-                      padding: const Pad(horizontal: 20, top: 18, bottom: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                        return Pressable(
+                          child: Padding(
+                            padding: const Pad(horizontal: 20, top: 18, bottom: 20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Text(
-                                  Jiffy.parse(point.createdAt.value, isUtc: true).format(
-                                    pattern: 'yyyy.MM.dd HH:mm 사용',
-                                  ),
-                                  style: const TextStyle(
-                                    fontSize: 11,
-                                    fontWeight: FontWeight.w500,
-                                    color: BrandColors.gray_500,
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        Jiffy.parse(point.createdAt.value, isUtc: true).format(
+                                          pattern: 'yyyy.MM.dd HH:mm 사용',
+                                        ),
+                                        style: const TextStyle(
+                                          fontSize: 11,
+                                          fontWeight: FontWeight.w500,
+                                          color: BrandColors.gray_500,
+                                        ),
+                                      ),
+                                      const Gap(4),
+                                      Text(
+                                        point.maybeWhen(
+                                          unlockContentPointTransaction: (transaction) =>
+                                              transaction.post.publishedRevision?.title ?? '제목 없음',
+                                          purchasePointTransaction: (transaction) =>
+                                              '${_pointPurchasePaymentMethod[transaction.purchase.paymentMethod]}',
+                                          orElse: () => '${_pointTransactionCause[point.cause]}',
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700,
+                                          color: BrandColors.gray_500,
+                                        ),
+                                      ),
+                                      const Gap(6),
+                                      Text(
+                                        '${point.amount}P',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w800,
+                                          color: point.G__typename == 'UnlockContentPointTransaction'
+                                              ? BrandColors.gray_900
+                                              : BrandColors.brand_400,
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                                const Gap(4),
-                                Text(
-                                  point.maybeWhen(
-                                    unlockContentPointTransaction: (transaction) =>
-                                        transaction.post.publishedRevision?.title ?? '제목 없음',
-                                    purchasePointTransaction: (transaction) =>
-                                        '${_pointPurchasePaymentMethod[transaction.purchase.paymentMethod]}',
-                                    orElse: () => '${_pointTransactionCause[point.cause]}',
+                                if (point.G__typename == 'UnlockContentPointTransaction') ...[
+                                  const Gap(18),
+                                  Img(
+                                    point.maybeWhen(
+                                      unlockContentPointTransaction: (transaction) => transaction.post.thumbnail,
+                                      orElse: () => null,
+                                    ),
+                                    width: 78,
+                                    aspectRatio: 16 / 10,
+                                    borderWidth: 1,
+                                    borderRadius: 4,
                                   ),
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w700,
-                                    color: BrandColors.gray_500,
-                                  ),
-                                ),
-                                const Gap(6),
-                                Text(
-                                  '${point.amount}P',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w800,
-                                    color: point.G__typename == 'UnlockContentPointTransaction'
-                                        ? BrandColors.gray_900
-                                        : BrandColors.brand_400,
-                                  ),
-                                ),
+                                ],
                               ],
                             ),
                           ),
-                          if (point.G__typename == 'UnlockContentPointTransaction') ...[
-                            const Gap(18),
-                            Img(
-                              point.maybeWhen(
-                                unlockContentPointTransaction: (transaction) => transaction.post.thumbnail,
-                                orElse: () => null,
-                              ),
-                              width: 78,
-                              aspectRatio: 16 / 10,
-                              borderWidth: 1,
-                              borderRadius: 4,
-                            ),
-                          ],
-                        ],
-                      ),
-                    ),
-                    onPressed: () async {
-                      if (point.G__typename == 'UnlockContentPointTransaction') {
-                        await context.router.push(
-                          PostRoute(
-                            permalink: point.maybeWhen(
-                              unlockContentPointTransaction: (transaction) => transaction.post.permalink,
-                              orElse: () => '',
-                            ),
-                          ),
+                          onPressed: () async {
+                            if (point.G__typename == 'UnlockContentPointTransaction') {
+                              await context.router.push(
+                                PostRoute(
+                                  permalink: point.maybeWhen(
+                                    unlockContentPointTransaction: (transaction) => transaction.post.permalink,
+                                    orElse: () => '',
+                                  ),
+                                ),
+                              );
+                            }
+                          },
                         );
-                      }
-                    },
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const Padding(
-                    padding: Pad(horizontal: 20),
-                    child: HorizontalDivider(color: BrandColors.gray_50),
-                  );
-                },
-              ),
+                      },
+                      separatorBuilder: (context, index) {
+                        return const Padding(
+                          padding: Pad(horizontal: 20),
+                          child: HorizontalDivider(color: BrandColors.gray_50),
+                        );
+                      },
+                    ),
             ),
           ],
         );
