f3c3036be6627dcbe8ce5d2b221f4bac094967d1
devunt <337728+devunt@users.noreply.github.com>
2024-07-15 03:32:06 +0000

    prosemirror용 embedded_webview 추가 (#2771)

---
+++ apps/mobile/lib/prosemirror/special/embedded_webview.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:glyph/env.dart';
+
+const _htmlTemplate = '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      body {
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="content">__CODE__</div>
+    <script>
+      window.addEventListener('flutterInAppWebViewPlatformReady', function (event) {
+        var observer = new ResizeObserver(function (entries) {
+          entries.forEach(function (entry) {
+            window.flutter_inappwebview.callHandler('setHeight', entry.contentRect.height);
+          });
+        });
+
+        observer.observe(document.body);
+      });
+    </script>
+  </body>
+</html>
+''';
+
+class ProseMirrorEmbeddedWebView extends StatefulWidget {
+  const ProseMirrorEmbeddedWebView({
+    required this.html,
+    super.key,
+  });
+
+  final String html;
+
+  @override
+  createState() => _ProseMirrorWebview2State();
+}
+
+class _ProseMirrorWebview2State extends State<ProseMirrorEmbeddedWebView> {
+  final GlobalKey _key = GlobalKey();
+
+  double _height = 1000;
+
+  final _settings = InAppWebViewSettings(
+    allowsBackForwardNavigationGestures: false,
+    disableContextMenu: true,
+    disableLongPressContextMenuOnLinks: true,
+    isTextInteractionEnabled: false,
+    disableHorizontalScroll: true,
+    disableVerticalScroll: true,
+    disallowOverScroll: true,
+    supportZoom: false,
+    transparentBackground: true,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: _height,
+      child: InAppWebView(
+        key: _key,
+        initialSettings: _settings,
+        initialData: InAppWebViewInitialData(
+          data: _htmlTemplate.replaceAll('__CODE__', widget.html),
+          baseUrl: WebUri(Env.baseUrl),
+        ),
+        onWebViewCreated: (controller) {
+          controller.addJavaScriptHandler(
+            handlerName: 'setHeight',
+            callback: (data) {
+              final height = (data[0] as num).toDouble();
+              setState(() {
+                _height = height;
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- apps/mobile/lib/prosemirror/widgets/html.dart
+++ apps/mobile/lib/prosemirror/widgets/html.dart
@@ -1,29 +1,9 @@
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:glyph/prosemirror/schema.dart';
+import 'package:glyph/prosemirror/special/embedded_webview.dart';
 import 'package:glyph/prosemirror/special/padded.dart';
 import 'package:glyph/themes/colors.dart';
-
-const String _getHeightJsSource = '''
-  var getHeight = function() {
-    window.flutter_inappwebview.callHandler('getHeight', document.body.scrollHeight);
-  };
-  getHeight();
-
-  var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      getHeight();
-    });
-  });
-
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-    characterData: true,
-    attributes: true,
-  });
-''';
 
 class ProseMirrorWidgetHtml extends StatelessWidget {
   const ProseMirrorWidgetHtml({
@@ -41,20 +21,6 @@ class ProseMirrorWidgetHtml extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final settings = InAppWebViewSettings(
-      allowsBackForwardNavigationGestures: false,
-      disableContextMenu: true,
-      disableLongPressContextMenuOnLinks: true,
-      isTextInteractionEnabled: false,
-      disableHorizontalScroll: true,
-      disableVerticalScroll: true,
-      disallowOverScroll: true,
-      supportZoom: false,
-      transparentBackground: true,
-    );
-
-    double height = 1.0;
-
     return ProseMirrorPadded(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -77,34 +43,8 @@ class ProseMirrorWidgetHtml extends StatelessWidget {
             decoration: BoxDecoration(
               border: Border.all(color: BrandColors.gray_150),
             ),
-            child: StatefulBuilder(
-              builder: (context, setState) {
-                return SizedBox(
-                  height: height,
-                  child: InAppWebView(
-                    key: key,
-                    initialSettings: settings,
-                    initialData: InAppWebViewInitialData(data: node.content?.first.text ?? ''),
-                    preventGestureDelay: true,
-                    onWebViewCreated: (controller) async {
-                      controller.addJavaScriptHandler(
-                        handlerName: 'getHeight',
-                        callback: (data) {
-                          final newHeight = double.parse(data[0].toString());
-                          setState(() {
-                            height = newHeight;
-                          });
-                        },
-                      );
-                    },
-                    onLoadStop: (controller, url) async {
-                      await controller.evaluateJavascript(
-                        source: _getHeightJsSource,
-                      );
-                    },
-                  ),
-                );
-              },
+            child: ProseMirrorEmbeddedWebView(
+              html: node.content?.first.text ?? '',
             ),
           ),
         ],
