ef632ef9590335616100fcc2340800a0d179525f
devunt <337728+devunt@users.noreply.github.com>
2024-07-15 03:32:09 +0000

    embed 노드 네이티브 렌더러 구현 (#2772)

--- apps/mobile/lib/prosemirror/widgets/embed.dart
+++ apps/mobile/lib/prosemirror/widgets/embed.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:glyph/ferry/widget.dart';
+import 'package:glyph/graphql/__generated__/prosemirror_widget_embed_unfurl_embed_mutation.req.gql.dart';
 import 'package:glyph/prosemirror/schema.dart';
-import 'package:glyph/prosemirror/special/webview.dart';
+import 'package:glyph/prosemirror/special/embedded_webview.dart';
+import 'package:glyph/prosemirror/special/padded.dart';
 
 class ProseMirrorWidgetEmbed extends StatelessWidget {
   const ProseMirrorWidgetEmbed({
-    required this.node,
+    required this.url,
+    required this.mode,
     super.key,
   });
 
   factory ProseMirrorWidgetEmbed.node(ProseMirrorNode node) {
     return ProseMirrorWidgetEmbed(
-      node: node,
+      url: node.attrs?['url'] as String? ?? '',
+      mode: node.attrs?['mode'] as String? ?? 'opengraph',
     );
   }
 
-  final ProseMirrorNode node;
+  final String url;
+  final String mode;
 
   @override
   Widget build(BuildContext context) {
-    return ProseMirrorWebView(node: node);
+    return GraphQLOperation(
+      operation: GProseMirrorWidgetEmbed_UnfurlEmbed_MutationReq(
+        (b) => b..vars.input.url = url,
+      ),
+      builder: (context, client, data) {
+        return switch (mode) {
+          'embed-full' => ProseMirrorEmbeddedWebView(html: data.unfurlEmbed.html!),
+          'embed-compact' => ProseMirrorPadded(
+              child: Center(
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 500),
+                  child: ProseMirrorEmbeddedWebView(html: data.unfurlEmbed.html!),
+                ),
+              ),
+            ),
+          'opengraph' => const ProseMirrorPadded(child: SizedBox.shrink()),
+          _ => const SizedBox.shrink(),
+        };
+      },
+    );
   }
 }
