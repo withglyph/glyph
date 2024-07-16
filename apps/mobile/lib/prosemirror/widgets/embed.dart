b8a92405c5093e62380cb33f49af773d1444219b
devunt <337728+devunt@users.noreply.github.com>
2024-07-16 03:10:06 +0000

    임베드 네이티브 렌더러 구현 (#2775)

--- apps/mobile/lib/prosemirror/widgets/embed.dart
+++ apps/mobile/lib/prosemirror/widgets/embed.dart
@@ -1,9 +1,16 @@
+import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
+import 'package:glyph/components/pressable.dart';
 import 'package:glyph/ferry/widget.dart';
 import 'package:glyph/graphql/__generated__/prosemirror_widget_embed_unfurl_embed_mutation.req.gql.dart';
 import 'package:glyph/prosemirror/schema.dart';
 import 'package:glyph/prosemirror/special/embedded_webview.dart';
 import 'package:glyph/prosemirror/special/padded.dart';
+import 'package:glyph/themes/colors.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProseMirrorWidgetEmbed extends StatelessWidget {
   const ProseMirrorWidgetEmbed({
@@ -39,7 +46,71 @@ class ProseMirrorWidgetEmbed extends StatelessWidget {
                 ),
               ),
             ),
-          'opengraph' => const ProseMirrorPadded(child: SizedBox.shrink()),
+          'opengraph' => ProseMirrorPadded(
+              child: Pressable(
+                child: Container(
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: BrandColors.gray_200),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Gap(16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Gap(12),
+                            Text(
+                              data.unfurlEmbed.title ?? '(제목 없음)',
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                            ),
+                            const Gap(2),
+                            if (data.unfurlEmbed.description != null)
+                              Text(
+                                data.unfurlEmbed.description!,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(fontSize: 12, color: BrandColors.gray_400),
+                              ),
+                            const Gap(6),
+                            Text(
+                              Uri.parse(data.unfurlEmbed.url).host,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(fontSize: 11, color: BrandColors.brand_400),
+                            ),
+                            const Gap(12),
+                          ],
+                        ),
+                      ),
+                      const Gap(32),
+                      if (data.unfurlEmbed.thumbnailUrl != null) ...[
+                        const Box(width: 1, color: BrandColors.gray_200),
+                        CachedNetworkImage(
+                          imageUrl: data.unfurlEmbed.thumbnailUrl!,
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                          fadeInDuration: const Duration(milliseconds: 150),
+                        ),
+                      ],
+                    ],
+                  ),
+                ),
+                onPressed: () async {
+                  try {
+                    await launchUrl(
+                      Uri.parse(data.unfurlEmbed.url),
+                      mode: LaunchMode.inAppBrowserView,
+                    );
+                  } on Exception {
+                    // ignore
+                  }
+                },
+              ),
+            ),
           _ => const SizedBox.shrink(),
         };
       },
